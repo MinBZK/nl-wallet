ce81e325a273b0adbba9bedde4d3fccc09217182
confiks <confiks@scriptbase.org>
2022-10-10 11:07:17 +0200

    chore: Overwrite with: flutter create (...) --template skeleton

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,31 @@
-// This is a basic Flutter widget test.
+// This is an example Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+//
+// Visit https://flutter.dev/docs/cookbook/testing/widget/introduction for
+// more information about Widget testing.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wallet/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('MyWidget', () {
+    testWidgets('should display a string of text', (WidgetTester tester) async {
+      // Define a Widget
+      const myWidget = MaterialApp(
+        home: Scaffold(
+          body: Text('Hello'),
+        ),
+      );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Build myWidget and trigger a frame.
+      await tester.pumpWidget(myWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Verify myWidget shows some text
+      expect(find.byType(Text), findsOneWidget);
+    });
   });
 }
