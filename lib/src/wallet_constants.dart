18e4e9ef4d0e2794c5c6d2a7b6c6bce66ff0994f
Rob <edi@minbzk.nl>
2022-12-29 10:47:18 +0100

    Increase AutoLock Timeouts (#168)
    Reference: https://jira.ictu-sd.nl/jira/browse/PVW-190

--- lib/src/wallet_constants.dart
+++ lib/src/wallet_constants.dart
@@ -7,8 +7,8 @@ const kDefaultAnimationDuration = Duration(milliseconds: 300);
 const kPinDigits = 6;
 const kMaxUnlockAttempts = 3;
 const kMockPin = '123456';
-const kBackgroundLockTimeout = Duration(minutes: kDebugMode ? 10 : 1);
-const kIdleLockTimeout = Duration(minutes: kDebugMode ? 50 : 5);
+const kBackgroundLockTimeout = Duration(minutes: kDebugMode ? 10 : 5);
+const kIdleLockTimeout = Duration(minutes: kDebugMode ? 50 : 20);
 
 // Mocking
 const kDefaultMockDelay = Duration(milliseconds: 500);
