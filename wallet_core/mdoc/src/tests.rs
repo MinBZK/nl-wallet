8494ba4316265fe2eae9b4f46762569928462adb
Sietse Ringers <mail@sietseringers.net>
2023-01-28 17:15:57 +0100

    refactor: decrease nested code in DebugCollapseBts::fmt()

--- wallet_core/mdoc/src/tests.rs
+++ wallet_core/mdoc/src/tests.rs
@@ -248,34 +248,30 @@ where
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         // Match numbers within square brackets, e.g.: [1, 2, 3]
-        let re = regex::Regex::new(r"\[\s*(\d,?\s*)+\]").unwrap();
-
-        write!(
-            f,
-            "{}",
-            re.replace_all(
-                format!("{:#?}", self.0).as_ref(),
-                |caps: &regex::Captures| {
-                    let no_whitespace = remove_whitespace(&caps[0]);
-                    let trimmed = no_whitespace[1..no_whitespace.len() - 2].to_string(); // Remove square brackets
-                    if trimmed.split(',').any(|r| r.parse::<u8>().is_err()) {
-                        // If any of the numbers don't fit in a u8, just return the numbers without whitespace
-                        no_whitespace
-                    } else {
-                        format!(
-                            "h'{}'", // CBOR diagnostic-like notation
-                            hex::encode(
-                                trimmed
-                                    .split(',')
-                                    .map(|i| i.parse::<u8>().unwrap())
-                                    .collect::<Vec<u8>>(),
-                            )
-                            .to_uppercase()
+        let debugstr = format!("{:#?}", self.0);
+        let debugstr_collapsed = regex::Regex::new(r"\[\s*(\d,?\s*)+\]")
+            .unwrap()
+            .replace_all(debugstr.as_str(), |caps: &regex::Captures| {
+                let no_whitespace = remove_whitespace(&caps[0]);
+                let trimmed = no_whitespace[1..no_whitespace.len() - 2].to_string(); // Remove square brackets
+                if trimmed.split(',').any(|r| r.parse::<u8>().is_err()) {
+                    // If any of the numbers don't fit in a u8, just return the numbers without whitespace
+                    no_whitespace
+                } else {
+                    format!(
+                        "h'{}'", // CBOR diagnostic-like notation
+                        hex::encode(
+                            trimmed
+                                .split(',')
+                                .map(|i| i.parse::<u8>().unwrap())
+                                .collect::<Vec<u8>>(),
                         )
-                    }
+                        .to_uppercase()
+                    )
                 }
-            )
-        )
+            });
+
+        write!(f, "{}", debugstr_collapsed)
     }
 }
 
