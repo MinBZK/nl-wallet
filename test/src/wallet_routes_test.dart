c0696d6886528cebcdf20adfba6854fdda05f9ff
Daan <2034049+daandebie@users.noreply.github.com>
2023-02-23 07:47:18 +0000

    Add unit test conventions
    * Update existing tests
    
    * Update existing tests
    
    * Add unit test conventions
    
    * Remove example tests
    
    * Add conventions sub list

--- test/src/wallet_routes_test.dart
+++ test/src/wallet_routes_test.dart
@@ -3,18 +3,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wallet/src/wallet_routes.dart';
 
 void main() {
-  group('Route Security', () {
-    test('home route should be secured', () {
-      const routeSettings = RouteSettings(name: WalletRoutes.homeRoute);
-      final Route homeRoute = WalletRoutes.routeFactory(routeSettings);
-      expect(homeRoute, isA<SecuredPageRoute>());
-    });
+  test('.homeRoute should be a `SecuredPageRoute`', () {
+    const routeSettings = RouteSettings(name: WalletRoutes.homeRoute);
+    final Route homeRoute = WalletRoutes.routeFactory(routeSettings);
+    expect(homeRoute, isA<SecuredPageRoute>());
+  });
 
-    test('public routes should not be secured', () {
-      for (var routeName in WalletRoutes.publicRoutes) {
-        final Route route = WalletRoutes.routeFactory(RouteSettings(name: routeName));
-        expect(route, isNot(isA<SecuredPageRoute>()));
-      }
-    });
+  test('.publicRoutes should not be a `SecuredPageRoute`', () {
+    for (var routeName in WalletRoutes.publicRoutes) {
+      final Route route = WalletRoutes.routeFactory(RouteSettings(name: routeName));
+      expect(route, isNot(isA<SecuredPageRoute>()));
+    }
   });
 }
