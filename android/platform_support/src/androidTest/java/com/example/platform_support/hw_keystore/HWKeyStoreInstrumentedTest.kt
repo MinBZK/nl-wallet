640e15c4553040127dcc8f248d503035111a082e
Daan <2034049+daandebie@users.noreply.github.com>
2023-03-13 14:50:08 +0100

    Clean up module dependencies

--- android/platform_support/src/androidTest/java/com/example/platform_support/hw_keystore/HWKeyStoreInstrumentedTest.kt
+++ android/platform_support/src/androidTest/java/com/example/platform_support/hw_keystore/HWKeyStoreInstrumentedTest.kt
@@ -7,11 +7,6 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.assertNotNull
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class HWKeyStoreInstrumentedTest {
     @Test
