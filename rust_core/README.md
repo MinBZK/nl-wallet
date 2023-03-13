b4afe50b01173b82e9e3821008028ce4316773e4
Ruud Klaver <ruud@pantsforcats.nl>
2023-03-13 17:12:53 +0100

    Make flutter-data-types code generation independent of working dir, integrate in CI

--- rust_core/README.md
+++ rust_core/README.md
@@ -1,6 +1,6 @@
 # Regenerate the flutter bindings
 
-To regenerate the bindings, run the following command from the root of the repository:
+To regenerate the bindings, run the following command from `rust_core`:
 
 ```sh
 cargo run --manifest-path flutter_rust_bridge_codegen/Cargo.toml
@@ -8,14 +8,14 @@ cargo run --manifest-path flutter_rust_bridge_codegen/Cargo.toml
 
 # Regenerate the datatypes for bincode
 
-From the folder `rust_core/flutter-data-types`, run the following command:
+To regenerate the data types, run the following command from `rust_core`:
 
 ```sh
-cargo run
+cargo run --manifest-path flutter-data-types/Cargo.toml
 ```
 
-This will generate dart code in `$PROJECT_ROOT/pub/core_domain`. After which the classes are available in the Flutter app by importing `import 'package:core_domain/core_domain.dart';
-`.
+This will generate dart code in `$PROJECT_ROOT/pub/core_domain`.
+After which the classes are available in the Flutter app by importing `import 'package:core_domain/core_domain.dart';`.
 
 # Project structure
 
--- rust_core/flutter-data-types/src/main.rs
+++ rust_core/flutter-data-types/src/main.rs
@@ -1,25 +1,15 @@
-use flutter_data_types::*;
+use flutter_data_types::{PinError, PinResult};
 use serde_reflection::{Registry, Tracer, TracerConfig};
-use std::path::PathBuf;
+use std::{concat, env, path::PathBuf};
+
+const DART_OUTPUT_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../../pub/core_domain");
 
 fn main() {
     let mut tracer = Tracer::new(TracerConfig::default());
     tracer.trace_simple_type::<PinResult>().unwrap();
     tracer.trace_simple_type::<PinError>().unwrap();
     let registry = tracer.registry().unwrap();
-    // generate_java(&registry);
     generate_dart(&registry);
-    // generate_swift(&registry);
-}
-
-// Create Java class definitions.
-fn generate_java(registry: &Registry) {
-    let config =
-        serde_generate::CodeGeneratorConfig::new("nl.rijksoverheid.edi.wallet".to_string())
-            .with_encodings(vec![serde_generate::Encoding::Bincode])
-            .with_c_style_enums(true);
-    let generator = serde_generate::java::CodeGenerator::new(&config);
-    let _result = generator.write_source_files(PathBuf::from("./generated/java/"), &registry);
 }
 
 // Create Dart class definitions.
@@ -28,18 +18,5 @@ fn generate_dart(registry: &Registry) {
         .with_encodings(vec![serde_generate::Encoding::Bincode])
         .with_c_style_enums(true);
     let generator = serde_generate::dart::CodeGenerator::new(&config);
-    let _result = generator.output(PathBuf::from("../../pub/core_domain"), &registry);
-}
-
-// Create Swift class definitions.
-fn generate_swift(registry: &Registry) {
-    let mut source = Vec::new();
-    let config =
-        serde_generate::CodeGeneratorConfig::new("nl.rijksoverheid.edi.wallet".to_string())
-            .with_c_style_enums(true)
-            .with_encodings(vec![serde_generate::Encoding::Bincode]);
-    let generator = serde_generate::swift::CodeGenerator::new(&config);
-    // let _result = generator.output(PathBuf::from("./generated/swift/"), &registry);
-    let _result = generator.output(&mut source, &registry);
-    println!("{}", String::from_utf8_lossy(&source))
+    let _result = generator.output(PathBuf::from(DART_OUTPUT_PATH), &registry);
 }
