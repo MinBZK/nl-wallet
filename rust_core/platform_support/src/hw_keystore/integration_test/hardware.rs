90373473bb361ab79fdbf4269a2ff2d33df5c2cf
Rob <edi@minbzk.nl>
2023-03-17 11:42:16 +0000

    Remove hidden doc attribute from "platform_support" integration test code

--- rust_core/platform_support/src/hw_keystore/integration_test/hardware.rs
+++ rust_core/platform_support/src/hw_keystore/integration_test/hardware.rs
@@ -16,7 +16,6 @@ fn hw_keystore_test_hardware_signature() -> bool {
     sign_and_verify_signature(&keystore, payload, identifier)
 }
 
-#[doc(hidden)]
 #[no_mangle]
 pub unsafe extern "C" fn Java_nl_rijksoverheid_edi_wallet_platform_1support_hw_1keystore_HWKeyStoreInstrumentedTest_hw_1keystore_1test_1hardware_1signature(
     _env: JNIEnv,
