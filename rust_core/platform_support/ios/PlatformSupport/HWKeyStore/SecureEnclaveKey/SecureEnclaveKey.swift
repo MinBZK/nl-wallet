0f54b8532e3f720b8f214ed44f03448894581a53
Ruud Klaver <ruud@pantsforcats.nl>
2023-03-17 18:53:54 +0100

    Remove public key caching from iOS implementation of hw_keystore, since caching is now being done on the Rust side

--- rust_core/platform_support/ios/PlatformSupport/HWKeyStore/SecureEnclaveKey/SecureEnclaveKey.swift
+++ rust_core/platform_support/ios/PlatformSupport/HWKeyStore/SecureEnclaveKey/SecureEnclaveKey.swift
@@ -130,7 +130,6 @@ final class SecureEnclaveKey {
     let identifier: String
 
     private let privateKey: SecKey
-    private var _publicKey: Data?
 
     // MARK: - Initializer
 
@@ -149,14 +148,7 @@ final class SecureEnclaveKey {
     // MARK: - Instance methods
 
     func publicKey() throws -> Data {
-        guard let publicKey = self._publicKey else {
-            let publicKey = try Self.derivePublicKey(from: self.privateKey)
-            self._publicKey = publicKey
-
-            return publicKey
-        }
-
-        return publicKey
+        return try Self.derivePublicKey(from: self.privateKey)
     }
 
     func sign(payload: Data) throws -> Data {
