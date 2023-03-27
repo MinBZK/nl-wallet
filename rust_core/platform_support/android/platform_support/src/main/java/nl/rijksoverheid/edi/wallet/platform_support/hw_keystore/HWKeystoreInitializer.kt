6e11be8f1368be193564366e3e1f3e35d0495c6e
Rob <edi@minbzk.nl>
2023-03-27 17:21:26 +0200

    Override loaded .so Library
    The app was crashing because the platform_support
    library was always trying to load the libplatform_support.so
    file, even though all this code is part of the librust_core.so
    file and therefor the libplatform_support.so file is excluded
    when packaging the app to safe space. This commit allows
    consumers of the platform_support module to override which
    library file should be loaded by setting some metadata in
    the AndroidManifest.xml.

--- rust_core/platform_support/android/platform_support/src/main/java/nl/rijksoverheid/edi/wallet/platform_support/hw_keystore/HWKeystoreInitializer.kt
+++ rust_core/platform_support/android/platform_support/src/main/java/nl/rijksoverheid/edi/wallet/platform_support/hw_keystore/HWKeystoreInitializer.kt
@@ -1,10 +1,39 @@
 package nl.rijksoverheid.edi.wallet.platform_support.hw_keystore
 
 import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.startup.Initializer
 
+// Any app consuming this library can (optionally) use this key to override which .so should be loaded
+private const val libraryOverrideManifestKey =
+    "nl.rijksoverheid.edi.wallet.platform_support.libraryOverride"
+
+// The key used by the generated code [hw_keystore.kt] to check which .so should be loaded
+private const val libraryOverridePropertyKey = "uniffi.component.hw_keystore.libraryOverride"
+
 class HWKeystoreInitializer : Initializer<HWKeyStore> {
-    override fun create(context: Context): HWKeyStore = HWKeyStore(context)
+    override fun create(context: Context): HWKeyStore {
+        val appInfo = context.packageManager.getApplicationInfoCompat(
+            context.packageName,
+            PackageManager.GET_META_DATA
+        )
+        appInfo.metaData.getString(libraryOverrideManifestKey)?.let { libraryOverride ->
+            System.setProperty(libraryOverridePropertyKey, libraryOverride)
+        }
+        return HWKeyStore(context)
+    }
 
     override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
 }
+
+private fun PackageManager.getApplicationInfoCompat(
+    packageName: String,
+    flags: Int = 0
+): ApplicationInfo =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getApplicationInfo(packageName, PackageManager.ApplicationInfoFlags.of(flags.toLong()))
+    } else {
+        @Suppress("DEPRECATION") getApplicationInfo(packageName, flags)
+    }
