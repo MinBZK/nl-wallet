64b343705a6f3b568f2b7c99a3364c1d76bc178a
Ruud Klaver <ruud@pantsforcats.nl>
2023-03-28 21:04:59 +0200

    Remove now unnecessary comment from "platform_support" integration test function

--- rust_core/platform_support/src/hw_keystore/integration_test/hardware.rs
+++ rust_core/platform_support/src/hw_keystore/integration_test/hardware.rs
@@ -11,9 +11,6 @@ fn hw_keystore_test_hardware_signature() -> bool {
     sign_and_verify_signature::<HardwareSigningKey>(payload, identifier)
 }
 
-/// # Safety
-///
-/// Should only be called by Android integration test
 #[no_mangle]
 extern "C" fn Java_nl_rijksoverheid_edi_wallet_platform_1support_hw_1keystore_HWKeyStoreInstrumentedTest_hw_1keystore_1test_1hardware_1signature(
     _env: JNIEnv,
