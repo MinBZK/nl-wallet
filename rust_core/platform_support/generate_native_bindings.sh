4382c91f0c90ab84e7191964934d0cfa785c9238
Daan <2034049+daandebie@users.noreply.github.com>
2023-03-28 09:41:22 +0200

    Remove unused uniffi-bindgen config file approach
    This approach isn't needed anymore due to not covering
    all scenarios where native library name needs to be
    controlled/overritten. The new approach is using a
    System property to override the library name when the
    app is launched.

--- rust_core/platform_support/generate_native_bindings.sh
+++ rust_core/platform_support/generate_native_bindings.sh
@@ -5,11 +5,10 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-DEFAULT_CONFIG_FILE="uniffi.toml"
 
 MODULE_NAME=$1
 NATIVE_LANGUAGE=$2
-CONFIG_FILE="${3:-$DEFAULT_CONFIG_FILE}"
+CONFIG_FILE="uniffi.toml"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
