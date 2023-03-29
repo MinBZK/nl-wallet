cc1b9bacfb2bb0464a732148e84be32a749ec60a
Ruud Klaver <ruud@pantsforcats.nl>
2023-03-29 17:13:21 +0200

    Fix error string formatting in "platform_support"

--- rust_core/platform_support/src/hw_keystore/error.rs
+++ rust_core/platform_support/src/hw_keystore/error.rs
@@ -4,15 +4,15 @@ use thiserror::Error;
 pub enum HardwareKeyStoreError {
     #[error(transparent)]
     KeyStoreError(#[from] KeyStoreError),
-    #[error("Error decoding public key from hardware: {0:?}")]
+    #[error("Error decoding public key from hardware: {0}")]
     PublicKeyError(#[from] p256::pkcs8::spki::Error),
 }
 
 // implementation of KeyStoreError from UDL
 #[derive(Debug, Error)]
 pub enum KeyStoreError {
-    #[error("Key error: {reason:?}")]
+    #[error("Key error: {reason}")]
     KeyError { reason: String },
-    #[error("Bridging error: {reason:?}")]
+    #[error("Bridging error: {reason}")]
     BridgingError { reason: String },
 }
