c60f6d025584e790ccf7e23d67eb0ec774c0a1e7
Ruud Klaver <ruud@pantsforcats.nl>
2023-03-30 14:46:52 +0200

    Propagate errors when creating hardware signing key during wallet registration

--- rust_core/wallet/src/lib.rs
+++ rust_core/wallet/src/lib.rs
@@ -8,7 +8,7 @@ mod utils;
 mod wallet;
 
 use once_cell::sync::Lazy;
-use platform_support::hw_keystore::{PlatformSigningKey, PreferredPlatformSigningKey};
+use platform_support::hw_keystore::PreferredPlatformSigningKey;
 use std::sync::Mutex;
 
 use crate::{account::client::server::AccountServer, wallet::Wallet};
@@ -16,11 +16,8 @@ use crate::{account::client::server::AccountServer, wallet::Wallet};
 const WALLET_KEY_ID: &str = "wallet";
 
 pub static WALLET: Lazy<Mutex<Wallet<AccountServer, PreferredPlatformSigningKey>>> = Lazy::new(|| {
-    let hw_privkey =
-        PreferredPlatformSigningKey::signing_key(WALLET_KEY_ID).expect("Could not fetch or generate wallet key");
-
     let account_server = AccountServer::new_stub(); // TODO
     let pubkey = account_server.pubkey.clone();
 
-    Mutex::new(Wallet::new(account_server, pubkey, hw_privkey))
+    Mutex::new(Wallet::new(account_server, pubkey))
 });
--- rust_core/wallet/src/wallet/mod.rs
+++ rust_core/wallet/src/wallet/mod.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
-use platform_support::hw_keystore::PlatformSigningKey;
+use once_cell::sync::OnceCell;
+use platform_support::hw_keystore::{error::HardwareKeyStoreError, PlatformSigningKey};
 
 use crate::{
     account::{
@@ -9,13 +10,15 @@ use crate::{
     pin::validate_pin,
 };
 
+const WALLET_KEY_ID: &str = "wallet";
+
 pub struct Wallet<T, S> {
     account_server: T,
     account_server_pubkey: Vec<u8>,
     registration_cert: Option<WalletCertificate>,
 
     pin_salt: Vec<u8>,
-    hw_privkey: S,
+    hw_privkey: OnceCell<S>,
 }
 
 impl<T, S> Wallet<T, S>
@@ -23,23 +26,32 @@ where
     T: AccountServerClient,
     S: PlatformSigningKey,
 {
-    pub fn new(account_server: T, account_server_pubkey: Vec<u8>, hw_privkey: S) -> Wallet<T, S> {
+    fn hw_privkey(&self) -> std::result::Result<&S, HardwareKeyStoreError> {
+        self.hw_privkey.get_or_try_init(|| {
+            let signing_key = S::signing_key(WALLET_KEY_ID)?;
+
+            Ok::<_, HardwareKeyStoreError>(signing_key)
+        })
+    }
+
+    pub fn new(account_server: T, account_server_pubkey: Vec<u8>) -> Wallet<T, S> {
         Wallet {
             account_server,
             account_server_pubkey,
             registration_cert: None,
             pin_salt: new_pin_salt(), // TODO look up in storage
-            hw_privkey,
+            hw_privkey: OnceCell::new(),
         }
     }
 
     pub fn register(&mut self, pin: String) -> Result<()> {
         validate_pin(&pin)?;
 
+        let hw_privkey = self.hw_privkey()?;
         let challenge = self.account_server.registration_challenge()?;
         let pin_key = PinKey::new(&pin, &self.pin_salt);
 
-        let registration_message = Registration::new_signed(&self.hw_privkey, &pin_key, &challenge)?;
+        let registration_message = Registration::new_signed(hw_privkey, &pin_key, &challenge)?;
         let cert = self.account_server.register(registration_message)?;
 
         self.registration_cert = Some(cert);
@@ -53,12 +65,11 @@ mod tests {
     use super::*;
 
     use p256::ecdsa::SigningKey;
-    use rand::rngs::OsRng;
 
     #[test]
     fn it_works() {
         let (account_server, account_server_pubkey) = crate::account::client::server::tests::new_account_server();
-        let mut wallet = Wallet::new(account_server, account_server_pubkey, SigningKey::random(&mut OsRng));
+        let mut wallet: Wallet<_, SigningKey> = Wallet::new(account_server, account_server_pubkey);
 
         assert!(wallet.register("123456".to_owned()).is_err());
 
