1a1e16f031d9f34496b894d4d308a03f5f02615b
Rob <edi@minbzk.nl>
2023-04-07 10:27:10 +0200

    Test through PlatformUtilities trait

--- rust_core/platform_support/src/utils/integration_test/mod.rs
+++ rust_core/platform_support/src/utils/integration_test/mod.rs
@@ -5,7 +5,7 @@ use crate::utils::PlatformUtilities;
 pub mod hardware;
 
 pub fn get_and_verify_storage_path<K: PlatformUtilities>() -> bool {
-    let path = UTILITIES.get().expect("Could not get utilities")
-        .get_storage_path().expect("Could not get storage path");
+    let path =  K::storage_path().expect("Could not get storage path")
+        .into_os_string().into_string().expect("Could not convert PathBuf to String");
     path.len() > 0 && path.starts_with("/")
 }
