cc7477f85e667ea9a8bf87508e4a316fd246b714
Ruud Klaver <ruud@pantsforcats.nl>
2023-04-12 16:28:31 +0200

    Initialize "platform_support" utils module in main iOS app

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,12 +5,14 @@ import Flutter
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   var hardwareKeyStore: HWKeyStore?
+  var utils: Utils?
 
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     self.hardwareKeyStore = HWKeyStore.shared
+    self.utils = Utils.shared
 
     let dummy = dummy_method_to_enforce_bundling()
     print(dummy)
