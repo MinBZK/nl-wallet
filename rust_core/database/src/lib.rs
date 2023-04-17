ab7a46306b78405155497692798e54f5e5dacc8a
Rob <edi@minbzk.nl>
2023-04-17 15:44:58 +0200

    Encrypt Stored Passwords
    Make sure the passwords that are written
    to disk are encrypted before storing them
    to disk.

--- rust_core/database/src/lib.rs
+++ rust_core/database/src/lib.rs
@@ -1,17 +1,22 @@
 use std::fs;
+use std::str;
 
 use rand::distributions::{Alphanumeric, DistString};
 use rusqlite::Connection;
+
+use platform_support::hw_keystore::PlatformEncryptionKey;
+use platform_support::hw_keystore::software::SoftwareEncryptionKey;
 use platform_support::utils::PlatformUtilities;
 use platform_support::utils::software::SoftwareUtilities;
 
 pub async fn get_or_create_db(db_name: &str) -> Connection {
     //Get path to database
+    //TODO: Migrate to generic PlatformUtilities
     let storage_path = SoftwareUtilities::storage_path().expect("Could not get storage path");
     let db_path = storage_path.join(format!("{}.db", db_name));
 
     // Get db password
-    let db_password = get_or_create_db_password(db_name).await;
+    let db_password = get_or_create_password(db_name).await;
 
     // Open db
     let conn = Connection::open(db_path).expect("Failed to open database");
@@ -25,30 +30,48 @@ pub async fn get_or_create_db(db_name: &str) -> Connection {
 }
 
 fn delete_db(db_name: &str) {
-    //Get path to database
+    // Get path to database
+    //TODO: Migrate to generic PlatformUtilities
     let storage_path = SoftwareUtilities::storage_path().expect("Could not get storage path");
     let db_path = storage_path.join(format!("{}.db", db_name));
     if db_path.exists() { fs::remove_file(db_path).expect("Failed to delete database"); }
+
+    // Database password relies on same name password file, clean that up too.
+    delete_password(db_name);
 }
 
-pub async fn get_or_create_db_password(alias: &str) -> String {
+pub async fn get_or_create_password(alias: &str) -> String {
     // Get path to password file
+    //TODO: Migrate to generic PlatformUtilities
     let storage_path = SoftwareUtilities::storage_path().expect("Could not get storage path");
     let pw_file_path = storage_path.join(format!("{}.pass", alias));
 
     if pw_file_path.exists() {
         // Open file
-        let contents = fs::read_to_string(pw_file_path).expect("Could not read password.txt");
-        // Decrypt password
-        //TODO: Decrypt the contents
-        contents
+        let contents = fs::read(pw_file_path).expect("Unable to read password from file");
+        //TODO: Migrate to generic PlatformEncryptionKey
+        let key = SoftwareEncryptionKey::encryption_key("database").expect("Could not get key");
+        let decrypted_contents = key.decrypt(&contents).expect("Could not decrypt contents");
+        let password = str::from_utf8(&decrypted_contents).expect("Could not convert");
+        password.to_string()
     } else {
         // Generate password
         let new_password = generate_db_password();
-        //TODO: Encrypt the password
-        fs::write(pw_file_path, &new_password).expect("Unable to write password.txt");
+        //TODO: Migrate to generic PlatformEncryptionKey
+        let key = SoftwareEncryptionKey::encryption_key("database").expect("Could not get key");
+        let encrypted_pass = key.encrypt(new_password.as_bytes()).expect("Could not encrypt password");
+        fs::write(pw_file_path, &encrypted_pass).expect("Unable to write password to file");
         new_password
     }
+}
+
+
+fn delete_password(alias: &str) {
+    // Get path to the password file
+    //TODO: Migrate to generic PlatformUtilities
+    let storage_path = SoftwareUtilities::storage_path().expect("Could not get storage path");
+    let db_path = storage_path.join(format!("{}.pass", alias));
+    if db_path.exists() { fs::remove_file(db_path).expect("Failed to delete password"); }
 }
 
 fn generate_db_password() -> String {
@@ -74,26 +97,32 @@ mod tests {
 
     #[tokio::test]
     async fn test_create_and_get_password() {
-        let alias = "alias";
-        let created_pass = get_or_create_db_password(alias).await;
-        let fetched_pass = get_or_create_db_password(alias).await;
+        let alias = "password_alias";
+        // Make sure we start with a clean slate
+        delete_password(alias);
+        let created_pass = get_or_create_password(alias).await;
+        let fetched_pass = get_or_create_password(alias).await;
         assert_eq!(created_pass, fetched_pass)
     }
 
     #[tokio::test]
     async fn test_password_should_be_unique() {
-        let alias = "pass1";
-        let alias2 = "pass2";
-        let pass1 = get_or_create_db_password(alias).await;
-        let pass2 = get_or_create_db_password(alias2).await;
+        let alias = "password1";
+        let alias2 = "password2";
+        // Make sure we start with a clean slate
+        delete_password(alias);
+        delete_password(alias2);
+
+        let pass1 = get_or_create_password(alias).await;
+        let pass2 = get_or_create_password(alias2).await;
         assert_ne!(pass1, pass2)
     }
 
     #[tokio::test]
     async fn open_db() {
-        let db_name = "test";
+        let db_name = "test_db";
 
-        // Make sure we always start clean, e.g. when previous test failed.
+        // Make sure we start with a clean slate
         delete_db(db_name);
 
         // Create a new (encrypted) database
@@ -143,8 +172,5 @@ mod tests {
 
         // Verify we really checked our test person (and did not loop over empty iterator)
         assert_eq!(person_count, 1);
-
-        // Clean up test db
-        delete_db(db_name);
     }
 }
