53ebc75d50edd81257f6837aa64ef702a1ff201a
Ruud Klaver <ruud@pantsforcats.nl>
2023-04-25 15:33:57 +0200

    Use SeaORM instead of Rusqlite for opening database

--- wallet_core/wallet/src/storage/database.rs
+++ wallet_core/wallet/src/storage/database.rs
@@ -1,9 +1,9 @@
-use std::{path::PathBuf, str::FromStr};
+use std::path::PathBuf;
 
 use anyhow::Result;
 use platform_support::{hw_keystore::PlatformEncryptionKey, utils::PlatformUtilities};
-use rusqlite::Connection;
-use tokio::{fs, task::block_in_place};
+use sea_orm::{ConnectOptions, ConnectionTrait, DatabaseConnection};
+use tokio::fs;
 
 use super::{
     key_file::{delete_key_file, get_or_create_key_file},
@@ -14,60 +14,53 @@ const PRAGMA_KEY: &str = "key";
 
 pub struct Database {
     pub name: String,
-    connection: Connection,
+    path: PathBuf,
+    connection: DatabaseConnection,
 }
 
 impl Database {
-    fn new(name: String, connection: Connection) -> Self {
-        Database { name, connection }
+    fn new(name: String, path: PathBuf, connection: DatabaseConnection) -> Self {
+        Database { name, path, connection }
     }
 
     pub async fn open<K: PlatformEncryptionKey, U: PlatformUtilities>(name: impl Into<String>) -> Result<Self> {
         let name = name.into();
         // Get path to database, stored as "<storage_path>/<name>.db"
         let path = U::storage_path()?.join(format!("{}.db", &name));
+        // Open database connection and set database key
+        let connection_options = ConnectOptions::new(format!("sqlite://{}?mode=rwc", path.to_str().unwrap()));
+        let connection = sea_orm::Database::connect(connection_options).await?;
+
         // Get database key of the correct length including a salt, stored in encrypted file.
         let key_bytes = get_or_create_key_file::<K, U>(&name, SqlCipherKey::size_with_salt()).await?;
         let key = SqlCipherKey::try_from(key_bytes.as_slice())?;
-        // Open database connection
-        let connection = block_in_place(|| Connection::open(path))?;
-
         // Set database password using PRAGMA statement
-        block_in_place(|| connection.pragma_update::<String>(None, PRAGMA_KEY, key.into()))?;
+        connection
+            .execute_unprepared(&format!(r#"PRAGMA {} = "{}";"#, PRAGMA_KEY, String::from(key)))
+            .await?;
 
-        Ok(Self::new(name, connection))
+        Ok(Self::new(name, path, connection))
     }
 
     /// If the database could not be closed for some reason, this will return
     /// another instance of [`Database`] as the first entry of the Result error tuple.
     /// Closing and deleting the database may then be tried at another point in time.
-    pub async fn close_and_delete<U: PlatformUtilities>(
-        self,
-    ) -> std::result::Result<(), (Option<Self>, anyhow::Error)> {
-        // Get the path from the database connection, assume it has one.
-        let path = PathBuf::from_str(self.connection.path().unwrap()).unwrap();
-
-        // Close the database connection, return a new Database instance if we could not close.
-        let result = block_in_place(|| self.connection.close());
-        if result.is_err() {
-            return result
-                .map_err(|(connection, error)| (Some(Self::new(self.name, connection)), anyhow::Error::new(error)));
-        }
+    pub async fn close_and_delete<U: PlatformUtilities>(self) -> Result<()> {
+        // Close the database connection
+        self.connection.close().await?;
 
         // Remove the database file and ignore any errors.
-        _ = fs::remove_file(&path).await;
+        _ = fs::remove_file(&self.path).await;
 
         // Delete the password file and remap errors (note that deletion errors will be ignored).
-        delete_key_file::<U>(&self.name).await.map_err(|e| (None, e))?;
-
-        Ok(())
+        delete_key_file::<U>(&self.name).await
     }
 }
 
 #[cfg(test)]
 mod tests {
     use platform_support::{hw_keystore::software::SoftwareEncryptionKey, utils::software::SoftwareUtilities};
-    use rusqlite::params;
+    use sea_orm::{DatabaseBackend, DbBackend, Statement, Value};
 
     use super::*;
 
@@ -79,13 +72,14 @@ mod tests {
         Ok(())
     }
 
+    #[derive(Debug, PartialEq, Eq)]
     struct Person {
         id: i32,
         name: String,
         data: Option<Vec<u8>>,
     }
 
-    #[tokio::test(flavor = "multi_thread")]
+    #[tokio::test]
     async fn test_database() {
         let db_name = "test_db";
 
@@ -99,14 +93,14 @@ mod tests {
 
         // Create a table for our [Person] model.
         db.connection
-            .execute(
+            .execute_unprepared(
                 "CREATE TABLE person (
                     id    INTEGER PRIMARY KEY,
                     name  TEXT NOT NULL,
                     data  BLOB
                 )",
-                [],
             )
+            .await
             .expect("Could not create table");
 
         // Create and insert our test Person.
@@ -116,51 +110,43 @@ mod tests {
             data: None,
         };
         db.connection
-            .execute(
-                "INSERT INTO person (id, name, data) VALUES (?1, ?2, ?3)",
-                params![&me.id, &me.name, &me.data],
-            )
+            .execute(Statement::from_sql_and_values(
+                DbBackend::Sqlite,
+                "INSERT INTO person (id, name, data) VALUES ($1, $2, $3)",
+                [
+                    me.id.into(),
+                    me.name.clone().into(),
+                    Value::Bytes(me.data.clone().map(Box::new)),
+                ],
+            ))
+            .await
             .expect("Could not insert person");
 
-        // Start a new context, because the prepared statement below borrows the connection.
-        // Calling the close_and_delete() method consumes the Database instance, which owns
-        // the connection, meaning there can be no more borrows present. The context limits
-        // the lifetime of the statement appropriately.
-        {
-            // Query our person table for any [Person]s.
-            let mut stmt = db
-                .connection
-                .prepare("SELECT id, name, data FROM person")
-                .expect("Could not execute select statement");
+        // Query our person table for any [Person]s.
+        let person_query_results = db
+            .connection
+            .query_all(Statement::from_string(
+                DatabaseBackend::Sqlite,
+                "SELECT id, name, data FROM person".to_string(),
+            ))
+            .await
+            .expect("Could not execute select statement");
 
-            // Map our query results back to our [Person] model.
-            let person_iter = stmt
-                .query_map([], |row| {
-                    Ok(Person {
-                        id: row.get(0)?,
-                        name: row.get(1)?,
-                        data: row.get(2)?,
-                    })
-                })
-                .expect("Could not create iterator");
+        // Map our query results back to our [Person] model.
+        let persons: Vec<Person> = person_query_results
+            .into_iter()
+            .map(|row| Person {
+                id: row.try_get("", "id").unwrap(),
+                name: row.try_get("", "name").unwrap(),
+                data: row.try_get("", "data").unwrap(),
+            })
+            .collect();
 
-            // Verify our test [Person] was correctly inserted.
-            let mut person_count = 0;
-            for person in person_iter {
-                let result = person.unwrap();
-                assert_eq!(1337, result.id);
-                assert_eq!("Willeke", result.name);
-                assert_eq!(None, result.data);
-                person_count += 1;
-            }
-
-            // Verify we really checked our test person (and did not loop over empty iterator).
-            assert_eq!(person_count, 1);
-        }
+        // Verify our test [Person] was correctly inserted.
+        assert_eq!(persons, [me]);
 
         db.close_and_delete::<SoftwareUtilities>()
             .await
-            .map_err(|(_, e)| e)
             .expect("Could not close and delete database");
     }
 }
