dcda2c305182ac0de2ba9174375a633503a7a59b
Rob <edi@minbzk.nl>
2023-05-15 11:43:36 +0000

    Improve Landscape Support
    * Improve Landscape Support #3
    
    Add Landscape support (make content scrollable)
    on the DigidPinPage.
    
    * Improve Landscape Support #2
    
    Add Landscape support (make content scrollable)
    on the WalletPersonalizeDigidErrorPage.
    
    * Improve Landscape Support
    
    Add Landscape support (make content scrollable)
    on the WalletPersonalizeIntroPage.

--- wallet_app/lib/src/feature/wallet/personalize/page/wallet_personalize_digid_error_page.dart
+++ wallet_app/lib/src/feature/wallet/personalize/page/wallet_personalize_digid_error_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../common/widget/button/text_icon_button.dart';
+import '../../../common/widget/sliver_sized_box.dart';
 
 const _kDigidLogoPath = 'assets/images/digid_logo.png';
 
@@ -18,59 +19,79 @@ class WalletPersonalizeDigidErrorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final locale = AppLocalizations.of(context);
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 12),
-          SizedBox(
-            height: 105,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.secondaryContainer,
-                borderRadius: BorderRadius.circular(8),
+    return Scrollbar(
+      thumbVisibility: true,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: CustomScrollView(
+          slivers: [
+            const SliverSizedBox(height: 36),
+            SliverSizedBox(
+              height: 105,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondaryContainer,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                alignment: Alignment.center,
+                child: const Text('Placeholder image'),
               ),
-              alignment: Alignment.center,
-              child: const Text('Placeholder image'),
             ),
-          ),
-          const SizedBox(height: 24),
-          Text(
-            locale.walletPersonalizeDigidErrorPageTitle,
-            textAlign: TextAlign.start,
-            style: Theme.of(context).textTheme.displaySmall,
-          ),
-          const SizedBox(height: 8),
-          Text(
-            locale.walletPersonalizeDigidErrorPageDescription,
-            textAlign: TextAlign.start,
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          const Spacer(),
-          ElevatedButton(
-            onPressed: onRetryPressed,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(_kDigidLogoPath),
-                const SizedBox(width: 12),
-                Text(locale.walletPersonalizeDigidErrorPageLoginWithDigidCta),
-              ],
-            ),
-          ),
-          const SizedBox(height: 8),
-          Center(
-            child: TextIconButton(
-              onPressed: onHelpPressed,
+            const SliverSizedBox(height: 24),
+            SliverToBoxAdapter(
               child: Text(
-                locale.walletPersonalizeDigidErrorPageNoDigidCta,
+                locale.walletPersonalizeDigidErrorPageTitle,
+                textAlign: TextAlign.start,
+                style: Theme.of(context).textTheme.displaySmall,
               ),
             ),
-          ),
-        ],
+            const SliverSizedBox(height: 8),
+            SliverToBoxAdapter(
+              child: Text(
+                locale.walletPersonalizeDigidErrorPageDescription,
+                textAlign: TextAlign.start,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            const SliverSizedBox(height: 32),
+            SliverFillRemaining(
+              hasScrollBody: false,
+              fillOverscroll: true,
+              child: _buildBottomSection(context),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildBottomSection(BuildContext context) {
+    final locale = AppLocalizations.of(context);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        ElevatedButton(
+          onPressed: onRetryPressed,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(_kDigidLogoPath),
+              const SizedBox(width: 12),
+              Text(locale.walletPersonalizeDigidErrorPageLoginWithDigidCta),
+            ],
+          ),
+        ),
+        const SizedBox(height: 8),
+        Center(
+          child: TextIconButton(
+            onPressed: onHelpPressed,
+            child: Text(
+              locale.walletPersonalizeDigidErrorPageNoDigidCta,
+            ),
+          ),
+        ),
+        const SizedBox(height: 24),
+      ],
     );
   }
 }
