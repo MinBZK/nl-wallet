595a06c19a2c3cab5a1b272a3d2b7a8e04bd1620
Ruud Klaver <ruud@pantsforcats.nl>
2023-05-19 18:42:17 +0200

    Implement tracing output for Android
    For printing log statements on Android, we use just the low-level access
    provided by the "android_logger" statement. Alternatively, we could use
    the logging implementation that this crate provides (which provides
    output for using the "log" crate) and configure "tracing" to output
    using that (using this crate's "log" feature), but then we lose
    consistent formatting between iOS and Android.

--- wallet_core/flutter_api/src/logging.rs
+++ wallet_core/flutter_api/src/logging.rs
@@ -31,9 +31,12 @@ fn init_tracing_subscriber() {
     #[cfg(debug_assertions)]
     let builder = builder.with_max_level(Level::DEBUG);
 
-    // For iOS, disable ANSI colours and use a custom log writer instead of stdout.
+    // For Android and iOS, disable ANSI colours and use a custom log writer instead of stdout.
+    #[cfg(target_os = "android")]
+    let builder = builder.with_ansi(false).with_writer(android::WriterMaker::default());
+
     #[cfg(target_os = "ios")]
-    let builder = builder.with_ansi(false).with_writer(ios::OsLogWriter::default);
+    let builder = builder.with_ansi(false).with_writer(ios::LogWriter::default);
 
     // Set the result of the builder as global default subscriber.
     builder.init();
@@ -71,30 +74,111 @@ fn init_panic_logger() {
     }));
 }
 
+#[cfg(target_os = "android")]
+mod android {
+    use std::{ffi::CStr, fmt::Write, io};
+
+    use android_logger::{LogId, PlatformLogWriter};
+    use cstr::cstr;
+    use tracing::{Level, Metadata};
+    use tracing_subscriber::fmt::MakeWriter;
+
+    /// Print "core" as the tag in Android logs, to differentiate from Flutter messages.
+    const TAG: &CStr = cstr!("core");
+    const DEFAULT_TRACE_LEVEL: &Level = &Level::INFO;
+
+    /// We need something that implements the [`WriterMaker`] trait in order to have different
+    /// [`LogWriter`] instances per debug level.
+    #[derive(Default)]
+    pub struct WriterMaker();
+
+    impl WriterMaker {
+        fn writer(&self, level: &Level) -> LogWriter {
+            let level = match *level {
+                Level::TRACE => log::Level::Trace,
+                Level::DEBUG => log::Level::Debug,
+                Level::INFO => log::Level::Info,
+                Level::WARN => log::Level::Warn,
+                Level::ERROR => log::Level::Error,
+            };
+
+            LogWriter(PlatformLogWriter::new(Some(LogId::Main), level, TAG))
+        }
+    }
+
+    impl<'a> MakeWriter<'a> for WriterMaker {
+        type Writer = LogWriter<'a>;
+
+        fn make_writer(&'a self) -> Self::Writer {
+            // This method may never get called (as there should normally be metadata present),
+            // but if it does we should just pick a debug level ourselves.
+            self.writer(DEFAULT_TRACE_LEVEL)
+        }
+
+        fn make_writer_for(&'a self, meta: &Metadata<'_>) -> Self::Writer {
+            self.writer(meta.level())
+        }
+    }
+
+    /// This wraps an instance of [`PlatformLogWriter`], which is a low-level type
+    /// contained in the [`android_logger`] crate. We use this so we can more directly
+    /// write to the Android logger, instead of using the higher-level components offered
+    /// by [`android_logger`].
+    ///
+    /// Unfortunately, [`PlatformLogWriter`] implements the [`std::fmt::Write`] trait,
+    /// instead of the [`std::io::Write`] trait that is required, so have have to convert
+    /// between the two. In practice this means converting the provided by slices back to
+    /// strings.
+    pub struct LogWriter<'a>(PlatformLogWriter<'a>);
+
+    impl io::Write for LogWriter<'_> {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            self.0
+                .write_str(&String::from_utf8_lossy(buf))
+                .map(|_| buf.len())
+                // Convert any resulting error to io::Error.
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+            // Event though we implement flush() below, this does not seem to get called!
+            // For that reason we just flush after every write, so the tracing statements
+            // actually show up in the Android logs.
+            self.0.flush();
+
+            Ok(buf.len())
+        }
+
+        fn flush(&mut self) -> std::io::Result<()> {
+            self.0.flush();
+
+            Ok(())
+        }
+    }
+}
+
 #[cfg(target_os = "ios")]
 mod ios {
-    use std::io::Write;
+    use std::io::{Result, Write};
 
     use oslog::OsLog;
 
     /// This wraps a OSLog instance so it can implement [`Write`].
-    pub struct OsLogWriter(OsLog);
+    pub struct LogWriter(OsLog);
 
-    impl From<OsLog> for OsLogWriter {
+    impl From<OsLog> for LogWriter {
         fn from(value: OsLog) -> Self {
-            OsLogWriter(value)
+            LogWriter(value)
         }
     }
 
     /// The default instance simply wraps the global OSLog.
-    impl Default for OsLogWriter {
+    impl Default for LogWriter {
         fn default() -> Self {
-            OsLogWriter(OsLog::global())
+            LogWriter(OsLog::global())
         }
     }
 
-    impl Write for OsLogWriter {
-        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+    impl Write for LogWriter {
+        fn write(&mut self, buf: &[u8]) -> Result<usize> {
             // Do not actually use differentiated logging levels of OSLog, since the Flutter
             // console only outputs the default level or higher (i.e. not info or debug).
             self.0.default(String::from_utf8_lossy(buf).as_ref());
