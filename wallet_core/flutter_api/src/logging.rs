b576d4289481b22cf00ebaeb4f4073b9b3eabc2f
Ruud Klaver <ruud@pantsforcats.nl>
2023-05-19 12:37:23 +0200

    Implement panic backtrace logging in debug builds
    Set a custom panic hook for debug builds and log a full backtrace using
    tracing from it. Note that this backtrace is split over multiple lines
    in order to circumvent the OSLog 1024 character limit on iOS.

--- wallet_core/flutter_api/src/logging.rs
+++ wallet_core/flutter_api/src/logging.rs
@@ -8,23 +8,67 @@ static LOGGING: Once = Once::new();
 pub fn init_logging() {
     // Make sure this initializer can be called multiple times, but executes only once.
     LOGGING.call_once(|| {
-        let builder = SubscriberBuilder::default();
+        // Set up a subscriber to log to the relevant output for the platform.
+        init_tracing_subscriber();
 
-        // For release builds, set the log level to WARN and remove timestamps.
-        #[cfg(not(debug_assertions))]
-        let builder = builder.with_max_level(Level::WARN).without_time();
-
-        // For debug builds, set the log level to DEBUG.
+        // Set a custom panic handler for debug builds. For release builds
+        // we can rely on Flutter logging panics as uncaught exceptions.
+        // As init_tracing_subscriber() might panic, we want that to be caught by
+        // the default handler, as we cannot log anything yet anyway.
         #[cfg(debug_assertions)]
-        let builder = builder.with_max_level(Level::DEBUG);
+        init_panic_logger();
+    });
+}
 
-        // For iOS, disable ANSI colours and use a custom log writer instead of stdout.
-        #[cfg(target_os = "ios")]
-        let builder = builder.with_ansi(false).with_writer(ios::OsLogWriter::default);
+fn init_tracing_subscriber() {
+    let builder = SubscriberBuilder::default();
 
-        // Set the result of the builder as global default subscriber.
-        builder.init();
-    })
+    // For release builds, set the log level to WARN and remove timestamps.
+    #[cfg(not(debug_assertions))]
+    let builder = builder.with_max_level(Level::WARN).without_time();
+
+    // For debug builds, set the log level to DEBUG.
+    #[cfg(debug_assertions)]
+    let builder = builder.with_max_level(Level::DEBUG);
+
+    // For iOS, disable ANSI colours and use a custom log writer instead of stdout.
+    #[cfg(target_os = "ios")]
+    let builder = builder.with_ansi(false).with_writer(ios::OsLogWriter::default);
+
+    // Set the result of the builder as global default subscriber.
+    builder.init();
+}
+
+#[cfg(debug_assertions)]
+fn init_panic_logger() {
+    use std::{backtrace::Backtrace, panic};
+
+    use tracing::error;
+
+    panic::set_hook(Box::new(|panic_info| {
+        let backtrace = Backtrace::force_capture();
+
+        // The payload may either be a reference to a [`String`] or a `&'static str`.
+        let payload = panic_info.payload();
+        let message = match (payload.downcast_ref::<String>(), payload.downcast_ref::<&'static str>()) {
+            (Some(s), _) => Some(s.as_ref()),
+            (_, Some(s)) => Some(*s),
+            (_, _) => None,
+        };
+
+        // Log the panic message and backtrace, each on separate lines
+        // because OSLog on iOS has a 1024 character limit.
+        // See: https://stackoverflow.com/questions/39584707/nslog-on-devices-in-ios-10-xcode-8-seems-to-truncate-why/40283623#40283623
+        //
+        // Note that we need to use string formatting to prevent
+        // the [`error!`] macro from printing the variable name.
+        error!("Panic occurred: {}", message.unwrap_or("UNKNOWN"));
+        backtrace
+            .to_string()
+            .split('\n')
+            .filter(|backtrace_line| !backtrace_line.is_empty())
+            .for_each(|backtrace_line| error!("{}", backtrace_line));
+    }));
 }
 
 #[cfg(target_os = "ios")]
