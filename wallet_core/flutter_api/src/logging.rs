977a170b1795fd786ecbbf47fcafee931dc54980
Ruud Klaver <ruud@pantsforcats.nl>
2023-05-22 13:11:09 +0200

    Implement different tracing error levels for iOS
    The effect of this is that on iOS error logs will be marked as "fault"
    and warning logs will be marked as "error".

--- wallet_core/flutter_api/src/logging.rs
+++ wallet_core/flutter_api/src/logging.rs
@@ -36,7 +36,7 @@ fn init_tracing_subscriber() {
     let builder = builder.with_ansi(false).with_writer(android::WriterMaker::default());
 
     #[cfg(target_os = "ios")]
-    let builder = builder.with_ansi(false).with_writer(ios::LogWriter::default);
+    let builder = builder.with_ansi(false).with_writer(ios::WriterMaker::default());
 
     // Set the result of the builder as global default subscriber.
     builder.init();
@@ -160,28 +160,73 @@ mod ios {
     use std::io::{Result, Write};
 
     use oslog::OsLog;
+    use tracing::{Level, Metadata};
+    use tracing_subscriber::fmt::MakeWriter;
 
-    /// This wraps a OSLog instance so it can implement [`Write`].
-    pub struct LogWriter(OsLog);
+    #[derive(Default)]
+    pub struct WriterMaker();
 
-    impl From<OsLog> for LogWriter {
-        fn from(value: OsLog) -> Self {
-            LogWriter(value)
+    /// We need something that implements the [`WriterMaker`] trait in order to have different
+    /// [`LogWriter`] instances per debug level.
+    ///
+    /// * For [`Level::ERROR`] messages, use the fault logging function.
+    /// * For [`Level::WARN`] messages. use the error logging function.
+    /// * For any message that is [`Level::INFO`] or below, use the default logging function.
+    ///   This is necessary, because Flutter will not show output on the console for the info
+    ///   and debug logging functions.
+    impl<'a> MakeWriter<'a> for WriterMaker {
+        type Writer = LogWriter;
+
+        fn make_writer(&'a self) -> Self::Writer {
+            LogWriter::default()
+        }
+
+        fn make_writer_for(&'a self, meta: &Metadata<'_>) -> Self::Writer {
+            match *meta.level() {
+                Level::ERROR => LogWriter::fault(),
+                Level::WARN => LogWriter::error(),
+                _ => LogWriter::default(),
+            }
         }
     }
 
-    /// The default instance simply wraps the global OSLog.
+    /// This wraps a OSLog instance so it can implement [`Write`].
+    pub struct LogWriter {
+        log: OsLog,
+        fun: fn(&OsLog, &str),
+    }
+
+    impl LogWriter {
+        /// Wrap the global OSLog with a specific logging function.
+        fn global(fun: fn(&OsLog, &str)) -> Self {
+            LogWriter {
+                log: OsLog::global(),
+                fun,
+            }
+        }
+
+        /// Wrap the global OSLog and the error logging function.
+        fn error() -> Self {
+            Self::global(OsLog::error)
+        }
+
+        /// Wrap the global OSLog and the fault logging function.
+        fn fault() -> Self {
+            Self::global(OsLog::fault)
+        }
+    }
+
+    /// The default instance simply wraps the global OSLog and the default logging function.
     impl Default for LogWriter {
         fn default() -> Self {
-            LogWriter(OsLog::global())
+            Self::global(OsLog::default)
         }
     }
 
     impl Write for LogWriter {
         fn write(&mut self, buf: &[u8]) -> Result<usize> {
-            // Do not actually use differentiated logging levels of OSLog, since the Flutter
-            // console only outputs the default level or higher (i.e. not info or debug).
-            self.0.default(String::from_utf8_lossy(buf).as_ref());
+            let fun = self.fun;
+            fun(&self.log, String::from_utf8_lossy(buf).as_ref());
 
             Ok(buf.len())
         }
