39848fa026551e5cb906b38929479621ec7f8952
Ruud Klaver <ruud@pantsforcats.nl>
2023-05-25 15:16:09 +0200

    Find wallet_provider config file through cargo
    When stared with "cargo run", look for the wallet_provider config file
    next to the "Cargo.toml file. Otherwise, just look for it in the current
    directory.

--- wallet_core/wallet_provider/src/settings.rs
+++ wallet_core/wallet_provider/src/settings.rs
@@ -1,3 +1,5 @@
+use std::{env, path::PathBuf};
+
 use config::{Config, ConfigError, Environment, File};
 use serde::Deserialize;
 
@@ -10,8 +12,15 @@ pub struct Settings {
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
+        // Look for a config file that is in the same directory as Cargo.toml if run through cargo,
+        // otherwise look in the current working directory.
+        let config_path = env::var("CARGO_MANIFEST_DIR")
+            .map(PathBuf::from)
+            .unwrap_or_default()
+            .join("config");
+
         Config::builder()
-            .add_source(File::with_name("wallet_provider/config.toml").required(false))
+            .add_source(File::from(config_path).required(false))
             .add_source(Environment::with_prefix("wallet_provider"))
             .build()?
             .try_deserialize()
