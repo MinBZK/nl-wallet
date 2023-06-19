2767a2d341812b865791f0a24c1c2216ce5643b8
Ruud Klaver <ruud@pantsforcats.nl>
2023-06-19 12:12:34 +0200

    Implement Debug for Configuration structs
    Derive `Debug` trait for `Configuration` struct in wallet core. Manually
    implement `Debug` for `AccountServerConfiguration`.

--- wallet_core/wallet/src/config.rs
+++ wallet_core/wallet/src/config.rs
@@ -1,9 +1,12 @@
+use std::fmt::Debug;
+
 use base64::{engine::general_purpose::STANDARD, Engine};
 use once_cell::sync::Lazy;
 use url::Url;
 
 use wallet_common::account::jwt::EcdsaDecodingKey;
 
+#[derive(Debug)]
 pub struct Configuration {
     pub account_server: AccountServerConfiguration,
 }
@@ -11,6 +14,14 @@ pub struct Configuration {
 pub struct AccountServerConfiguration {
     pub base_url: Url,
     pub public_key: EcdsaDecodingKey,
+}
+
+impl Debug for AccountServerConfiguration {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("AccountServerConfiguration")
+            .field("base_url", &self.base_url)
+            .finish_non_exhaustive()
+    }
 }
 
 pub static CONFIGURATION: Lazy<Configuration> = Lazy::new(|| Configuration {
