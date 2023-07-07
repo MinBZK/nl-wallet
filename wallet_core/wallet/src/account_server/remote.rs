97db473becbe10cfa1309255e7c4f61eccf660d4
Ruud Klaver <ruud@pantsforcats.nl>
2023-07-07 14:10:38 +0200

    Add tests for RemoteAccountServerClient
    Strictly speaking these are not unit tests since they work with mocking
    actual HTTP server responses. The goal of these tests is to validate
    that HTTP error responses get converted to the correct error variants.

--- wallet_core/wallet/src/account_server/remote.rs
+++ wallet_core/wallet/src/account_server/remote.rs
@@ -106,3 +106,154 @@ impl AccountServerClient for RemoteAccountServerClient {
         Ok(certificate)
     }
 }
+
+#[cfg(test)]
+/// Ceci n'est pas une unit test.
+///
+/// This test sets up a mock HTTP server and by definition also tests the `reqwest` dependency.
+/// Its goal is mostly to validate that HTTP error responses get converted to the right variant
+/// of `RemoteAccountServerClient` and `AccountServerResponseError`.
+mod tests {
+    use std::collections::HashMap;
+
+    use reqwest::StatusCode;
+    use serde::{Deserialize, Serialize};
+    use wiremock::{
+        matchers::{method, path},
+        Mock, MockServer, ResponseTemplate,
+    };
+
+    use wallet_common::account::messages::errors::{DataValue, ErrorType};
+
+    use super::*;
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct ExampleBody {
+        pub foo: String,
+        pub bar: u64,
+    }
+
+    async fn create_mock_server_and_client() -> (MockServer, RemoteAccountServerClient) {
+        let server = MockServer::start().await;
+        let client =
+            RemoteAccountServerClient::new(Url::parse(&server.uri()).expect("Could not parse mock server URI"));
+
+        (server, client)
+    }
+
+    async fn post_example_request(
+        client: &RemoteAccountServerClient,
+        path: &str,
+    ) -> Result<ExampleBody, AccountServerClientError> {
+        let request = client
+            .client
+            .post(client.url(path).expect("Could not build URL"))
+            .build()
+            .expect("Could not create request");
+
+        client.send_json_request::<ExampleBody>(request).await
+    }
+
+    #[tokio::test]
+    async fn test_remote_account_server_client_send_json_request_ok() {
+        let (server, client) = create_mock_server_and_client().await;
+
+        Mock::given(method("POST"))
+            .and(path("/foobar"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(ExampleBody {
+                foo: "blah".to_string(),
+                bar: 1234,
+            }))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let body = post_example_request(&client, "foobar")
+            .await
+            .expect("Could not get succesful response from server");
+
+        assert_eq!(body.foo, "blah");
+        assert_eq!(body.bar, 1234);
+    }
+
+    #[tokio::test]
+    async fn test_remote_account_server_client_send_json_request_error_status() {
+        let (server, client) = create_mock_server_and_client().await;
+
+        Mock::given(method("POST"))
+            .and(path("/foobar_404"))
+            .respond_with(ResponseTemplate::new(404))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let error = post_example_request(&client, "foobar_404")
+            .await
+            .expect_err("No error received from server");
+
+        assert!(matches!(
+            error,
+            AccountServerClientError::Response(AccountServerResponseError::Status(StatusCode::NOT_FOUND))
+        ))
+    }
+
+    #[tokio::test]
+    async fn test_remote_account_server_client_send_json_request_error_text() {
+        let (server, client) = create_mock_server_and_client().await;
+
+        Mock::given(method("POST"))
+            .and(path("/foobar_502"))
+            .respond_with(ResponseTemplate::new(502).set_body_string("Your gateway is bad and you should feel bad!"))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let error = post_example_request(&client, "foobar_502")
+            .await
+            .expect_err("No error received from server");
+
+        assert!(matches!(
+            error,
+            AccountServerClientError::Response(AccountServerResponseError::Text(StatusCode::BAD_GATEWAY, body))
+       if body == "Your gateway is bad and you should feel bad!"))
+    }
+
+    #[tokio::test]
+    async fn test_remote_account_server_client_send_json_request_error_data() {
+        let (server, client) = create_mock_server_and_client().await;
+
+        Mock::given(method("POST"))
+            .and(path("/foobar_400"))
+            .respond_with(ResponseTemplate::new(400).set_body_json(ErrorData {
+                typ: ErrorType::ChallengeValidation,
+                title: "Error title".to_string(),
+                data: Some(HashMap::from([
+                    ("foo".to_string(), DataValue::String("bar".to_string())),
+                    ("bleh".to_string(), DataValue::String("blah".to_string())),
+                ])),
+            }))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let error = post_example_request(&client, "foobar_400")
+            .await
+            .expect_err("No error received from server");
+
+        if let AccountServerClientError::Response(AccountServerResponseError::Data(StatusCode::BAD_REQUEST, data)) =
+            error
+        {
+            assert!(matches!(data.typ, ErrorType::ChallengeValidation));
+            assert_eq!(data.title, "Error title");
+
+            if let Some(data) = data.data {
+                assert!(matches!(data.get("foo"), Some(DataValue::String(string)) if string == "bar"));
+                assert!(matches!(data.get("bleh"), Some(DataValue::String(string)) if string == "blah"));
+            } else {
+                panic!("Error has no additional data")
+            }
+        } else {
+            panic!("No error data received")
+        }
+    }
+}
