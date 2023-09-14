8d370484f3ed4fc19d5fc0b5485433700b1144ed
peterpaul <pp.kleinhaneveld@gmail.com>
2023-09-14 13:53:15 +0200

    Add more attributes to mock

--- wallet_core/pid_issuer/src/app.rs
+++ wallet_core/pid_issuer/src/app.rs
@@ -165,7 +165,44 @@ pub mod mock {
 
     pub struct MockAttributesLookup;
 
+    const PID_FAMILY_NAME: &str = "family_name";
+    const PID_GIVEN_NAME: &str = "given_name";
+    const PID_BIRTH_DATE: &str = "birth_date";
+    const PID_AGE_OVER_18: &str = "age_over_18";
+    const PID_AGE_OVER_NN: &str = "age_over_NN";
+    const PID_AGE_IN_YEARS: &str = "age_in_years";
+    const PID_AGE_BIRTH_YEAR: &str = "age_birth_year";
+    const PID_UNIQUE_ID: &str = "unique_id";
+    const PID_FAMILY_NAME_BIRTH: &str = "family_name_birth";
+    const PID_GIVEN_NAME_BIRTH: &str = "given_name_birth";
+    const PID_BIRTH_PLACE: &str = "birth_place";
+    const PID_BIRTH_COUNTRY: &str = "birth_country";
+    const PID_BIRTH_STATE: &str = "birth_state";
+    const PID_BIRTH_CITY: &str = "birth_city";
+    const PID_RESIDENT_ADDRESS: &str = "resident_address";
+    const PID_RESIDENT_COUNTRY: &str = "resident_country";
+    const PID_RESIDENT_STATE: &str = "resident_state";
+    const PID_RESIDENT_CITY: &str = "resident_city";
+    const PID_RESIDENT_POSTAL_CODE: &str = "resident_postal_code";
+    const PID_RESIDENT_STREET: &str = "resident_street";
+    const PID_RESIDENT_HOUSE_NUMBER: &str = "resident_house_number";
+    const PID_GENDER: &str = "gender";
+    const PID_NATIONALITY: &str = "nationality";
+
+    const PID_ISSUANCE_DATE: &str = "issuance_date";
+    const PID_EXPIRY_DATE: &str = "expiry_date";
+    const PID_ISSUING_AUTHORITY: &str = "issuing_authority";
+    const PID_DOCUMENT_NUMBER: &str = "document_number";
+    const PID_ADMINISTRATIVE_NUMBER: &str = "administrative_number";
+    const PID_ISSUING_COUNTRY: &str = "issuing_country";
+    const PID_ISSUING_JURISDICTION: &str = "issuing_jurisdiction";
+    const PID_PORTRAIT: &str = "portrait";
+    const PID_PORTRAIT_CAPTURE_DATE: &str = "portrait_capture_date";
+
     const MOCK_PID_DOCTYPE: &str = "com.example.pid";
+    const MOCK_ADDRESS_DOCTYPE: &str = "com.example.address";
+
+    const NL_PID_NAMESPACE_PREFIX: &str = "eu.europa.ec.eudiw.pid.nl";
 
     impl AttributesLookup for MockAttributesLookup {
         fn attributes(&self, bsn: &str) -> Vec<UnsignedMdoc> {
@@ -174,15 +211,97 @@ pub mod mock {
                 copy_count: 1,
                 valid_from: Tdate::now(),
                 valid_until: Utc::now().add(Days::new(365)).into(),
+                attributes: IndexMap::from([
+                    (
+                        MOCK_PID_DOCTYPE.to_string(),
+                        vec![
+                            Entry {
+                                name: PID_GIVEN_NAME.to_string(),
+                                value: Value::Text("Willeke Liselotte".to_string()),
+                            },
+                            Entry {
+                                name: PID_FAMILY_NAME.to_string(),
+                                value: Value::Text("De Bruijn".to_string()),
+                            },
+                            Entry {
+                                name: PID_FAMILY_NAME_BIRTH.to_string(),
+                                value: Value::Text("Molenaar".to_string()),
+                            },
+                            Entry {
+                                name: PID_GENDER.to_string(),
+                                // TODO must be int according to ISO/IEC 5218
+                                value: Value::Text("Vrouw".to_string()),
+                            },
+                            Entry {
+                                name: PID_BIRTH_DATE.to_string(),
+                                value: Value::Integer(
+                                    chrono::DateTime::parse_from_rfc3339("1997-05-10")
+                                        .unwrap()
+                                        .timestamp()
+                                        .into(),
+                                ),
+                            },
+                            Entry {
+                                name: PID_AGE_OVER_18.to_string(),
+                                value: Value::Bool(true),
+                            },
+                            Entry {
+                                name: PID_BIRTH_PLACE.to_string(),
+                                value: Value::Text("Delft NL".to_string()),
+                            },
+                            Entry {
+                                name: PID_BIRTH_COUNTRY.to_string(),
+                                // TODO according to ISO 3166-1
+                                value: Value::Text("NL".to_string()),
+                            },
+                            Entry {
+                                name: PID_BIRTH_CITY.to_string(),
+                                value: Value::Text("Delft".to_string()),
+                            },
+                            Entry {
+                                name: PID_NATIONALITY.to_string(),
+                                // TODO according to ISO 3166-1
+                                value: Value::Text("NL".to_string()),
+                            },
+                        ],
+                    ),
+                    (
+                        format!("{}.{}", NL_PID_NAMESPACE_PREFIX, "1"),
+                        vec![Entry {
+                            name: "bsn".to_string(),
+                            value: Value::Text(bsn.to_string()),
+                        }],
+                    ),
+                ]),
+            };
+            let address_card = UnsignedMdoc {
+                doc_type: MOCK_ADDRESS_DOCTYPE.to_string(),
+                copy_count: 1,
+                valid_from: Tdate::now(),
+                valid_until: Utc::now().add(Days::new(365)).into(),
                 attributes: IndexMap::from([(
-                    MOCK_PID_DOCTYPE.to_string(),
-                    vec![Entry {
-                        name: "bsn".to_string(),
-                        value: Value::Text(bsn.to_string()),
-                    }],
+                    MOCK_ADDRESS_DOCTYPE.to_string(),
+                    vec![
+                        Entry {
+                            name: PID_RESIDENT_STREET.to_string(),
+                            value: Value::Text("Turfmarkt".to_string()),
+                        },
+                        Entry {
+                            name: PID_RESIDENT_HOUSE_NUMBER.to_string(),
+                            value: Value::Text("147".to_string()),
+                        },
+                        Entry {
+                            name: PID_RESIDENT_POSTAL_CODE.to_string(),
+                            value: Value::Text("2511 DP".to_string()),
+                        },
+                        Entry {
+                            name: PID_RESIDENT_CITY.to_string(),
+                            value: Value::Text("Den Haag".to_string()),
+                        },
+                    ],
                 )]),
             };
-            vec![pid]
+            vec![pid, address_card]
         }
     }
 }
