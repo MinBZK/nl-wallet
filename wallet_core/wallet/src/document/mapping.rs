d7dee00bbd16f8f13bd5b4d6ae9b57107cf600a7
Ruud Klaver <ruud@pantsforcats.nl>
2023-09-20 19:11:34 +0200

    Implement tests for mdoc to Document conversion

--- wallet_core/wallet/src/document/mapping.rs
+++ wallet_core/wallet/src/document/mapping.rs
@@ -430,3 +430,315 @@ impl TryFrom<Integer> for GenderAttributeValue {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::mem;
+
+    use assert_matches::assert_matches;
+    use chrono::{Days, Utc};
+
+    use nl_wallet_mdoc::Tdate;
+
+    use super::*;
+
+    /// This creates a minimal `UnsignedMdoc` that is valid.
+    fn create_minimal_unsigned_pid_mdoc() -> UnsignedMdoc {
+        UnsignedMdoc {
+            doc_type: "com.example.pid".to_string(),
+            copy_count: 1,
+            valid_from: Tdate::now(),
+            valid_until: (Utc::now() + Days::new(365)).into(),
+            attributes: IndexMap::from([(
+                "com.example.pid".to_string(),
+                vec![
+                    Entry {
+                        name: "bsn".to_string(),
+                        value: DataElementValue::Text("999999999".to_string()),
+                    },
+                    Entry {
+                        name: "family_name".to_string(),
+                        value: DataElementValue::Text("De Bruijn".to_string()),
+                    },
+                    Entry {
+                        name: "given_name".to_string(),
+                        value: DataElementValue::Text("Willeke Liselotte".to_string()),
+                    },
+                    Entry {
+                        name: "birth_date".to_string(),
+                        value: DataElementValue::Text("1997-05-10".to_string()),
+                    },
+                    Entry {
+                        name: "age_over_18".to_string(),
+                        value: DataElementValue::Bool(true),
+                    },
+                    Entry {
+                        name: "unique_id".to_string(),
+                        value: DataElementValue::Text("78f39496-701f-4f05-a507-5852fa898fd8".to_string()),
+                    },
+                ],
+            )]),
+        }
+    }
+
+    /// This creates a full `UnsignedMdoc` that is valid.
+    fn create_full_unsigned_pid_mdoc() -> UnsignedMdoc {
+        let mut unsigned_mdoc = create_minimal_unsigned_pid_mdoc();
+
+        unsigned_mdoc
+            .attributes
+            .get_mut("com.example.pid")
+            .unwrap()
+            .extend(vec![
+                Entry {
+                    name: "family_name_birth".to_string(),
+                    value: DataElementValue::Text("Molenaar".to_string()),
+                },
+                Entry {
+                    name: "given_name_birth".to_string(),
+                    value: DataElementValue::Text("Liselotte Willeke".to_string()),
+                },
+                Entry {
+                    name: "birth_place".to_string(),
+                    value: DataElementValue::Text("Delft".to_string()),
+                },
+                Entry {
+                    name: "birth_country".to_string(),
+                    value: DataElementValue::Text("NL".to_string()),
+                },
+                Entry {
+                    name: "birth_state".to_string(),
+                    value: DataElementValue::Text("Zuid-Holland".to_string()),
+                },
+                Entry {
+                    name: "birth_city".to_string(),
+                    value: DataElementValue::Text("Delft".to_string()),
+                },
+                Entry {
+                    name: "gender".to_string(),
+                    value: DataElementValue::Integer(2.into()),
+                },
+            ]);
+
+        unsigned_mdoc
+    }
+
+    #[test]
+    fn test_minimal_unsigned_mdoc_to_document_mapping() {
+        let unsigned_mdoc = create_minimal_unsigned_pid_mdoc();
+
+        let document = Document::try_from(unsigned_mdoc).expect("Could not convert minimal mdoc to document");
+
+        assert!(document.id.is_none());
+        assert_eq!(document.doc_type, "com.example.pid");
+        assert_eq!(
+            document.attributes.keys().cloned().collect::<Vec<_>>(),
+            vec![
+                "unique_id",
+                "given_name",
+                "family_name",
+                "birth_date",
+                "age_over_18",
+                "bsn"
+            ]
+        );
+        assert_matches!(
+            document.attributes.get("unique_id").unwrap(),
+            Attribute {
+                key_labels,
+                value: AttributeValue::String(unique_id),
+            } if key_labels == &HashMap::from([("en", "Unique identifier"), ("nl", "Unieke identificatiecode")]) &&
+                 unique_id == "78f39496-701f-4f05-a507-5852fa898fd8"
+        );
+        assert_matches!(
+            document.attributes.get("given_name").unwrap(),
+            Attribute {
+                key_labels: _,
+                value: AttributeValue::String(given_name),
+            } if given_name == "Willeke Liselotte"
+        );
+        assert_matches!(
+            document.attributes.get("family_name").unwrap(),
+            Attribute {
+                key_labels: _,
+                value: AttributeValue::String(family_name),
+            } if family_name == "De Bruijn"
+        );
+        assert_matches!(
+            document.attributes.get("birth_date").unwrap(),
+            Attribute {
+                key_labels: _,
+                value: AttributeValue::Date(birth_date),
+            } if birth_date == &NaiveDate::parse_from_str("1997-05-10", "%Y-%m-%d").unwrap()
+        );
+        assert_matches!(
+            document.attributes.get("age_over_18").unwrap(),
+            Attribute {
+                key_labels: _,
+                value: AttributeValue::Boolean(true),
+            }
+        );
+        assert_matches!(
+            document.attributes.get("bsn").unwrap(),
+            Attribute {
+                key_labels: _,
+                value: AttributeValue::String(given_name),
+            } if given_name == "999999999"
+        );
+    }
+
+    #[test]
+    fn test_full_unsigned_mdoc_to_document_mapping() {
+        let unsigned_mdoc = create_full_unsigned_pid_mdoc();
+
+        let document = Document::try_from(unsigned_mdoc).expect("Could not convert full mdoc to document");
+
+        assert_matches!(
+            document.attributes.get("gender").unwrap(),
+            Attribute {
+                key_labels: _,
+                value: AttributeValue::Gender(GenderAttributeValue::Female),
+            }
+        );
+    }
+
+    #[test]
+    fn test_unsigned_mdoc_to_document_mapping_doc_type_error() {
+        // Test changing the doc_type.
+        let mut unsigned_mdoc = create_minimal_unsigned_pid_mdoc();
+        unsigned_mdoc.doc_type = "com.example.foobar".to_string();
+
+        let result = Document::try_from(unsigned_mdoc);
+
+        assert_matches!(
+            result,
+            Err(DocumentMdocError::UnknownDocType { doc_type }) if doc_type == "com.example.foobar"
+        );
+    }
+
+    #[test]
+    fn test_unsigned_mdoc_to_document_mapping_missing_attribute_error() {
+        // Test removing the "unique_id" attribute.
+        let mut unsigned_mdoc = create_minimal_unsigned_pid_mdoc();
+        unsigned_mdoc.attributes.get_mut("com.example.pid").unwrap().pop();
+
+        let result = Document::try_from(unsigned_mdoc);
+
+        assert_matches!(
+            result,
+            Err(DocumentMdocError::MissingAttribute {
+                doc_type,
+                name_space,
+                name
+            }) if doc_type == "com.example.pid" && name_space == "com.example.pid" && name == "unique_id"
+        );
+
+        // Test removing the "gender" attribute, conversion should still succeed.
+        unsigned_mdoc = create_full_unsigned_pid_mdoc();
+        unsigned_mdoc.attributes.get_mut("com.example.pid").unwrap().pop();
+
+        _ = Document::try_from(unsigned_mdoc).expect("Could not convert full mdoc to document");
+    }
+
+    #[test]
+    fn test_unsigned_mdoc_to_document_mapping_attribute_value_type_mismatch_error() {
+        // Test changing the "bsn" attribute to an integer.
+        let mut unsigned_mdoc = create_minimal_unsigned_pid_mdoc();
+        _ = mem::replace(
+            &mut unsigned_mdoc.attributes.get_mut("com.example.pid").unwrap()[0],
+            Entry {
+                name: "bsn".to_string(),
+                value: DataElementValue::Integer(1234.into()),
+            },
+        );
+
+        let result = Document::try_from(unsigned_mdoc);
+
+        assert_matches!(
+            result,
+            Err(DocumentMdocError::AttributeValueTypeMismatch {
+                doc_type,
+                name_space,
+                name,
+                expected_type: AttributeValueType::String,
+                value,
+            }) if doc_type == "com.example.pid" && name_space == "com.example.pid" &&
+                  name == "bsn" && value == DataElementValue::Integer(1234.into())
+        );
+
+        // Test changing the "birth_date" attribute to an invalid date.
+        let mut unsigned_mdoc = create_minimal_unsigned_pid_mdoc();
+        _ = mem::replace(
+            &mut unsigned_mdoc.attributes.get_mut("com.example.pid").unwrap()[3],
+            Entry {
+                name: "birth_date".to_string(),
+                value: DataElementValue::Text("1997-04-31".to_string()),
+            },
+        );
+
+        let result = Document::try_from(unsigned_mdoc);
+
+        assert_matches!(
+            result,
+            Err(DocumentMdocError::AttributeValueTypeMismatch {
+                doc_type,
+                name_space,
+                name,
+                expected_type: AttributeValueType::Date,
+                value,
+            }) if doc_type == "com.example.pid" && name_space == "com.example.pid" &&
+                  name == "birth_date" && value == DataElementValue::Text("1997-04-31".to_string())
+        );
+
+        // Test changing the "gender" attribute to an invalid value.
+        let mut unsigned_mdoc = create_full_unsigned_pid_mdoc();
+        _ = mem::replace(
+            &mut unsigned_mdoc.attributes.get_mut("com.example.pid").unwrap()[12],
+            Entry {
+                name: "gender".to_string(),
+                value: DataElementValue::Integer(5.into()),
+            },
+        );
+
+        let result = Document::try_from(unsigned_mdoc);
+
+        assert_matches!(
+            result,
+            Err(DocumentMdocError::AttributeValueTypeMismatch {
+                doc_type,
+                name_space,
+                name,
+                expected_type: AttributeValueType::Gender,
+                value,
+            }) if doc_type == "com.example.pid" && name_space == "com.example.pid" &&
+                  name == "gender" && value == DataElementValue::Integer(5.into())
+        );
+    }
+
+    #[test]
+    fn test_unsigned_mdoc_to_document_mapping_unknown_attribute_error() {
+        // Test adding an unknown entry.
+        let mut unsigned_mdoc = create_minimal_unsigned_pid_mdoc();
+        unsigned_mdoc
+            .attributes
+            .get_mut("com.example.pid")
+            .unwrap()
+            .push(Entry {
+                name: "foobar".to_string(),
+                value: DataElementValue::Text("Foo Bar".to_string()),
+            });
+
+        let result = Document::try_from(unsigned_mdoc);
+
+        assert_matches!(
+            result,
+            Err(DocumentMdocError::UnknownAttribute {
+                doc_type,
+                name_space,
+                name,
+                value,
+            }) if doc_type == "com.example.pid" && name_space == "com.example.pid" &&
+                  name == "foobar" && value == DataElementValue::Text("Foo Bar".to_string())
+        );
+    }
+}
