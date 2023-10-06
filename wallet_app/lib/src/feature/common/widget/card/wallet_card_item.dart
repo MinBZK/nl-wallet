81f6dd8de2fd4de89bc3682f0b5a1dc5101bc0dc
Rob <edi@minbzk.nl>
2023-10-06 07:33:30 +0000

    PVW-1262: Dashboard card hero transition
    * Implement MR Feedback
    
    * Improve card shuttle animation
    
    - Make the 'show details' cta support fade out animation.
    - Clean up the animationStatus listener with an extension
    that automatically removes the statusListener as well.
    
    * Test CardDetailScreenArgument
    
    * Restructure CardDetailScreen
    
    Change the setup of the CardDetailScreen so that it
    renders the WalletCardItem inside a Hero widget with
    the id as a tag and make it so that it renders the card
    in the loading state too (if it is provided) so that
    the hero animation can be played.
    
    Also moved the logic of the shuttle builder into the
    WalletCardItem so that it can be easily re-used whenever
    we want to apply it as some extra pizazz for the hero
    animation.
    
    * Update CardDetailBloc to expose card while loading
    
    To implement the hero animation we need to be able to
    show the card as soon as we start rendering the screen,
    the first step to achieve this is to update the bloc
    and arguments so that the initial loading state can
    provide the card, which is done in this commit.

--- wallet_app/lib/src/feature/common/widget/card/wallet_card_item.dart
+++ wallet_app/lib/src/feature/common/widget/card/wallet_card_item.dart
@@ -5,6 +5,7 @@ import '../../../../theme/dark_wallet_theme.dart';
 import '../../../../theme/light_wallet_theme.dart';
 import '../../../../util/extension/build_context_extension.dart';
 import '../animated_fade_in.dart';
+import '../animated_fade_out.dart';
 import '../svg_or_image.dart';
 import '../utility/limit_font_scaling.dart';
 import 'card_holograph.dart';
@@ -46,8 +47,8 @@ class WalletCardItem extends StatelessWidget {
   /// The holograph asset rendered behind the text
   final String? holograph;
 
-  /// Enable to fade in the 'show details' cta
-  final bool fadeInCta;
+  /// Specify how to animate the 'show details' cta on the initial build
+  final CtaAnimation? ctaAnimation;
 
   /// Callback that is triggered when the card is clicked
   ///
@@ -64,10 +65,10 @@ class WalletCardItem extends StatelessWidget {
     this.holograph,
     required this.brightness,
     this.onPressed,
-    this.fadeInCta = false,
+    this.ctaAnimation,
   }) : super(key: key);
 
-  WalletCardItem.fromCardFront({required CardFront front, this.onPressed, this.fadeInCta = false, super.key})
+  WalletCardItem.fromCardFront({required CardFront front, this.onPressed, this.ctaAnimation, super.key})
       : title = front.title,
         background = front.backgroundImage,
         logo = front.logoImage,
@@ -183,11 +184,16 @@ class WalletCardItem extends StatelessWidget {
   Widget _buildShowDetailsCta(BuildContext context) {
     if (!_showDetailsCta) return const SizedBox.shrink();
     return Positioned(
-      bottom: _kCardContentPadding,
-      left: _kCardContentPadding,
-      right: _kCardContentPadding,
-      child: fadeInCta ? const AnimatedFadeIn(child: ShowDetailsCta()) : const ShowDetailsCta(),
-    );
+        bottom: _kCardContentPadding,
+        left: _kCardContentPadding,
+        right: _kCardContentPadding,
+        child: switch (ctaAnimation) {
+          null => onPressed == null ? const SizedBox.shrink() : const ShowDetailsCta(),
+          CtaAnimation.fadeIn => const AnimatedFadeIn(child: ShowDetailsCta()),
+          CtaAnimation.fadeOut => const AnimatedFadeOut(child: ShowDetailsCta()),
+          CtaAnimation.visible => const ShowDetailsCta(),
+          CtaAnimation.invisible => const SizedBox.shrink(),
+        });
   }
 
   /// Resolve the [ThemeData] for the selected [brightness], making sure the text contrasts the provided [background]
@@ -199,4 +205,72 @@ class WalletCardItem extends StatelessWidget {
   }
 
   bool get _showDetailsCta => onPressed != null;
+
+  static Widget buildShuttleCard(
+    Animation<double> animation,
+    CardFront front, {
+    CtaAnimation ctaAnimation = CtaAnimation.visible,
+  }) {
+    final scaleTween = TweenSequence<double>(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.0, end: 1.05).chain(CurveTween(curve: Curves.easeIn)),
+          weight: 30.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.05, end: 1.05),
+          weight: 60.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.05, end: 1.0).chain(CurveTween(curve: Curves.easeInCubic)),
+          weight: 10.0,
+        ),
+      ],
+    );
+
+    final perspectiveTween = TweenSequence<double>(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 0.2).chain(CurveTween(curve: Curves.easeInCubic)),
+          weight: 20.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.2, end: 0.2),
+          weight: 65.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.2, end: 0.0).chain(CurveTween(curve: Curves.decelerate)),
+          weight: 15.0,
+        ),
+      ],
+    );
+
+    VoidCallback? onPressed = switch (ctaAnimation) {
+      CtaAnimation.fadeIn => () {},
+      CtaAnimation.fadeOut => () {},
+      CtaAnimation.visible => () {},
+      CtaAnimation.invisible => null,
+    };
+
+    return AnimatedBuilder(
+      animation: animation,
+      child: WalletCardItem.fromCardFront(
+        front: front,
+        ctaAnimation: ctaAnimation,
+        onPressed: onPressed,
+      ),
+      builder: (context, child) {
+        return Transform(
+          alignment: FractionalOffset.center,
+          transform: Matrix4.identity()
+            ..scale(scaleTween.evaluate(animation))
+            ..setEntry(3, 2, 0.001)
+            ..rotateX(perspectiveTween.evaluate(animation)),
+          child: child,
+        );
+      },
+    );
+  }
 }
+
+enum CtaAnimation { fadeIn, fadeOut, visible, invisible }
--- wallet_app/lib/src/feature/common/widget/stacked_wallet_cards.dart
+++ wallet_app/lib/src/feature/common/widget/stacked_wallet_cards.dart
@@ -29,7 +29,7 @@ class StackedWalletCards extends StatelessWidget {
             BuildContext fromHeroContext,
             BuildContext toHeroContext,
           ) =>
-              _buildShuttleCard(animation, cards[index]),
+              WalletCardItem.buildShuttleCard(animation, cards[index].front, ctaAnimation: CtaAnimation.fadeIn),
           child: GestureDetector(
             child: WalletCardItem.fromCardFront(front: cards[index].front),
             onTap: () => onCardPressed?.call(cards[index]),
@@ -38,60 +38,5 @@ class StackedWalletCards extends StatelessWidget {
       );
     });
     return Stack(children: children);
-  }
-
-  Widget _buildShuttleCard(Animation<double> animation, WalletCard card) {
-    final scaleTween = TweenSequence<double>(
-      <TweenSequenceItem<double>>[
-        TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 1.0, end: 1.05).chain(CurveTween(curve: Curves.easeIn)),
-          weight: 30.0,
-        ),
-        TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 1.05, end: 1.05),
-          weight: 60.0,
-        ),
-        TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 1.05, end: 1.0).chain(CurveTween(curve: Curves.easeInCubic)),
-          weight: 10.0,
-        ),
-      ],
-    );
-
-    final perspectiveTween = TweenSequence<double>(
-      <TweenSequenceItem<double>>[
-        TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 0.0, end: 0.2).chain(CurveTween(curve: Curves.easeInCubic)),
-          weight: 20.0,
-        ),
-        TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 0.2, end: 0.2),
-          weight: 65.0,
-        ),
-        TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 0.2, end: 0.0).chain(CurveTween(curve: Curves.decelerate)),
-          weight: 15.0,
-        ),
-      ],
-    );
-
-    return AnimatedBuilder(
-      animation: animation,
-      child: WalletCardItem.fromCardFront(
-        front: card.front,
-        fadeInCta: true,
-        onPressed: () {},
-      ),
-      builder: (context, child) {
-        return Transform(
-          alignment: FractionalOffset.center,
-          transform: Matrix4.identity()
-            ..scale(scaleTween.evaluate(animation))
-            ..setEntry(3, 2, 0.001)
-            ..rotateX(perspectiveTween.evaluate(animation)),
-          child: child,
-        );
-      },
-    );
   }
 }
