7d23fb55a669302c9a54b3abdba1af9c00f2bda4
Ruud Klaver <ruud@pantsforcats.nl>
2023-10-09 16:37:31 +0200

    Implement tests for Wallet URI recognition methods

--- wallet_core/wallet/src/wallet/uri.rs
+++ wallet_core/wallet/src/wallet/uri.rs
@@ -5,6 +5,7 @@ use crate::digid::DigidSession;
 
 use super::Wallet;
 
+#[derive(Debug)]
 pub enum RedirectUriType {
     PidIssuance,
     Unknown,
@@ -27,5 +28,51 @@ where
         }
 
         RedirectUriType::Unknown
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_matches::assert_matches;
+
+    use crate::digid::MockDigidSession;
+
+    use super::{super::tests::WalletWithMocks, *};
+
+    #[test]
+    fn test_identify_redirect_uri() {
+        // Prepare an unregistered wallet.
+        let mut wallet = WalletWithMocks::default();
+
+        // Set up two URLs to work with.
+        let digid_url = Url::parse("redirect://here").unwrap();
+        let example_url = Url::parse("https://exampl.com").unwrap();
+
+        // The wallet should recognise neither of these URLs, as there is no `DigidSession`.
+        assert_matches!(wallet.identify_redirect_uri(&digid_url), RedirectUriType::Unknown);
+        assert_matches!(wallet.identify_redirect_uri(&example_url), RedirectUriType::Unknown);
+
+        // Set up a `DigidSession` that will match only the first URL.
+        let digid_session = {
+            let mut digid_session = MockDigidSession::new();
+
+            let digid_url_clone = digid_url.clone();
+            digid_session
+                .expect_matches_received_redirect_uri()
+                .returning(move |url| url == &digid_url_clone);
+
+            digid_session
+        };
+        wallet.digid_session = digid_session.into();
+
+        // The wallet should now recognise the DigiD URL.
+        assert_matches!(wallet.identify_redirect_uri(&digid_url), RedirectUriType::PidIssuance);
+        assert_matches!(wallet.identify_redirect_uri(&example_url), RedirectUriType::Unknown);
+
+        // After clearing the `DigidSession`, neither URL should be recognised again.
+        wallet.digid_session = None;
+
+        assert_matches!(wallet.identify_redirect_uri(&digid_url), RedirectUriType::Unknown);
+        assert_matches!(wallet.identify_redirect_uri(&example_url), RedirectUriType::Unknown);
     }
 }
