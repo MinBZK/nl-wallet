f359ff0b3d98ad4842266c642f59e4359397bc84
Ruud Klaver <ruud@pantsforcats.nl>
2023-10-10 17:38:42 +0200

    Implement tests for Wallet register() method

--- wallet_core/wallet/src/wallet/registration.rs
+++ wallet_core/wallet/src/wallet/registration.rs
@@ -137,3 +137,233 @@ impl<C, S, K, A, D, P> Wallet<C, S, K, A, D, P> {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use assert_matches::assert_matches;
+    use http::StatusCode;
+    use p256::{ecdsa::SigningKey, elliptic_curve::rand_core::OsRng};
+    use wallet_common::{account::jwt::Jwt, utils};
+
+    use crate::{account_provider::AccountProviderResponseError, wallet::tests::ACCOUNT_SERVER_KEYS};
+
+    use super::{super::tests::WalletWithMocks, *};
+
+    #[tokio::test]
+    async fn test_wallet_register_success() {
+        // Prepare an unregistered wallet.
+        let mut wallet = WalletWithMocks::default();
+
+        // The wallet should report that it is currently unregistered and locked.
+        assert!(!wallet.has_registration());
+        assert!(wallet.storage.get_mut().data.is_empty());
+        assert!(wallet.is_locked());
+
+        // Have the account server respond with a random
+        // challenge when the wallet sends a request for it.
+        wallet
+            .account_provider_client
+            .expect_registration_challenge()
+            .return_once(|_| Ok(utils::random_bytes(32)));
+
+        // Have the account server respond with a valid
+        // certificate when the wallet sends a request for it.
+        let cert = wallet.valid_certificate().await;
+        let cert_response = cert.clone();
+
+        wallet
+            .account_provider_client
+            .expect_register()
+            .return_once(|_, _| Ok(cert_response));
+
+        // Register the wallet with a valid PIN.
+        wallet
+            .register("051097".to_string())
+            .await
+            .expect("Could not register wallet");
+
+        // The wallet should now report that it is registered and unlocked.
+        assert!(wallet.has_registration());
+        assert!(!wallet.is_locked());
+
+        // The registration should be stored in the database.
+        let stored_registration: RegistrationData = wallet
+            .storage
+            .get_mut()
+            .fetch_data()
+            .await
+            .unwrap()
+            .expect("Registration data not present in storage");
+        assert_eq!(stored_registration.wallet_certificate.0, cert.0);
+    }
+
+    #[tokio::test]
+    async fn test_wallet_register_error_already_registered() {
+        // Create a wallet that is registered, then try to register.
+        let mut wallet = WalletWithMocks::registered().await;
+
+        let error = wallet
+            .register("051097".to_string())
+            .await
+            .expect_err("Wallet registration should have resulted in error");
+
+        assert_matches!(error, WalletRegistrationError::AlreadyRegistered);
+        assert!(wallet.has_registration());
+    }
+
+    #[tokio::test]
+    async fn test_wallet_register_error_invalid_pin() {
+        let mut wallet = WalletWithMocks::default();
+
+        // Try to register with an insecure PIN.
+        let error = wallet
+            .register("123456".to_string())
+            .await
+            .expect_err("Wallet registration should have resulted in error");
+
+        assert_matches!(error, WalletRegistrationError::InvalidPin(_));
+        assert!(!wallet.has_registration());
+        assert!(wallet.storage.get_mut().data.is_empty());
+    }
+
+    #[tokio::test]
+    async fn test_wallet_register_error_challenge_request() {
+        let mut wallet = WalletWithMocks::default();
+
+        // Have the account server respond to the challenge request with a 500 error.
+        wallet
+            .account_provider_client
+            .expect_registration_challenge()
+            .return_once(|_| Err(AccountProviderResponseError::Status(StatusCode::INTERNAL_SERVER_ERROR).into()));
+
+        let error = wallet
+            .register("051097".to_string())
+            .await
+            .expect_err("Wallet registration should have resulted in error");
+
+        assert_matches!(error, WalletRegistrationError::ChallengeRequest(_));
+        assert!(!wallet.has_registration());
+        assert!(wallet.storage.get_mut().data.is_empty());
+    }
+
+    // TODO: Check HardwarePublicKey and Signing errors by mocking `PlatformEcdsaKey`.
+
+    #[tokio::test]
+    async fn test_wallet_register_error_registration_request() {
+        let mut wallet = WalletWithMocks::default();
+
+        wallet
+            .account_provider_client
+            .expect_registration_challenge()
+            .return_once(|_| Ok(utils::random_bytes(32)));
+
+        // Have the account server respond to the registration request with a 401 error.
+        wallet
+            .account_provider_client
+            .expect_register()
+            .return_once(|_, _| Err(AccountProviderResponseError::Status(StatusCode::UNAUTHORIZED).into()));
+
+        let error = wallet
+            .register("051097".to_string())
+            .await
+            .expect_err("Wallet registration should have resulted in error");
+
+        assert_matches!(error, WalletRegistrationError::RegistrationRequest(_));
+        assert!(!wallet.has_registration());
+        assert!(wallet.storage.get_mut().data.is_empty());
+    }
+
+    #[tokio::test]
+    async fn test_wallet_register_error_certificate_validation() {
+        let mut wallet = WalletWithMocks::default();
+
+        wallet
+            .account_provider_client
+            .expect_registration_challenge()
+            .return_once(|_| Ok(utils::random_bytes(32)));
+
+        // Have the account server sign the wallet certificate with
+        // a keyto which the certificate public key does not belong.
+        let other_key = SigningKey::random(&mut OsRng);
+        let cert = Jwt::sign(&wallet.valid_certificate_claims().await, &other_key)
+            .await
+            .unwrap();
+
+        wallet
+            .account_provider_client
+            .expect_register()
+            .return_once(|_, _| Ok(cert));
+
+        let error = wallet
+            .register("051097".to_string())
+            .await
+            .expect_err("Wallet registration should have resulted in error");
+
+        assert_matches!(error, WalletRegistrationError::CertificateValidation(_));
+        assert!(!wallet.has_registration());
+        assert!(wallet.storage.get_mut().data.is_empty());
+    }
+
+    #[tokio::test]
+    async fn test_wallet_register_error_public_key_mismatch() {
+        let mut wallet = WalletWithMocks::default();
+
+        wallet
+            .account_provider_client
+            .expect_registration_challenge()
+            .return_once(|_| Ok(utils::random_bytes(32)));
+
+        // Have the account server include a hardware public key
+        // in the wallet certificate that the wallet did not send.
+        let other_key = SigningKey::random(&mut OsRng);
+        let mut cert_claims = wallet.valid_certificate_claims().await;
+        cert_claims.hw_pubkey = (*other_key.verifying_key()).into();
+        let cert = Jwt::sign(&cert_claims, &ACCOUNT_SERVER_KEYS.certificate_signing_key)
+            .await
+            .unwrap();
+
+        wallet
+            .account_provider_client
+            .expect_register()
+            .return_once(|_, _| Ok(cert));
+
+        let error = wallet
+            .register("051097".to_string())
+            .await
+            .expect_err("Wallet registration should have resulted in error");
+
+        assert_matches!(error, WalletRegistrationError::PublicKeyMismatch);
+        assert!(!wallet.has_registration());
+        assert!(wallet.storage.get_mut().data.is_empty());
+    }
+
+    #[tokio::test]
+    async fn test_wallet_register_error_store_certificate() {
+        let mut wallet = WalletWithMocks::default();
+
+        wallet
+            .account_provider_client
+            .expect_registration_challenge()
+            .return_once(|_| Ok(utils::random_bytes(32)));
+
+        let cert = wallet.valid_certificate().await;
+
+        wallet
+            .account_provider_client
+            .expect_register()
+            .return_once(|_, _| Ok(cert));
+
+        // Have the database return an error
+        // when inserting the wallet certificate.
+        wallet.storage.get_mut().has_query_error = true;
+
+        let error = wallet
+            .register("051097".to_string())
+            .await
+            .expect_err("Wallet registration should have resulted in error");
+
+        assert_matches!(error, WalletRegistrationError::StoreCertificate(_));
+        assert!(!wallet.has_registration());
+        assert!(wallet.storage.get_mut().data.is_empty());
+    }
+}
--- wallet_core/wallet/src/wallet/tests.rs
+++ wallet_core/wallet/src/wallet/tests.rs
@@ -1,11 +1,31 @@
-use wallet_common::keys::software::SoftwareEcdsaKey;
+use once_cell::sync::Lazy;
+use p256::{ecdsa::SigningKey, elliptic_curve::rand_core::OsRng};
+
+use wallet_common::{
+    account::{
+        jwt::Jwt,
+        messages::auth::{WalletCertificate, WalletCertificateClaims},
+    },
+    keys::{software::SoftwareEcdsaKey, EcdsaKey},
+    utils,
+};
 
 use crate::{
-    account_provider::MockAccountProviderClient, config::LocalConfigurationRepository, digid::MockDigidSession,
-    pid_issuer::MockPidIssuerClient, storage::MockStorage,
+    account_provider::MockAccountProviderClient,
+    config::LocalConfigurationRepository,
+    digid::MockDigidSession,
+    pid_issuer::MockPidIssuerClient,
+    pin::key as pin_key,
+    storage::{KeyedData, MockStorage, RegistrationData, StorageState},
+    Configuration,
 };
 
 use super::{Wallet, WalletInitError};
+
+/// This contains key material that is used to generate valid account server responses.
+pub struct AccountServerKeys {
+    pub certificate_signing_key: SigningKey,
+}
 
 pub type WalletWithMocks = Wallet<
     LocalConfigurationRepository,
@@ -16,7 +36,57 @@ pub type WalletWithMocks = Wallet<
     MockPidIssuerClient,
 >;
 
+/// The account server key material, generated once for testing.
+pub static ACCOUNT_SERVER_KEYS: Lazy<AccountServerKeys> = Lazy::new(|| AccountServerKeys {
+    certificate_signing_key: SigningKey::random(&mut OsRng),
+});
+
 impl WalletWithMocks {
+    /// Creates a registered and unlocked `Wallet` with mock dependencies.
+    pub async fn registered() -> Self {
+        let mut wallet = Self::default();
+
+        // Generate registration data.
+        let registration = RegistrationData {
+            pin_salt: pin_key::new_pin_salt().into(),
+            wallet_certificate: wallet.valid_certificate().await,
+        };
+
+        // Store the registration in `Storage`, populate the field
+        // on `Wallet` and set the wallet to unlocked.
+        wallet.storage.get_mut().state = StorageState::Opened;
+        wallet.storage.get_mut().data.insert(
+            <RegistrationData as KeyedData>::KEY,
+            serde_json::to_string(&registration).unwrap(),
+        );
+        wallet.registration = registration.into();
+        wallet.lock.unlock();
+
+        wallet
+    }
+
+    /// Generates a valid certificate for the `Wallet`.
+    pub async fn valid_certificate(&self) -> WalletCertificate {
+        Jwt::sign(
+            &self.valid_certificate_claims().await,
+            &ACCOUNT_SERVER_KEYS.certificate_signing_key,
+        )
+        .await
+        .unwrap()
+    }
+
+    /// Generates valid certificate claims for the `Wallet`.
+    pub async fn valid_certificate_claims(&self) -> WalletCertificateClaims {
+        WalletCertificateClaims {
+            wallet_id: utils::random_string(32),
+            hw_pubkey: self.hw_privkey.verifying_key().await.unwrap().into(),
+            pin_pubkey_hash: utils::random_bytes(32).into(),
+            version: 0,
+            iss: "wallet_unit_test".to_string(),
+            iat: jsonwebtoken::get_current_timestamp(),
+        }
+    }
+
     /// Creates all mocks and calls `Wallet::init_registration()`.
     pub async fn init_registration_mocks() -> Result<Self, WalletInitError> {
         Self::init_registration_mocks_with_storage(MockStorage::default()).await
@@ -35,9 +105,22 @@ impl WalletWithMocks {
 }
 
 impl Default for WalletWithMocks {
+    /// Creates an unregistered `Wallet` with mock dependencies.
     fn default() -> Self {
+        let keys = Lazy::force(&ACCOUNT_SERVER_KEYS);
+
+        // Override public keys in the `Configuration`.
+        let config = {
+            let mut config = Configuration::default();
+            config.account_server.certificate_public_key = (*keys.certificate_signing_key.verifying_key()).into();
+
+            config
+        };
+
+        let config_repository = LocalConfigurationRepository::new(config);
+
         Wallet::new(
-            LocalConfigurationRepository::default(),
+            config_repository,
             MockStorage::default(),
             MockAccountProviderClient::default(),
             MockPidIssuerClient::default(),
