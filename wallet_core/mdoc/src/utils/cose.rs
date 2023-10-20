caf2c9d0bbd7422496c668f5806574b49ad4df71
peterpaul <pp.kleinhaneveld@gmail.com>
2023-10-20 07:01:18 +0000

    Define reader auth extension
    * Use `chrono` feature, as it implies `chrono_0_4`
    
    * Base OID on joint-iso-itu-t.asn1.examples
    
    * Refactor renames and remove organization.id
    
    Renames:
    - Organization.name -> display_name
    - Organization.short_name -> legal_name
    
    * Refactor remove TryFrom implementations and renames
    
    * Use chrono::Duration for RetentionPolicy
    
    * Reorder entries to cluster them better
    
    * Introduce Image struct, and extract OrganizatioId
    
    * Reorder code
    
    * Restore original
    
    * Refactor extract CustomExtension conversions to reader_auth
    
    * Restore original CertificateUsage, and introduce CertificateType
    
    CertificateType is used as configuration to create a new certificate
    
    * Rename organisation to organization
    
    * Define reader auth extension

--- wallet_core/mdoc/src/utils/cose.rs
+++ wallet_core/mdoc/src/utils/cose.rs
@@ -387,7 +387,7 @@ mod tests {
     use crate::{
         utils::{
             cose::CoseError,
-            x509::{Certificate, CertificateUsage},
+            x509::{Certificate, CertificateType, CertificateUsage},
         },
         Error,
     };
@@ -480,7 +480,7 @@ mod tests {
     async fn cose_with_certificate() {
         let (ca, ca_privkey) = Certificate::new_ca("ca.example.com").unwrap();
         let (cert, cert_privkey) =
-            Certificate::new(&ca, &ca_privkey, "cert.example.com", CertificateUsage::Mdl).unwrap();
+            Certificate::new(&ca, &ca_privkey, "cert.example.com", CertificateType::Mdl).unwrap();
 
         let payload = ToyMessage::default();
         let header = HeaderBuilder::new()
