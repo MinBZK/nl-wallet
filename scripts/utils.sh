34210cf2cabce1c3dc4c05402c90adcd6ea5aa59
Jippe Holwerda <mail@jippeholwerda.nl>
2023-11-28 16:14:33 +0100

    Better way of handling unset HOMEBREW_PREFIX

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -20,8 +20,7 @@ function is_macos() {
 
 function detect_softhsm() {
   # shellcheck disable=SC2206
-  local locations=("/usr/local/lib" ${NIX_PROFILES:-} "/usr/lib/")
-  [ -n "$HOMEBREW_PREFIX" ] && locations+=("${HOMEBREW_PREFIX}/lib")
+  local locations=("/usr/local/lib" ${NIX_PROFILES:-} "/usr/lib/" "${HOMEBREW_PREFIX:+${HOMEBREW_PREFIX}/lib}")
 
   for location in "${locations[@]}"; do
       local library_path
