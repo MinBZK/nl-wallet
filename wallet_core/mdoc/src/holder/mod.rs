8ecb632965bab11811a39ac5f86415f7c8d9e294
Ruud Klaver <ruud@pantsforcats.nl>
2023-12-21 17:45:56 +0100

    Remove unused error invariants from HolderError

--- wallet_core/mdoc/src/holder/mod.rs
+++ wallet_core/mdoc/src/holder/mod.rs
@@ -22,20 +22,14 @@ pub use mdocs::*;
 
 #[derive(thiserror::Error, Debug)]
 pub enum HolderError {
-    #[error("unsatisfiable request: DocType {0} not in wallet")]
-    UnsatisfiableRequest(DocType),
     #[error("readerAuth not present for all documents")]
     ReaderAuthMissing,
     #[error("document requests were signed by different readers")]
     ReaderAuthsInconsistent,
     #[error("no unsigned mdocs received from issuer")]
     NoUnsignedMdocs,
-    #[error("issuer not trusted for doctype {0}")]
-    UntrustedIssuer(DocType),
     #[error("certificate error: {0}")]
     CertificateError(#[from] CertificateError),
-    #[error("wrong private key type")]
-    PrivateKeyTypeMismatch { expected: String, have: String },
     #[error("request error: {0}")]
     RequestError(#[from] reqwest::Error),
     #[error("malformed Service Engagement: url missing")]
