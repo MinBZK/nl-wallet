24dcba2dcdb1f7366f0f46d0d4c477a6710853d6
Daan <2034049+daandebie@users.noreply.github.com>
2024-01-23 13:34:22 +0000

    Combine declaration and definition on a single line

--- wallet_app/lib/src/feature/organization/detail/organization_detail_screen.dart
+++ wallet_app/lib/src/feature/organization/detail/organization_detail_screen.dart
@@ -315,9 +315,7 @@ class OrganizationDetailScreen extends StatelessWidget {
   }
 
   Widget _buildInteractionRow(BuildContext context, OrganizationDetailSuccess state) {
-    String interaction;
-    interaction =
-      context.l10n.organizationDetailScreenSomeInteractions(state.organization.displayName.l10nValue(context));
+    String interaction = context.l10n.organizationDetailScreenSomeInteractions(state.organization.displayName.l10nValue(context));
     return Column(
       children: [
         const Divider(height: 1),
