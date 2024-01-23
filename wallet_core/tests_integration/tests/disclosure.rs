caeeb4d48b79bc340400f6eec06b2b686c059246
peterpaul <pp.kleinhaneveld@gmail.com>
2024-01-23 11:46:48 +0100

    Ignore unused fields

--- wallet_core/tests_integration/tests/disclosure.rs
+++ wallet_core/tests_integration/tests/disclosure.rs
@@ -264,9 +264,8 @@ async fn test_disclosure_without_pid() {
     assert_matches!(
         error,
         DisclosureError::AttributesNotAvailable {
-            reader_registration: _,
             missing_attributes: attrs,
-            shared_data_before: _,
+            ..
         } if attrs
             .iter()
             .flat_map(|attr| attr.attributes.keys().map(|k| k.to_owned()).collect::<Vec<&str>>())
