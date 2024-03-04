c2df419afe892b9711c3080b4f651c72251971ce
Rob <edi@minbzk.nl>
2024-03-04 12:16:49 +0100

    Avoid showing PinOverlay without registration
    Avoid rendering the PinOverlay on top of any content when
    the app is not (or no longer) in a registered state.

--- wallet_app/lib/src/feature/pin/pin_overlay.dart
+++ wallet_app/lib/src/feature/pin/pin_overlay.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../domain/usecase/app/check_is_app_initialized_usecase.dart';
 import '../../domain/usecase/pin/unlock_wallet_with_pin_usecase.dart';
 import 'bloc/pin_bloc.dart';
 import 'pin_screen.dart';
@@ -31,18 +32,33 @@ class PinOverlay extends StatelessWidget {
       builder: (context, snapshot) {
         final isLocked = _lockedStreamCache = snapshot.data!;
         if (isLocked) {
-          return BlocProvider<PinBloc>(
-            create: (BuildContext context) => bloc ?? PinBloc(context.read<UnlockWalletWithPinUseCase>()),
-            child: PinScreen(
-              onUnlock: (_) {
-                /// Unused, locked state is observed above, causing this widget to be replaced
-              },
-            ),
+          /// Check for initialization to make sure we don't show the
+          /// pin overlay when the app is not initialized yet/anymore.
+          return FutureBuilder(
+            future: context.read<IsWalletInitializedUseCase>().invoke(),
+            builder: (context, isInitialized) {
+              final loading = !isInitialized.hasData && !isInitialized.hasError;
+              if (loading) return child;
+              if (isInitialized.hasError) return _buildLockedState();
+              if (isInitialized.data!) return _buildLockedState();
+              return child;
+            },
           );
         } else {
           return child;
         }
       },
+    );
+  }
+
+  Widget _buildLockedState() {
+    return BlocProvider<PinBloc>(
+      create: (context) => bloc ?? PinBloc(context.read<UnlockWalletWithPinUseCase>()),
+      child: PinScreen(
+        onUnlock: (_) {
+          /// Unused, locked state is observed above, causing this widget to be replaced
+        },
+      ),
     );
   }
 }
