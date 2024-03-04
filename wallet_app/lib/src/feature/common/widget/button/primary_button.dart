dbc4019622630ff6bb6e119b6d81c0210514b033
Daan <2034049+daandebie@users.noreply.github.com>
2024-03-04 10:50:21 +0000

    PVW-2255: Improve CTA readability with font scaling
    * Fix primary button on large fonts
    
    * Update primary button to handle large font size
    
    * Update secondary button to handle large font size

--- wallet_app/lib/src/feature/common/widget/button/primary_button.dart
+++ wallet_app/lib/src/feature/common/widget/button/primary_button.dart
@@ -10,13 +10,18 @@ class PrimaryButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(Icons.arrow_forward, size: 16),
-          const SizedBox(width: 8),
-          Text(text),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.arrow_forward, size: 16),
+            const SizedBox(width: 8),
+            Flexible(
+              child: Text(text),
+            ), //text
+          ],
+        ),
       ),
     );
   }
