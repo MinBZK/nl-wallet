3d9db85b2209f8e792e35ad17b46c034e3c7bbce
Jippe Holwerda <mail@jippeholwerda.nl>
2024-03-07 14:44:42 +0000

    PVW-2041: Add test doing registration, issuance and disclosure

--- wallet_core/tests_integration/tests/common/mod.rs
+++ wallet_core/tests_integration/tests/common/mod.rs
@@ -8,7 +8,7 @@ use std::{
 use ctor::ctor;
 use indexmap::IndexMap;
 use jsonwebtoken::{Algorithm, EncodingKey, Header};
-use reqwest::Certificate;
+use reqwest::{Certificate, Client};
 use sea_orm::{Database, DatabaseConnection, EntityTrait, PaginatorTrait};
 use tokio::time;
 use url::Url;
@@ -263,6 +263,63 @@ async fn wait_for_server(base_url: Url, trust_anchors: Vec<Certificate>) {
     })
     .await
     .unwrap();
+}
+
+// Use the mock flow of the DigiD bridge to simulate a DigiD login,
+// invoking the same URLs at the DigiD bridge that would normally be invoked by the app and browser in the mock
+// flow of the DigiD bridge.
+// Note that this depends of part of the internal API of the DigiD bridge, so it may break when the bridge
+// is updated.
+pub async fn fake_digid_auth(authorization_url: &Url, digid_base_url: &Url) -> Url {
+    let client_builder = Client::builder();
+    #[cfg(feature = "disable_tls_validation")]
+    let client_builder = client_builder.danger_accept_invalid_certs(true);
+    let client = client_builder.build().unwrap();
+
+    // Avoid the DigiD/mock DigiD landing page of the DigiD bridge by preselecting the latter
+    let authorization_url = authorization_url.to_string() + "&login_hint=digid_mock";
+
+    // Start authentication by GETting the authorization URL.
+    // In the resulting HTML page, find the "RelayState" parameter which we need for the following URL.
+    let relay_state_page = do_get_as_text(&client, authorization_url).await;
+    let relay_state_line = relay_state_page
+        .lines()
+        .find(|l| l.contains("RelayState"))
+        .expect("failed to find RelayState");
+    let relay_state = find_in_text(relay_state_line, "value=\"", "\"");
+
+    // Note: the above HTTP response contains a HTML form that is normally automatically submitted
+    // by the browser, leading to a page that contains the link that we invoke below.
+    // To actually simulate autosubmitting that form and running some related JavaScript would be a bit of a hassle,
+    // so here we skip autosubmitting that form. Turns out the DigiD bridge is fine with this.
+
+    // Get the HTML page containing the redirect_uri back to our own app
+    let finish_digid_url = format!(
+        "{}acs?SAMLart=999991772&RelayState={}&mocking=1",
+        digid_base_url, relay_state
+    );
+    let redirect_page = do_get_as_text(&client, finish_digid_url).await;
+    let redirect_url = find_in_text(&redirect_page, "url=", "\"");
+
+    Url::parse(redirect_url).expect("failed to parse redirect url")
+}
+
+async fn do_get_as_text(client: &reqwest::Client, url: String) -> String {
+    client
+        .get(url)
+        .send()
+        .await
+        .expect("failed to GET URL")
+        .text()
+        .await
+        .expect("failed to get body text")
+}
+
+fn find_in_text<'a>(text: &'a str, start: &str, end: &str) -> &'a str {
+    let start_index = text.find(start).expect("start not found");
+    let remaining = &text[start_index + start.len()..];
+    let end_index = remaining.find(end).expect("end not found");
+    &remaining[..end_index]
 }
 
 pub async fn do_wallet_registration(mut wallet: WalletWithMocks, pin: String) -> WalletWithMocks {
