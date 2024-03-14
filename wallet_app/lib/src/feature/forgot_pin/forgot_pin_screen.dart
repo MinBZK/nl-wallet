d0b4443fd6ea38a9fc433e9a6e88ceb91cc226b1
Daan <2034049+daandebie@users.noreply.github.com>
2024-03-14 07:49:23 +0000

    PVW-2199: Update forgot pin UI and user flow
    * Implement MR feedback; sync EN texts
    
    * Implement `SliverWalletAppBar`
    
    * Remove app bar
    
    * Remove translation and rearrange UI elements
    
    * Align wallet reset flows
    
    * Sync Lokalise translations

--- wallet_app/lib/src/feature/forgot_pin/forgot_pin_screen.dart
+++ wallet_app/lib/src/feature/forgot_pin/forgot_pin_screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../domain/usecase/wallet/reset_wallet_usecase.dart';
-import '../../navigation/wallet_routes.dart';
 import '../../util/extension/build_context_extension.dart';
 import '../../wallet_assets.dart';
+import '../common/dialog/reset_wallet_dialog.dart';
 import '../common/widget/button/primary_button.dart';
 import '../common/widget/button/secondary_button.dart';
-import '../common/widget/wallet_app_bar.dart';
+import '../common/widget/sliver_wallet_app_bar.dart';
 
 class ForgotPinScreen extends StatelessWidget {
   const ForgotPinScreen({super.key});
@@ -16,9 +14,6 @@ class ForgotPinScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       key: const Key('forgotPinScreen'),
-      appBar: WalletAppBar(
-        title: Text(context.l10n.forgotPinScreenTitle),
-      ),
       body: SafeArea(
         child: Column(
           children: [
@@ -32,30 +27,29 @@ class ForgotPinScreen extends StatelessWidget {
 
   Widget _buildScrollableSection(BuildContext context) {
     return Scrollbar(
-      child: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-        children: [
-          Image.asset(WalletAssets.illustration_forgot_pin_header, fit: BoxFit.fitWidth),
-          const SizedBox(height: 24),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                context.l10n.forgotPinScreenHeadline,
-                textAlign: TextAlign.start,
-                style: context.textTheme.displayMedium,
-              ),
-              const SizedBox(height: 8),
-              Text(
-                context.l10n.forgotPinScreenDescription,
-                textAlign: TextAlign.start,
-                style: context.textTheme.bodyLarge,
-              ),
-            ],
+      child: CustomScrollView(
+        slivers: [
+          SliverWalletAppBar(title: context.l10n.forgotPinScreenTitle),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+            sliver: _buildContentSliver(context),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildContentSliver(BuildContext context) {
+    return SliverList.list(
+      children: [
+        Text(
+          context.l10n.forgotPinScreenDescription,
+          textAlign: TextAlign.start,
+          style: context.textTheme.bodyLarge,
+        ),
+        const SizedBox(height: 32),
+        Image.asset(WalletAssets.illustration_forgot_pin_header, fit: BoxFit.fitWidth),
+      ],
     );
   }
 
@@ -68,14 +62,7 @@ class ForgotPinScreen extends StatelessWidget {
           child: Column(
             children: [
               PrimaryButton(
-                onPressed: () async {
-                  final navigator = Navigator.of(context);
-                  await context.read<ResetWalletUseCase>().invoke();
-                  navigator.restorablePushNamedAndRemoveUntil(
-                    WalletRoutes.setupSecurityRoute,
-                    ModalRoute.withName(WalletRoutes.splashRoute),
-                  );
-                },
+                onPressed: () => ResetWalletDialog.show(context),
                 text: context.l10n.forgotPinScreenCta,
               ),
               const SizedBox(height: 12),
