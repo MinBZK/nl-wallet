0b80d73bc52f85f3dc4a7c9abf880e5fa2afc077
Rob <edi@minbzk.nl>
2024-04-03 10:24:31 +0000

    PVW-2560: Migrate to ConfirmButtons and make sticky
    * Remove unused padding
    
    * Update WalletPersonalizeNoDigidScreen UI buttons
    
    - Use ConfirmButtons so they can reflow in landscape
    - Make the button section sticky
    
    * Update WalletPersonalizeDigidErrorPage UI buttons
    
    - Use ConfirmButtons so they can reflow in landscape
    - Make the button section sticky
    - Update the ConfirmButton to support custom icons to show the digid logo
    
    * Update WalletPersonalizeIntroPage UI buttons
    
    - Use ConfirmButtons so they can reflow in landscape
    - Make the button section sticky
    
    * Update ConditionsScreen UI buttons
    
    - Use ConfirmButtons so they can reflow in landscape
    - Make the button section sticky
    
    * Update PrivacyScreen UI buttons
    
    - Use ConfirmButtons so they can reflow in landscape
    - Make the button section sticky

--- wallet_app/lib/src/feature/introduction/introduction_conditions_screen.dart
+++ wallet_app/lib/src/feature/introduction/introduction_conditions_screen.dart
@@ -5,8 +5,8 @@ import '../../util/extension/build_context_extension.dart';
 import '../../wallet_assets.dart';
 import '../common/screen/placeholder_screen.dart';
 import '../common/widget/bullet_list.dart';
-import '../common/widget/button/primary_button.dart';
-import '../common/widget/button/secondary_button.dart';
+import '../common/widget/button/confirm/confirm_button.dart';
+import '../common/widget/button/confirm/confirm_buttons.dart';
 import '../common/widget/button/wallet_app_bar_back_button.dart';
 import '../common/widget/sliver_sized_box.dart';
 import '../common/widget/sliver_wallet_app_bar.dart';
@@ -23,82 +23,84 @@ class IntroductionConditionsScreen extends StatelessWidget {
   }
 
   Widget _buildContent(BuildContext context) {
-    return Scrollbar(
-      thumbVisibility: true,
-      child: CustomScrollView(
-        slivers: [
-          SliverWalletAppBar(
-            title: context.l10n.introductionConditionsScreenHeadline,
-            progress: 0.16,
-            leading: const WalletAppBarBackButton(),
-            actions: [
-              IconButton(
-                onPressed: () => Navigator.pushNamed(context, WalletRoutes.aboutRoute),
-                icon: const Icon(Icons.help_outline_rounded),
-              ),
-            ],
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            sliver: SliverToBoxAdapter(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BulletList(
-                    items: context.l10n.introductionConditionsScreenBulletPoints.split('\n'),
+    return Column(
+      children: [
+        Expanded(
+          child: Scrollbar(
+            thumbVisibility: true,
+            child: CustomScrollView(
+              slivers: [
+                SliverWalletAppBar(
+                  title: context.l10n.introductionConditionsScreenHeadline,
+                  progress: 0.16,
+                  leading: const WalletAppBarBackButton(),
+                  actions: [
+                    IconButton(
+                      onPressed: () => Navigator.pushNamed(context, WalletRoutes.aboutRoute),
+                      icon: const Icon(Icons.help_outline_rounded),
+                    ),
+                  ],
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  sliver: SliverToBoxAdapter(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        BulletList(
+                          items: context.l10n.introductionConditionsScreenBulletPoints.split('\n'),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  sliver: SliverToBoxAdapter(
+                    child: Image.asset(
+                      WalletAssets.illustration_conditions_screen,
+                      fit: context.isLandscape ? BoxFit.contain : BoxFit.fitWidth,
+                      height: context.isLandscape ? 160 : null,
+                      width: double.infinity,
+                    ),
+                  ),
+                ),
+                const SliverSizedBox(height: 24),
+              ],
             ),
           ),
-          SliverPadding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            sliver: SliverToBoxAdapter(
-              child: Image.asset(
-                WalletAssets.illustration_conditions_screen,
-                fit: context.isLandscape ? BoxFit.contain : BoxFit.fitWidth,
-                height: context.isLandscape ? 160 : null,
-                width: double.infinity,
-              ),
-            ),
-          ),
-          const SliverSizedBox(height: 24),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            fillOverscroll: true,
-            child: _buildBottomSection(context),
-          )
-        ],
-      ),
+        ),
+        _buildBottomSection(context),
+      ],
     );
   }
 
   Widget _buildBottomSection(BuildContext context) {
+    final nextButton = ConfirmButton(
+      key: const Key('introductionConditionsScreenNextCta'),
+      onPressed: () => Navigator.of(context).restorablePushNamedAndRemoveUntil(
+        WalletRoutes.setupSecurityRoute,
+        ModalRoute.withName(WalletRoutes.splashRoute),
+      ),
+      icon: Icons.arrow_forward_rounded,
+      text: context.l10n.introductionConditionsScreenNextCta,
+      buttonType: ConfirmButtonType.primary,
+    );
+    final conditionsButton = ConfirmButton(
+      text: context.l10n.introductionConditionsScreenConditionsCta,
+      icon: Icons.arrow_forward_rounded,
+      buttonType: ConfirmButtonType.text,
+      onPressed: () => PlaceholderScreen.show(context, secured: false),
+      key: const Key('introductionConditionsScreenConditionsCta'),
+    );
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         const Divider(height: 1),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: context.isLandscape ? 8 : 24),
-          child: Column(
-            children: [
-              SecondaryButton(
-                key: const Key('introductionConditionsScreenConditionsCta'),
-                onPressed: () => PlaceholderScreen.show(context, secured: false),
-                text: context.l10n.introductionConditionsScreenConditionsCta,
-              ),
-              const SizedBox(height: 12),
-              PrimaryButton(
-                key: const Key('introductionConditionsScreenNextCta'),
-                onPressed: () => Navigator.of(context).restorablePushNamedAndRemoveUntil(
-                  WalletRoutes.setupSecurityRoute,
-                  ModalRoute.withName(WalletRoutes.splashRoute),
-                ),
-                text: context.l10n.introductionConditionsScreenNextCta,
-              ),
-            ],
-          ),
+        ConfirmButtons(
+          secondaryButton: context.isLandscape ? conditionsButton : nextButton,
+          primaryButton: context.isLandscape ? nextButton : conditionsButton,
         ),
       ],
     );
