082cd36ee3e1ae852feb2f4b4606259a20e85cbb
Ruud Klaver <ruud@pantsforcats.nl>
2024-04-22 18:24:36 +0200

    Remove duplicate PostgreSQL integration test

--- wallet_core/wallet_server/src/store.rs
+++ wallet_core/wallet_server/src/store.rs
@@ -283,49 +283,4 @@ pub mod postgres {
             Ok(())
         }
     }
-
-    #[cfg(test)]
-    mod tests {
-        use serde::Deserialize;
-
-        use crate::settings::Settings;
-
-        use super::*;
-
-        #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-        struct TestData {
-            id: String,
-            data: Vec<u8>,
-        }
-
-        impl HasProgress for TestData {
-            fn progress(&self) -> Progress {
-                Progress::Active
-            }
-        }
-
-        impl SessionDataType for TestData {
-            const TYPE: &'static str = "testdata";
-        }
-
-        #[cfg_attr(not(feature = "db_test"), ignore)]
-        #[tokio::test]
-        async fn test_write() {
-            let settings = Settings::new().unwrap();
-            let store = PostgresSessionStore::try_new(settings.store_url).await.unwrap();
-
-            let expected = SessionState::<TestData>::new(
-                SessionToken::new_random(),
-                TestData {
-                    id: "hello".to_owned(),
-                    data: vec![1, 2, 3],
-                },
-            );
-
-            store.write(expected.clone(), true).await.unwrap();
-
-            let actual: SessionState<TestData> = store.get(&expected.token).await.unwrap().unwrap();
-            assert_eq!(actual.data, expected.data);
-        }
-    }
 }
