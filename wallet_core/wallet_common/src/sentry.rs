57c7635434437cf53cce45b061f89d774beb65b5
peterpaul <pp.kleinhaneveld@gmail.com>
2024-05-17 11:10:45 +0200

    Fix release_name to main crate, and setup sentry before tokio

--- wallet_core/wallet_common/src/sentry.rs
+++ wallet_core/wallet_common/src/sentry.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 
-use sentry::{release_name, ClientInitGuard, ClientOptions};
+use sentry::{ClientInitGuard, ClientOptions};
 use serde::Deserialize;
 
 #[derive(Clone, Deserialize)]
@@ -11,16 +11,28 @@ pub struct Sentry {
 
 impl Sentry {
     /// Initialize Sentry and return the [`ClientInitGuard`].
+    /// [release] must be set to `sentry::release_name!()` from the `main.rs` of the application, so that the release
+    /// matches the main crate.
     #[must_use = "this value must be retained during the lifetime of the application"]
-    pub fn init(&self) -> ClientInitGuard {
+    pub fn init(&self, release: Option<Cow<'static, str>>) -> ClientInitGuard {
         sentry::init((
             self.dsn.clone(),
             ClientOptions {
-                release: release_name!(),
+                release,
                 environment: self.environment.as_ref().map(|e| Cow::from(e.clone())),
                 debug: cfg!(debug_assertions),
                 ..Default::default()
             },
         ))
     }
+}
+
+/// Helper macro, to initialize Sentry from an `Option<Sentry>` reference.
+/// This macro ensures that the `release_name` of the main application is used.
+/// When the argument is `None`, sentry will not be initialized.
+#[macro_export]
+macro_rules! try_init_sentry {
+    ($x:expr) => {
+        $x.as_ref().map(|sentry| sentry.init(sentry::release_name!()))
+    };
 }
--- wallet_core/wallet_provider/src/main.rs
+++ wallet_core/wallet_provider/src/main.rs
@@ -3,15 +3,15 @@ use std::error::Error;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::EnvFilter;
 
-use wallet_common::sentry::Sentry;
+use wallet_common::try_init_sentry;
 use wallet_provider::{server, settings::Settings};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+// Cannot use #[tokio::main], see: https://docs.sentry.io/platforms/rust/#async-main-function
+fn main() -> Result<(), Box<dyn Error>> {
     let settings = Settings::new()?;
 
     // Retain [`ClientInitGuard`]
-    let _guard = settings.sentry.as_ref().map(Sentry::init);
+    let _guard = try_init_sentry!(settings.sentry);
 
     let builder = tracing_subscriber::fmt().with_env_filter(
         EnvFilter::builder()
@@ -24,7 +24,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         builder.init()
     }
 
-    server::serve(settings).await?;
-
-    Ok(())
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?
+        .block_on(async { server::serve(settings).await })
 }
