7aa6e71745f78395a315838ebf7b547280f3d224
peterpaul <pp.kleinhaneveld@gmail.com>
2024-05-30 15:10:28 +0200

    sha1sum is not defined for macos

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -34,7 +34,7 @@ export GBA_HC_CONV_PORT=3008
 
 export RDO_MAX_PORT=8006
 
-export PID_ISSUER_API_KEY=$(echo $RANDOM | sha1sum | head -c 40)
+export PID_ISSUER_API_KEY=$(echo $RANDOM | shasum -a1 | head -c 40)
 
 # Database properties for the wallet_provider, with defaults.
 # The defaults will work when using the `wallet_core/wallet_provider/docker-compose.yml` file.
