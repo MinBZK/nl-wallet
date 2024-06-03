24d2df137c97bf17a36f21c933210e3d6619aef5
peterpaul <pp.kleinhaneveld@gmail.com>
2024-06-03 11:36:09 +0200

    Simplify imports

--- wallet_core/wallet_server/src/lib.rs
+++ wallet_core/wallet_server/src/lib.rs
@@ -1,12 +1,8 @@
-cfg_if::cfg_if! {
-    if #[cfg(any(feature = "disclosure", feature = "issuance"))] {
-    pub mod cbor;
-    pub mod log_requests;
-    pub mod server;
-    pub mod settings;
-    pub mod store;
-    }
-}
+pub mod cbor;
+pub mod log_requests;
+pub mod server;
+pub mod settings;
+pub mod store;
 
 #[cfg(feature = "postgres")]
 pub mod entity;
