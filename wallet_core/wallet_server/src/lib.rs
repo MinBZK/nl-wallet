407ad610087a88442047b2fbd507c55029d9d7dd
peterpaul <pp.kleinhaneveld@gmail.com>
2024-06-06 15:26:11 +0200

    Put all modules behind features

--- wallet_core/wallet_server/src/lib.rs
+++ wallet_core/wallet_server/src/lib.rs
@@ -1,21 +1,23 @@
-pub mod log_requests;
-pub mod server;
-pub mod settings;
-pub mod store;
-
-#[cfg(feature = "postgres")]
-pub mod entity;
-
+cfg_if::cfg_if! {
+    if #[cfg(any(feature = "disclosure", feature = "issuance"))] {
+    pub mod log_requests;
+    pub mod server;
+    pub mod settings;
+    pub mod store;
+    }
+}
 cfg_if::cfg_if! {
     if #[cfg(feature = "disclosure")] {
     pub mod verifier;
     pub mod cbor;
     }
 }
-
 cfg_if::cfg_if! {
     if #[cfg(feature = "issuance")] {
     pub mod issuer;
     pub mod pid;
     }
 }
+
+#[cfg(feature = "postgres")]
+pub mod entity;
