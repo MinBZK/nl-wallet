628c30a03f6107a35f48f64678005761bb5198ab
peterpaul <pp.kleinhaneveld@gmail.com>
2024-07-05 16:22:53 +0200

    Verify whether sentry is invoked in integration tests

--- wallet_core/wallet_common/wallet_common_macros/tests/sentry_capture_error.rs
+++ wallet_core/wallet_common/wallet_common_macros/tests/sentry_capture_error.rs
@@ -1,3 +1,4 @@
+use sentry::{test::with_captured_events, Level};
 use thiserror::Error;
 
 use wallet_common::error_category::{sentry_capture_error, ErrorCategory};
@@ -8,14 +9,14 @@ struct Wallet;
 #[allow(dead_code)]
 enum Error {
     #[error("Just some error")]
-    #[category(expected)]
-    Unit,
+    #[category(critical)]
+    CriticalError,
 }
 
 #[sentry_capture_error]
 impl Wallet {
     pub fn do_something(&self) -> Result<(), Error> {
-        Err(Error::Unit)
+        Err(Error::CriticalError)
     }
 }
 
@@ -26,45 +27,49 @@ trait Foo {
 #[sentry_capture_error]
 impl Foo for Wallet {
     fn foo(&self) -> Result<(), Error> {
-        Err(Error::Unit)
+        Err(Error::CriticalError)
     }
 }
 
 #[sentry_capture_error]
 fn bar() -> Result<(), Error> {
-    Err(Error::Unit)
+    Err(Error::CriticalError)
 }
 
 #[test]
 fn test_do_something() {
     let wallet = Wallet;
-    match wallet.do_something() {
-        Ok(_) => panic!("Expected error"),
-        Err(error) => {
-            dbg!(error);
-        }
-    }
+    let events = with_captured_events(|| {
+        let _ = wallet.do_something();
+    });
+    assert_eq!(events.len(), 1);
+    assert_eq!(events[0].level, Level::Error);
+    assert_eq!(events[0].exception.values[0].ty, "CriticalError".to_string());
+    assert_eq!(events[0].exception.values[0].value, Some("Just some error".to_string()));
 }
 
 #[test]
 fn test_foo() {
     let wallet = Wallet;
-    match wallet.foo() {
-        Ok(_) => panic!("Expected error"),
-        Err(error) => {
-            dbg!(error);
-        }
-    }
+
+    let events = with_captured_events(|| {
+        let _ = wallet.foo();
+    });
+    assert_eq!(events.len(), 1);
+    assert_eq!(events[0].level, Level::Error);
+    assert_eq!(events[0].exception.values[0].ty, "CriticalError".to_string());
+    assert_eq!(events[0].exception.values[0].value, Some("Just some error".to_string()));
 }
 
 #[test]
 fn test_bar() {
-    match bar() {
-        Ok(_) => panic!("Expected error"),
-        Err(error) => {
-            dbg!(error);
-        }
-    }
+    let events = with_captured_events(|| {
+        let _ = bar();
+    });
+    assert_eq!(events.len(), 1);
+    assert_eq!(events[0].level, Level::Error);
+    assert_eq!(events[0].exception.values[0].ty, "CriticalError".to_string());
+    assert_eq!(events[0].exception.values[0].value, Some("Just some error".to_string()));
 }
 
 #[test]
