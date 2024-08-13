77fe80be1b52e3c0d90aa04d68e662e1282d07f9
Rubin Simons <rubin.simons@ictu.nl>
2024-08-13 14:58:37 +0000

    PVW-3286 Change var to const and two style.css issues
    * Pixelated.
    
    * Set modal-anchor, svg.loading-indicator once.
    
    * Don't set image-rendering twice.
    
    * Fixed typo in comment.
    
    * Changed require line from var to const.

--- wallet_web/lib/main.js
+++ wallet_web/lib/main.js
@@ -1,5 +1,5 @@
 "use strict"
 Object.defineProperty(exports, "__esModule", { value: true })
 exports.WalletButton = void 0
-var WalletButton_vue_1 = require("./WalletButton.vue")
+const WalletButton_vue_1 = require("./WalletButton.vue")
 exports.WalletButton = WalletButton_vue_1.default
