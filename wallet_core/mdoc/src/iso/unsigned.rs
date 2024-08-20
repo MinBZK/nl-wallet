06e30473b032888a7f81db340b6a453275c27d85
Sietse Ringers <mail@sietseringers.net>
2024-08-20 16:09:16 +0200

    Strip CBOR-specifics from JSON output of disclosed_attributes endpoint output

--- wallet_core/mdoc/src/iso/unsigned.rs
+++ wallet_core/mdoc/src/iso/unsigned.rs
@@ -1,5 +1,7 @@
 use std::num::NonZeroU8;
 
+use base64::prelude::*;
+use ciborium::Value;
 use indexmap::IndexMap;
 use nutype::nutype;
 use serde::{Deserialize, Serialize};
@@ -41,6 +43,8 @@ pub struct UnsignedMdoc {
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Entry {
     pub name: DataElementIdentifier,
+
+    #[serde(serialize_with = "json_serialize_cbor_value")]
     pub value: DataElementValue,
 }
 
@@ -57,13 +61,49 @@ impl From<&Attributes> for Vec<Entry> {
     }
 }
 
+fn json_serializable_value(value: &Value) -> Value {
+    match value {
+        Value::Integer(int) => Value::Integer(*int),
+        Value::Float(float) => Value::Float(*float),
+        Value::Bool(bool) => Value::Bool(*bool),
+        Value::Text(text) => Value::Text(text.clone()),
+        Value::Null => Value::Null,
+
+        Value::Bytes(bytes) => Value::Text(BASE64_STANDARD_NO_PAD.encode(bytes)),
+        Value::Tag(_, val) => json_serializable_value(val),
+        Value::Array(arr) => Value::Array(arr.iter().map(json_serializable_value).collect()),
+        Value::Map(map) => Value::Map(
+            map.iter()
+                .map(|(key, val)| (json_serializable_value(key), json_serializable_value(val)))
+                .collect(),
+        ),
+
+        // Value is a non-exhaustive enum
+        _ => panic!("unknown CBOR value type"),
+    }
+}
+
+/// A serializer for [`ciborium::Value`] that, when serializing to JSON, converts CBOR-types that have no
+/// JSON equivalent (tagged values, byte sequences) to something more natural in JSON.
+fn json_serialize_cbor_value<S: serde::Serializer>(value: &Value, serializer: S) -> Result<S::Ok, S::Error> {
+    if !serializer.is_human_readable() {
+        value.serialize(serializer)
+    } else {
+        json_serializable_value(value).serialize(serializer)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use base64::prelude::*;
+    use ciborium::cbor;
     use regex::Regex;
+    use serde::Serialize;
+    use serde_json::json;
 
     use crate::test::data;
 
-    use super::*;
+    use super::{json_serialize_cbor_value, Engine, UnsignedMdoc};
 
     #[test]
     fn test_unsigned_mdoc_disclosure_count() {
@@ -89,5 +129,47 @@ mod tests {
             .replace(&unsigned_json_cc_0, "\"copy_count\": 100");
 
         serde_json::from_str::<UnsignedMdoc>(&unsigned_json_cc_100).expect("should be valid JSON of UnsignedMdoc");
+    }
+
+    #[test]
+    fn test_json_cbor_serialization() {
+        #[derive(Serialize)]
+        pub struct Test {
+            #[serde(serialize_with = "json_serialize_cbor_value")]
+            pub value: ciborium::Value,
+        }
+
+        let bytes = hex::decode("DEADBEEF").unwrap();
+
+        let testvalue = cbor!({
+            "int" => 42,
+            "float" => 2.818281828,
+            "text" => "Hello, world!",
+            "bool" => true,
+            "null" => null,
+            "array" => ["foo"],
+            "map" => {"recursive" => ciborium::Value::Bytes(bytes.clone())},
+            "tagged_date" => ciborium::Value::Tag(1004, ciborium::Value::Text("2020-01-01".to_string()).into()),
+            "bytes" => ciborium::Value::Bytes(bytes.clone())
+        })
+        .unwrap();
+
+        let serialized = serde_json::to_string_pretty(&Test { value: testvalue }).unwrap();
+        let deserialized: serde_json::Value = serde_json::from_str(&serialized).unwrap();
+
+        let expected = json!({
+            "value": {
+                "int": 42,
+                "float": 2.818281828,
+                "text": "Hello, world!",
+                "bool": true,
+                "null": null,
+                "array": ["foo"],
+                "map": {"recursive": BASE64_STANDARD_NO_PAD.encode(bytes.clone())},
+                "tagged_date": "2020-01-01",
+                "bytes": BASE64_STANDARD_NO_PAD.encode(bytes.clone())
+            }
+        });
+        assert_eq!(deserialized, expected);
     }
 }
