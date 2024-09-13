7efa3dcac748d0be7a23f2a9505d58a5e804e6d2
Ruud Klaver <ruud@pantsforcats.nl>
2024-09-13 21:19:33 +0200

    Finish Apple attestation validation
    This adds step 8 as listed in the Apple documentation as the final step.

--- wallet_core/apple_app_attest/src/attestation.rs
+++ wallet_core/apple_app_attest/src/attestation.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 use p256::ecdsa::VerifyingKey;
+use passkey_types::ctap2::Aaguid;
 use serde::Deserialize;
 use serde_with::{serde_as, TryFromInto};
 use sha2::{Digest, Sha256};
@@ -45,8 +46,27 @@ pub enum AttestationValidationError {
     CounterNotZero(u32),
     #[error("attested credential data is not present in authenticator data")]
     AttestedCredentialDataMissing,
+    #[error("attestation environment is not match, expected: {:?}, received: {:?}", expected.0, received.0)]
+    EnvironmentMismatch { expected: Aaguid, received: Aaguid },
     #[error("key identifier does not match calculated value")]
     KeyIdentifierMismatch,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum AttestationEnvironment {
+    Development,
+    Production,
+}
+
+impl AttestationEnvironment {
+    pub(crate) fn aaguid(&self) -> Aaguid {
+        let guid = match self {
+            Self::Development => b"appattestdevelop",
+            Self::Production => b"appattest\0\0\0\0\0\0\0",
+        };
+
+        Aaguid::from(*guid)
+    }
 }
 
 #[serde_as]
@@ -85,6 +105,7 @@ impl Attestation {
         time: DateTime<Utc>,
         challenge: &[u8],
         app_identifier: &AppIdentifier,
+        environment: AttestationEnvironment,
     ) -> Result<(Self, VerifyingKey, u32), AttestationError> {
         let attestation: Self = ciborium::from_reader(bytes).map_err(AttestationDecodingError::Cbor)?;
 
@@ -114,14 +135,15 @@ impl Attestation {
         //    performing the attestation, and append that hash to the end of the authenticator data (authData from the
         //    decoded object).
         // 3. Generate a new SHA256 hash of the composite item to create nonce.
-        // 4. Obtain the value of the credCert extension with OID 1.2.840.113635.100.8.2, which is a DER-encoded ASN.1
-        //    sequence. Decode the sequence and extract the single octet string that it contains. Verify that the string
-        //    equals nonce.
 
         let nonce = Sha256::new()
             .chain_update(attestation.auth_data.source())
             .chain_update(challenge)
             .finalize();
+
+        // 4. Obtain the value of the credCert extension with OID 1.2.840.113635.100.8.2, which is a DER-encoded ASN.1
+        //    sequence. Decode the sequence and extract the single octet string that it contains. Verify that the string
+        //    equals nonce.
 
         let extension_nonce = credential_certificate
             .attestation_extension_data()
@@ -155,7 +177,9 @@ impl Attestation {
             return Err(AttestationValidationError::CounterNotZero(counter))?;
         }
 
-        // TODO: Perform step 8.
+        // 8. Verify that the authenticator data’s aaguid field is either appattestdevelop if operating in the
+        // development environment, or appattest followed by seven 0x00 bytes if operating in the production
+        // environment.
 
         let attested_credential_data = attestation
             .auth_data
@@ -163,6 +187,14 @@ impl Attestation {
             .attested_credential_data
             .as_ref()
             .ok_or(AttestationValidationError::AttestedCredentialDataMissing)?;
+
+        let environment_aaguid = environment.aaguid();
+        if attested_credential_data.aaguid != environment_aaguid {
+            return Err(AttestationValidationError::EnvironmentMismatch {
+                expected: environment_aaguid,
+                received: attested_credential_data.aaguid,
+            })?;
+        }
 
         // 9. Verify that the authenticator data’s credentialId field is the same as the key identifier.
 
@@ -182,7 +214,7 @@ mod tests {
 
     use crate::app_identifier::AppIdentifier;
 
-    use super::Attestation;
+    use super::{Attestation, AttestationEnvironment};
 
     // Source: https://www.apple.com/certificateauthority/Apple_App_Attestation_Root_CA.pem
     const APPLE_ROOT_CA: [u8; 549] = Pem::decode(include_bytes!("../assets/Apple_App_Attestation_Root_CA.pem"));
@@ -203,7 +235,16 @@ mod tests {
             .to_utc();
         let app_id = AppIdentifier::new(TEST_APP_PREFIX, TEST_APP_BUNDLE_ID);
 
-        let _ = Attestation::parse_and_verify(&TEST_ATTESTATION, &[trust_anchor], time, TEST_CHALLENGE, &app_id)
-            .expect("should decode attestation object");
+        let (_, _, counter) = Attestation::parse_and_verify(
+            &TEST_ATTESTATION,
+            &[trust_anchor],
+            time,
+            TEST_CHALLENGE,
+            &app_id,
+            AttestationEnvironment::Production,
+        )
+        .expect("attestation object should be valid");
+
+        assert_eq!(counter, 0);
     }
 }
