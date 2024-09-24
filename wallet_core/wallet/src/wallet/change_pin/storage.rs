ca31b491be3246a9044145c1fec2d1ae1308e4d3
peterpaul <pp.kleinhaneveld@gmail.com>
2024-09-24 16:55:30 +0200

    Unit test ChangePinStorage implementation

--- wallet_core/wallet/src/wallet/change_pin/storage.rs
+++ wallet_core/wallet/src/wallet/change_pin/storage.rs
@@ -43,3 +43,52 @@ where
         storage.upsert_data(&data).await
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use assert_matches::assert_matches;
+
+    use crate::{
+        pin::change::{ChangePinStorage, State},
+        storage::{MockStorage, StorageState},
+    };
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_change_pin_storage() {
+        let storage = MockStorage::new(StorageState::Opened, None);
+        let change_pin_storage = RwLock::new(storage);
+
+        assert_matches!(change_pin_storage.get_change_pin_state().await, Ok(None));
+
+        assert_matches!(change_pin_storage.store_change_pin_state(State::Commit).await, Ok(()));
+
+        assert_matches!(change_pin_storage.get_change_pin_state().await, Ok(Some(State::Commit)));
+
+        assert_matches!(change_pin_storage.clear_change_pin_state().await, Ok(()));
+
+        assert_matches!(change_pin_storage.get_change_pin_state().await, Ok(None));
+
+        {
+            let storage = change_pin_storage.read().await;
+            assert_matches!(storage.fetch_data::<RegistrationData>().await, Ok(None));
+        }
+
+        let wallet_certificate = WalletCertificate::from("thisisdefinitelyvalid");
+        assert_matches!(
+            change_pin_storage.change_pin(vec![1, 2, 3], wallet_certificate).await,
+            Ok(())
+        );
+
+        {
+            let storage = change_pin_storage.read().await;
+            let actual = storage
+                .fetch_data::<RegistrationData>()
+                .await
+                .expect("database error")
+                .expect("no registation data found");
+            assert_eq!(actual.pin_salt, vec![1, 2, 3]);
+        }
+    }
+}
