899a16ad11957f23f1aad6e7ca4db5421c7b4364
peterpaul <pp.kleinhaneveld@gmail.com>
2024-10-22 13:34:35 +0200

    Scrub sensitive data from sentry events

--- wallet_core/error_category/src/lib.rs
+++ wallet_core/error_category/src/lib.rs
@@ -1,7 +1,17 @@
 #[cfg(feature = "sentry")]
 pub mod sentry;
 
+use std::{
+    fmt::{self, Display, Formatter},
+    str::FromStr,
+};
+
 pub use error_category_derive::{sentry_capture_error, ErrorCategory};
+
+const CRITICAL: &str = "critical";
+const EXPECTED: &str = "expected";
+const UNEXPECTED: &str = "unexpected";
+const PD: &str = "pd";
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Category {
@@ -9,6 +19,34 @@ pub enum Category {
     Critical,     // Report Error to Sentry, with contents
     PersonalData, // Report Error to Sentry, without contents
     Unexpected,   // Should never occer at runtime, panic!
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("not a category: {0}")]
+pub struct UnrecognizedCategory(String);
+
+impl Display for Category {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Expected => f.write_str(EXPECTED),
+            Self::Critical => f.write_str(CRITICAL),
+            Self::PersonalData => f.write_str(PD),
+            Self::Unexpected => f.write_str(UNEXPECTED),
+        }
+    }
+}
+
+impl FromStr for Category {
+    type Err = UnrecognizedCategory;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            EXPECTED => Ok(Self::Expected),
+            CRITICAL => Ok(Self::Critical),
+            PD => Ok(Self::PersonalData),
+            UNEXPECTED => Ok(Self::Unexpected),
+            _ => Err(UnrecognizedCategory(s.to_string())),
+        }
+    }
 }
 
 pub trait ErrorCategory {
--- wallet_core/error_category/src/sentry.rs
+++ wallet_core/error_category/src/sentry.rs
@@ -5,38 +5,91 @@ use sentry::{
     protocol::{Event, Exception},
     Level,
 };
-use tracing::debug;
 
 use crate::{Category, ErrorCategory};
 
+/// Create a sentry [`Event`] from an [`ErrorCategory`].
+/// A tag `category` with the string representation of the [`ErrorCategory`] is added to the event, so
 pub fn classify_mask_and_capture<T: ErrorCategory + Error + ?Sized>(error: &T) {
-    match error.category() {
-        Category::Expected => {
-            debug!("encountered expected error, not reporting to sentry: {}", error);
+    let category = error.category();
+    let mut event = event_from_error(error);
+    event
+        .tags
+        .insert("category".to_owned(), format!("{category}").to_owned());
+    sentry::capture_event(event);
+}
+
+/// Filter and scrub events
+pub fn filter_and_scrub_sensitive_data(mut event: Event) -> Option<Event> {
+    let category: Option<Category> = event.tags.get("category").and_then(|t| t.parse().ok());
+    match category {
+        Some(Category::Unexpected) => {
+            tracing::error!("event has category unexpected, this is a programming error, not sending to Sentry");
+            None
         }
-        Category::Critical => {
-            debug!("encountered critical error, reporting to sentry: {}", error);
-            let event = event_from_error(error, false);
-            let _uuid = sentry::capture_event(event);
+        Some(Category::Expected) => {
+            tracing::debug!("event has category expected, not sending to Sentry");
+            None
         }
-        Category::PersonalData => {
-            debug!(
-                "encountered critical error with possible PII, reporting to sentry without data: {}",
-                error
-            );
-            let event = event_from_error(error, true);
-            let _uuid = sentry::capture_event(event);
+        Some(Category::Critical) => {
+            tracing::debug!("event has category critical, sending event to Sentry verbatim");
+            event.scrub(false);
+            Some(event)
         }
-        Category::Unexpected => {
-            panic!(
-                "encountered unexpected error, which means that it should never occur in the Wallet: {}",
-                error
-            );
+        Some(Category::PersonalData) => {
+            tracing::debug!("event has category pd, sending scrubbed event to Sentry");
+            event.scrub(true);
+            Some(event)
+        }
+        None => {
+            tracing::debug!("uncategorized event, sending scrubbed event to Sentry");
+            event.scrub(true);
+            Some(event)
         }
     }
 }
 
-/// Create a sentry `Event` from a `std::error::Error`, remove PII by setting `sensitive` to true.
+/// Scrub sensitive data.
+/// By default breadcrumb, transaction and request data is removed as these might be filled automatically by
+/// the `sentry` crate and/or some of its integrations.
+/// If `sensitive_messages` is true, the value of exception messages is removed, these contain the error
+/// descriptions according to the `Display` implementation of each `Error`.
+trait Scrub {
+    fn scrub(&mut self, sensitive_messages: bool);
+}
+
+/// Scrubbing sensitive data from [`Event`] according to:
+/// https://docs.sentry.io/platforms/rust/data-management/sensitive-data/#scrubbing-data.
+///
+/// According to the docs, in general sensitive data can appear in the following areas:
+/// - stacktraces: Rust stacktraces do not contain sensitive data
+/// - breadcrumbs: By default Rust does not fill breadcrumbs, although it can be done manually or via logging/tracing
+/// - contextual information: Inspection showed no sensitive information, the following are detected for the Wallet
+///   - device: arch
+///   - os: name, version, kernel_version
+///   - rust: name, version, channel
+/// - transactional data: Rust can fill this for tower services, this is not configured for the Wallet
+/// - request: Rust can fill this for tower services, this is not configured for the Wallet
+impl Scrub for Event<'_> {
+    fn scrub(&mut self, sensitive_messages: bool) {
+        self.breadcrumbs = Default::default();
+        self.transaction = None;
+        self.request = None;
+
+        self.exception.iter_mut().for_each(|e| e.scrub(sensitive_messages));
+    }
+}
+
+/// Scrub `value` from [`Exception`] since it might contain sensitive data
+impl Scrub for Exception {
+    fn scrub(&mut self, sensitive_messages: bool) {
+        if sensitive_messages {
+            self.value = None;
+        }
+    }
+}
+
+/// Create a sentry `Event` from a `std::error::Error`.
 ///
 /// Copied from `sentry::event_from_error`.
 ///
@@ -56,22 +109,22 @@ pub fn classify_mask_and_capture<T: ErrorCategory + Error + ?Sized>(error: &T) {
 /// #[error("outer")]
 /// struct OuterError(#[from] InnerError);
 ///
-/// let event = error_category::sentry::event_from_error(&OuterError(InnerError), true);
+/// let event = error_category::sentry::event_from_error(&OuterError(InnerError));
 /// assert_eq!(event.level, sentry::protocol::Level::Error);
 /// assert_eq!(event.exception.len(), 2);
 /// assert_eq!(&event.exception[0].ty, "InnerError");
-/// assert_eq!(event.exception[0].value, None);
+/// assert_eq!(event.exception[0].value, Some("inner".to_string()));
 /// assert!(&event.exception[1].ty.ends_with("::OuterError"));
-/// assert_eq!(event.exception[1].value, None);
+/// assert_eq!(event.exception[1].value, Some("outer".to_string()));
 /// ```
 ///
 /// [sentry event payloads]: https://develop.sentry.dev/sdk/event-payloads/exception/
-pub fn event_from_error<E: Error + ?Sized>(err: &E, sensitive: bool) -> Event<'static> {
-    let mut exceptions = vec![exception_from_error(err, sensitive)];
+pub fn event_from_error<E: Error + ?Sized>(err: &E) -> Event<'static> {
+    let mut exceptions = vec![exception_from_error(err)];
 
     let mut source = err.source();
     while let Some(err) = source {
-        exceptions.push(exception_from_error(err, sensitive));
+        exceptions.push(exception_from_error(err));
         source = err.source();
     }
 
@@ -84,8 +137,8 @@ pub fn event_from_error<E: Error + ?Sized>(err: &E, sensitive: bool) -> Event<'s
 }
 
 /// Copied from `sentry::exception_from_error`.
-/// Extended with `sensitive` argument, to allow for removing sensitive data.
-fn exception_from_error<E: Error + ?Sized>(err: &E, sensitive: bool) -> Exception {
+/// This version provides better type information for the root error in the chain.
+fn exception_from_error<E: Error + ?Sized>(err: &E) -> Exception {
     let dbg = format!("{err:?}");
     let value = err.to_string();
     let type_name = std::any::type_name::<E>();
@@ -120,13 +173,14 @@ fn exception_from_error<E: Error + ?Sized>(err: &E, sensitive: bool) -> Exceptio
     };
     Exception {
         ty,
-        value: (!sensitive).then_some(value),
+        value: Some(value),
         ..Default::default()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
     use sentry::{test::with_captured_events, Level};
     use thiserror::Error;
 
@@ -160,85 +214,52 @@ mod tests {
         }
     }
 
-    #[test]
-    fn test_classify_mask_and_capture_expected() {
-        let error = ErrorEnum::Specific(SpecificError {
-            category: Category::Expected,
-        });
-        let events = with_captured_events(|| {
-            classify_mask_and_capture(&error);
-        });
-        assert_eq!(events.len(), 0);
-    }
-
-    #[test]
-    #[should_panic(
-        expected = "encountered unexpected error, which means that it should never occur in the Wallet: Some error: \
-                    My error message"
-    )]
-    fn test_classify_mask_and_capture_unexpected() {
-        let error = ErrorEnum::Specific(SpecificError {
-            category: Category::Unexpected,
-        });
-        classify_mask_and_capture(&error);
-    }
-
-    #[test]
-    fn test_classify_mask_and_capture_critical() {
-        let error = ErrorEnum::Specific(SpecificError {
-            category: Category::Critical,
-        });
-        let events = with_captured_events(|| {
+    #[rstest]
+    #[case(Category::PersonalData, "pd")]
+    #[case(Category::Critical, "critical")]
+    #[case(Category::Expected, "expected")]
+    #[case(Category::Unexpected, "unexpected")]
+    fn test_classify_mask_and_capture_enum(#[case] category: Category, #[case] expected_tag: String) {
+        let error = ErrorEnum::Specific(SpecificError { category });
+        let mut events = with_captured_events(|| {
             classify_mask_and_capture(&error);
         });
         assert_eq!(events.len(), 1);
-        assert_eq!(events[0].level, Level::Error);
-        assert_eq!(events[0].exception.values.len(), 2);
-        assert_eq!(events[0].exception.values[0].ty, "SpecificError".to_string());
+        let event = events.remove(0);
+        assert_eq!(event.level, Level::Error);
+        assert_eq!(event.exception.values.len(), 2);
+        assert_eq!(event.exception.values[0].ty, "SpecificError".to_string());
         assert_eq!(
-            events[0].exception.values[1].ty,
+            event.exception.values[1].ty,
             "error_category::sentry::tests::ErrorEnum::Specific".to_string()
         );
-        assert_eq!(events[0].exception.values[0].value, Some(ERROR_MSG.to_string()));
-        assert!(format!("{:?}", events[0]).contains(ERROR_MSG));
+        assert_eq!(event.exception.values[0].value, Some(ERROR_MSG.to_string()));
+        assert!(format!("{:?}", event).contains(ERROR_MSG));
+        let category = event.tags.get("category");
+        assert_eq!(category, Some(&expected_tag));
     }
 
-    #[test]
-    fn test_classify_mask_and_capture_critical_struct() {
-        let error = SpecificError {
-            category: Category::Critical,
-        };
-        let events = with_captured_events(|| {
+    #[rstest]
+    #[case(Category::PersonalData, "pd")]
+    #[case(Category::Critical, "critical")]
+    #[case(Category::Expected, "expected")]
+    #[case(Category::Unexpected, "unexpected")]
+    fn test_classify_mask_and_capture_critical_struct(#[case] category: Category, #[case] expected_tag: String) {
+        let error = SpecificError { category };
+        let mut events = with_captured_events(|| {
             classify_mask_and_capture(&error);
         });
         assert_eq!(events.len(), 1);
-        assert_eq!(events[0].level, Level::Error);
-        assert_eq!(events[0].exception.values.len(), 1);
+        let event = events.remove(0);
+        assert_eq!(event.level, Level::Error);
+        assert_eq!(event.exception.values.len(), 1);
         assert_eq!(
-            events[0].exception.values[0].ty,
+            event.exception.values[0].ty,
             "error_category::sentry::tests::SpecificError".to_string()
         );
-        assert_eq!(events[0].exception.values[0].value, Some(ERROR_MSG.to_string()));
-        assert!(format!("{:?}", events[0]).contains(ERROR_MSG));
-    }
-
-    #[test]
-    fn test_classify_mask_and_capture_personal_data() {
-        let error = ErrorEnum::Specific(SpecificError {
-            category: Category::PersonalData,
-        });
-        let events = with_captured_events(|| {
-            classify_mask_and_capture(&error);
-        });
-        assert_eq!(events.len(), 1);
-        assert_eq!(events[0].level, Level::Error);
-        assert_eq!(events[0].exception.values[0].ty, "SpecificError".to_string());
-        assert_eq!(
-            events[0].exception.values[1].ty,
-            "error_category::sentry::tests::ErrorEnum::Specific".to_string()
-        );
-        assert_eq!(events[0].exception.values[0].value, None);
-        assert_eq!(events[0].exception.values[1].value, None);
-        assert!(!format!("{:?}", events[0]).contains(ERROR_MSG));
+        assert_eq!(event.exception.values[0].value, Some(ERROR_MSG.to_string()));
+        assert!(format!("{:?}", event).contains(ERROR_MSG));
+        let category = event.tags.get("category");
+        assert_eq!(category, Some(&expected_tag));
     }
 }
