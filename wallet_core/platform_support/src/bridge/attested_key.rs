6845659781f4c51f9b7ee9ca1d86ef9ce25adfaf
Ruud Klaver <ruud@pantsforcats.nl>
2024-11-04 14:44:19 +0100

    Add more attested keys documentation

--- wallet_core/platform_support/src/bridge/attested_key.rs
+++ wallet_core/platform_support/src/bridge/attested_key.rs
@@ -4,7 +4,7 @@ use async_trait::async_trait;
 
 use super::get_platform_support;
 
-// Implementation of AttestedKeyError from UDL
+/// Implementation of `AttestedKeyError` from the UDL file.
 #[derive(Debug, thiserror::Error)]
 pub enum AttestedKeyError {
     #[error("key/app attestation is not supported on this device")]
@@ -17,13 +17,13 @@ pub enum AttestedKeyError {
     Other { reason: String },
 }
 
-// Implementation of AttestedKeyType from UDL
+/// Implementation of `AttestedKeyType`` from the UDL file.
 pub enum AttestedKeyType {
     Apple,
     Google,
 }
 
-// Implementation of AttestationData from UDL
+/// Implementation of `AttestationData`` from the UDL file.
 pub enum AttestationData {
     Apple {
         attestation_data: Vec<u8>,
@@ -34,7 +34,7 @@ pub enum AttestationData {
     },
 }
 
-// Implementation of AttestedKeyBridge from UDL
+/// Implementation of `AttestedKeyBridge` from the UDL file.
 // Unfortunately we cannot use the built-in support for traits with async methods,
 // as those are not object safe, so we have to rely on the async_trait crate instead.
 #[async_trait]
@@ -49,6 +49,8 @@ pub trait AttestedKeyBridge: Send + Sync + Debug {
     async fn delete(&self, identifier: String) -> Result<(), AttestedKeyError>;
 }
 
+/// Convenience function to access the a reference to `AttestedKeyBridge`,
+/// as set by by the native implementation.
 pub fn get_attested_key_bridge() -> &'static dyn AttestedKeyBridge {
     get_platform_support().attested_key.as_ref()
 }
