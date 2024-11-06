fa32530173f76bb090dda32c1cd86f00fc32eaf1
Sietse Ringers <mail@sietseringers.net>
2024-11-06 16:31:19 +0100

    Rename WS migration file

--- wallet_core/wallet_server/migration/src/lib.rs
+++ wallet_core/wallet_server/migration/src/lib.rs
@@ -2,7 +2,7 @@ use async_trait::async_trait;
 
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
+mod m20220101_000001_create_session_table;
 mod m20241023_095134_create_wte_table;
 
 pub struct Migrator;
@@ -11,7 +11,7 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_table::Migration),
+            Box::new(m20220101_000001_create_session_table::Migration),
             Box::new(m20241023_095134_create_wte_table::Migration),
         ]
     }
