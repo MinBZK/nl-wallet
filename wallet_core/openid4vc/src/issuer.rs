d4133a22ea6caa24f8da82dd3a05e3312d175d33
Arjen <edi@minbzk.nl>
2024-11-15 07:41:09 +0000

    Fix unused_async and needless_pass_by_value
    * Add unused_async, unused_self and unnecessary_wraps to clippy lints
    
    * Fix unused_async and needless_pass_by_value

--- wallet_core/openid4vc/src/issuer.rs
+++ wallet_core/openid4vc/src/issuer.rs
@@ -658,17 +658,17 @@ impl Session<WaitingForResponse> {
         (result, next)
     }
 
-    pub async fn check_credential_endpoint_access(
+    pub fn check_credential_endpoint_access(
         &self,
-        access_token: AccessToken,
-        dpop: Dpop,
+        access_token: &AccessToken,
+        dpop: &Dpop,
         endpoint: &str,
         issuer_data: &IssuerData<impl KeyRing, impl WteTracker>,
     ) -> Result<(), CredentialRequestError> {
         let session_data = self.session_data();
 
         // Check authorization of the request
-        if session_data.access_token != access_token {
+        if session_data.access_token != *access_token {
             return Err(CredentialRequestError::Unauthorized);
         }
 
@@ -677,7 +677,7 @@ impl Session<WaitingForResponse> {
             &session_data.dpop_public_key,
             &issuer_data.server_url.join(endpoint),
             &Method::POST,
-            Some(&access_token),
+            Some(access_token),
             Some(&session_data.dpop_nonce),
         )
         .map_err(|err| CredentialRequestError::IssuanceError(IssuanceError::DpopInvalid(err)))?;
@@ -731,8 +731,7 @@ impl Session<WaitingForResponse> {
     ) -> Result<CredentialResponse, CredentialRequestError> {
         let session_data = self.session_data();
 
-        self.check_credential_endpoint_access(access_token, dpop, "credential", issuer_data)
-            .await?;
+        self.check_credential_endpoint_access(&access_token, &dpop, "credential", issuer_data)?;
 
         // If we have exactly one credential on offer that matches the credential type that the client is
         // requesting, then we issue that credential.
@@ -753,9 +752,7 @@ impl Session<WaitingForResponse> {
             _ => Err(CredentialRequestError::UseBatchIssuance),
         }?;
 
-        let holder_pubkey = credential_request
-            .verify(&session_data.c_nonce, preview.credential_type(), issuer_data)
-            .await?;
+        let holder_pubkey = credential_request.verify(&session_data.c_nonce, preview.credential_type(), issuer_data)?;
 
         self.verify_wte_and_poa(
             credential_request.attestations,
@@ -803,8 +800,7 @@ impl Session<WaitingForResponse> {
     ) -> Result<CredentialResponses, CredentialRequestError> {
         let session_data = self.session_data();
 
-        self.check_credential_endpoint_access(access_token, dpop, "batch_credential", issuer_data)
-            .await?;
+        self.check_credential_endpoint_access(&access_token, &dpop, "batch_credential", issuer_data)?;
 
         let previews_and_holder_pubkeys = try_join_all(
             credential_requests
@@ -818,9 +814,7 @@ impl Session<WaitingForResponse> {
                         .flat_map(|preview| itertools::repeat_n(preview.clone(), preview.copy_count().into())),
                 )
                 .map(|(cred_req, preview)| async move {
-                    let key = cred_req
-                        .verify(&session_data.c_nonce, preview.credential_type(), issuer_data)
-                        .await?;
+                    let key = cred_req.verify(&session_data.c_nonce, preview.credential_type(), issuer_data)?;
 
                     Ok::<_, CredentialRequestError>((preview, key))
                 }),
@@ -890,7 +884,7 @@ impl CredentialPreview {
 }
 
 impl CredentialRequest {
-    pub(crate) async fn verify(
+    pub(crate) fn verify(
         &self,
         c_nonce: &str,
         expected_credential_type: Option<&str>,
