f29a94b9b9d6e7e39990c495a3127f9c0086fa9e
peterpaul <pp.kleinhaneveld@gmail.com>
2024-11-28 11:17:59 +0100

    Make log and android_logger dependencies optional
    use `log` crate in `print_panic`

--- wallet_core/platform_support/src/hardware_test/mod.rs
+++ wallet_core/platform_support/src/hardware_test/mod.rs
@@ -6,6 +6,23 @@ use std::panic::UnwindSafe;
 use std::panic::{self};
 use std::process;
 
+#[cfg(target_os = "android")]
+fn print_panic<F>(task: F)
+where
+    F: FnOnce() + UnwindSafe,
+{
+    if let Err(error) = panic::catch_unwind(task) {
+        if let Some(panic_message) = error.downcast_ref::<String>() {
+            log::error!("Rust panic: {}", panic_message);
+        } else {
+            log::error!("Rust panic of unknown type occurred");
+        }
+
+        process::abort();
+    }
+}
+
+#[cfg(not(target_os = "android"))]
 fn print_panic<F>(task: F)
 where
     F: FnOnce() + UnwindSafe,
