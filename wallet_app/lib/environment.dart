4356df8e393e0728f841a0e1f04665ad9407878b
Jippe Holwerda <mail@jippeholwerda.nl>
2024-12-24 11:34:16 +0100

    Set mockRepositories to be false by default

--- wallet_app/lib/environment.dart
+++ wallet_app/lib/environment.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class Environment {
   Environment._();
 
-  static bool get mockRepositories => const bool.fromEnvironment('MOCK_REPOSITORIES', defaultValue: true);
+  static bool get mockRepositories => const bool.fromEnvironment('MOCK_REPOSITORIES', defaultValue: false);
 
   static bool get isTest => Platform.environment.containsKey('FLUTTER_TEST');
 
