07798c0576c7494bb0feb99cc485b72e9cc7918c
Ruud Klaver <ruud@pantsforcats.nl>
2024-12-31 10:25:08 +0100

    Streamline cargo features for wallet_server tests
    This also makes the `issuance` feature optional for the
    `wallet_server_settings` test in that crate.

--- wallet_core/wallet_server/tests/wallet_server_settings.rs
+++ wallet_core/wallet_server/tests/wallet_server_settings.rs
@@ -3,7 +3,6 @@ use std::collections::HashMap;
 use assert_matches::assert_matches;
 
 use nl_wallet_mdoc::server_keys::KeyPair;
-use nl_wallet_mdoc::utils::issuer_auth::IssuerRegistration;
 use nl_wallet_mdoc::utils::reader_auth::ReaderRegistration;
 use nl_wallet_mdoc::utils::x509::CertificateError;
 use openid4vc::verifier::SessionTypeReturnUrl;
@@ -23,23 +22,27 @@ fn test_settings_success() {
     let mut settings =
         Settings::new_custom("ws_integration_test.toml", "ws_integration_test").expect("default settings");
 
-    let issuer_ca = KeyPair::generate_issuer_mock_ca().expect("generate issuer CA");
     let reader_ca = KeyPair::generate_reader_mock_ca().expect("generate reader CA");
-
-    let issuer_cert_valid = issuer_ca
-        .generate_issuer_mock(IssuerRegistration::new_mock().into())
-        .expect("generate valid issuer cert");
-
     let reader_cert_valid = reader_ca
         .generate_reader_mock(ReaderRegistration::new_mock().into())
         .expect("generate valid reader cert");
 
-    settings.issuer_trust_anchors = vec![issuer_ca.to_trust_anchor().unwrap()];
-    settings.issuer.private_keys.clear();
-    settings
-        .issuer
-        .private_keys
-        .insert("com.example.valid".to_string(), issuer_cert_valid.into());
+    #[cfg(feature = "issuance")]
+    {
+        let issuer_ca = KeyPair::generate_issuer_mock_ca().expect("generate issuer CA");
+        use nl_wallet_mdoc::utils::issuer_auth::IssuerRegistration;
+
+        let issuer_cert_valid = issuer_ca
+            .generate_issuer_mock(IssuerRegistration::new_mock().into())
+            .expect("generate valid issuer cert");
+
+        settings.issuer_trust_anchors = vec![issuer_ca.to_trust_anchor().unwrap()];
+        settings.issuer.private_keys.clear();
+        settings
+            .issuer
+            .private_keys
+            .insert("com.example.valid".to_string(), issuer_cert_valid.into());
+    }
 
     let mut usecases: HashMap<String, VerifierUseCase> = HashMap::new();
     usecases.insert("valid".to_string(), to_use_case(reader_cert_valid));
@@ -50,18 +53,20 @@ fn test_settings_success() {
     settings.verify_key_pairs().expect("should succeed");
 }
 
+#[cfg(feature = "issuance")]
 #[test]
 fn test_settings_no_issuer_trust_anchors() {
+    use nl_wallet_mdoc::utils::issuer_auth::IssuerRegistration;
+
     let mut settings =
         Settings::new_custom("ws_integration_test.toml", "ws_integration_test").expect("default settings");
 
     let issuer_ca = KeyPair::generate_issuer_mock_ca().expect("generate issuer CA");
-    let reader_ca = KeyPair::generate_reader_mock_ca().expect("generate reader CA");
-
     let issuer_cert_valid = issuer_ca
         .generate_issuer_mock(IssuerRegistration::new_mock().into())
         .expect("generate valid issuer cert");
 
+    let reader_ca = KeyPair::generate_reader_mock_ca().expect("generate reader CA");
     let reader_cert_valid = reader_ca
         .generate_reader_mock(ReaderRegistration::new_mock().into())
         .expect("generate valid reader cert");
@@ -88,23 +93,27 @@ fn test_settings_no_reader_trust_anchors() {
     let mut settings =
         Settings::new_custom("ws_integration_test.toml", "ws_integration_test").expect("default settings");
 
-    let issuer_ca = KeyPair::generate_issuer_mock_ca().expect("generate issuer CA");
     let reader_ca = KeyPair::generate_reader_mock_ca().expect("generate reader CA");
-
-    let issuer_cert_valid = issuer_ca
-        .generate_issuer_mock(IssuerRegistration::new_mock().into())
-        .expect("generate valid issuer cert");
-
     let reader_cert_valid = reader_ca
         .generate_reader_mock(ReaderRegistration::new_mock().into())
         .expect("generate valid reader cert");
 
-    settings.issuer_trust_anchors = vec![issuer_ca.to_trust_anchor().unwrap()];
-    settings.issuer.private_keys.clear();
-    settings
-        .issuer
-        .private_keys
-        .insert("com.example.valid".to_string(), issuer_cert_valid.into());
+    #[cfg(feature = "issuance")]
+    {
+        let issuer_ca = KeyPair::generate_issuer_mock_ca().expect("generate issuer CA");
+        use nl_wallet_mdoc::utils::issuer_auth::IssuerRegistration;
+
+        let issuer_cert_valid = issuer_ca
+            .generate_issuer_mock(IssuerRegistration::new_mock().into())
+            .expect("generate valid issuer cert");
+
+        settings.issuer_trust_anchors = vec![issuer_ca.to_trust_anchor().unwrap()];
+        settings.issuer.private_keys.clear();
+        settings
+            .issuer
+            .private_keys
+            .insert("com.example.valid".to_string(), issuer_cert_valid.into());
+    }
 
     let mut usecases: HashMap<String, VerifierUseCase> = HashMap::new();
     usecases.insert("valid".to_string(), to_use_case(reader_cert_valid));
@@ -121,13 +130,7 @@ fn test_settings_no_reader_registration() {
     let mut settings =
         Settings::new_custom("ws_integration_test.toml", "ws_integration_test").expect("default settings");
 
-    let issuer_ca = KeyPair::generate_issuer_mock_ca().expect("generate issuer CA");
     let reader_ca = KeyPair::generate_reader_mock_ca().expect("generate reader CA");
-
-    let issuer_cert_valid = issuer_ca
-        .generate_issuer_mock(IssuerRegistration::new_mock().into())
-        .expect("generate valid issuer cert");
-
     let reader_cert_valid = reader_ca
         .generate_reader_mock(ReaderRegistration::new_mock().into())
         .expect("generate valid reader cert");
@@ -135,12 +138,22 @@ fn test_settings_no_reader_registration() {
         .generate_reader_mock(None)
         .expect("generate reader cert without reader registration");
 
-    settings.issuer_trust_anchors = vec![issuer_ca.to_trust_anchor().unwrap()];
-    settings.issuer.private_keys.clear();
-    settings
-        .issuer
-        .private_keys
-        .insert("com.example.valid".to_string(), issuer_cert_valid.into());
+    #[cfg(feature = "issuance")]
+    {
+        let issuer_ca = KeyPair::generate_issuer_mock_ca().expect("generate issuer CA");
+        use nl_wallet_mdoc::utils::issuer_auth::IssuerRegistration;
+
+        let issuer_cert_valid = issuer_ca
+            .generate_issuer_mock(IssuerRegistration::new_mock().into())
+            .expect("generate valid issuer cert");
+
+        settings.issuer_trust_anchors = vec![issuer_ca.to_trust_anchor().unwrap()];
+        settings.issuer.private_keys.clear();
+        settings
+            .issuer
+            .private_keys
+            .insert("com.example.valid".to_string(), issuer_cert_valid.into());
+    }
 
     let mut usecases: HashMap<String, VerifierUseCase> = HashMap::new();
     usecases.insert("valid".to_string(), to_use_case(reader_cert_valid));
@@ -158,26 +171,31 @@ fn test_settings_wrong_reader_ca() {
     let mut settings =
         Settings::new_custom("ws_integration_test.toml", "ws_integration_test").expect("default settings");
 
-    let issuer_ca = KeyPair::generate_issuer_mock_ca().expect("generate issuer CA");
     let reader_ca = KeyPair::generate_reader_mock_ca().expect("generate reader CA");
-
-    let issuer_cert_valid = issuer_ca
-        .generate_issuer_mock(IssuerRegistration::new_mock().into())
-        .expect("generate valid issuer cert");
-
     let reader_cert_valid = reader_ca
         .generate_reader_mock(ReaderRegistration::new_mock().into())
         .expect("generate valid reader cert");
-    let reader_cert_wrong_ca = issuer_ca
+    let reader_wrong_ca = KeyPair::generate_reader_mock_ca().expect("generate wrong reader CA");
+    let reader_cert_wrong_ca = reader_wrong_ca
         .generate_reader_mock(ReaderRegistration::new_mock().into())
-        .expect("generate reader cert on issuer CA");
+        .expect("generate reader cert on wrong CA");
 
-    settings.issuer_trust_anchors = vec![issuer_ca.to_trust_anchor().unwrap()];
-    settings.issuer.private_keys.clear();
-    settings
-        .issuer
-        .private_keys
-        .insert("com.example.valid".to_string(), issuer_cert_valid.into());
+    #[cfg(feature = "issuance")]
+    {
+        let issuer_ca = KeyPair::generate_issuer_mock_ca().expect("generate issuer CA");
+        use nl_wallet_mdoc::utils::issuer_auth::IssuerRegistration;
+
+        let issuer_cert_valid = issuer_ca
+            .generate_issuer_mock(IssuerRegistration::new_mock().into())
+            .expect("generate valid issuer cert");
+
+        settings.issuer_trust_anchors = vec![issuer_ca.to_trust_anchor().unwrap()];
+        settings.issuer.private_keys.clear();
+        settings
+            .issuer
+            .private_keys
+            .insert("com.example.valid".to_string(), issuer_cert_valid.into());
+    }
 
     let mut usecases: HashMap<String, VerifierUseCase> = HashMap::new();
     usecases.insert("valid".to_string(), to_use_case(reader_cert_valid));
@@ -190,8 +208,11 @@ fn test_settings_wrong_reader_ca() {
     assert_matches!(error, CertificateVerificationError::InvalidCertificate(CertificateError::Verification(_), key) if key == "wrong_ca");
 }
 
+#[cfg(feature = "issuance")]
 #[test]
 fn test_settings_no_issuer_registration() {
+    use nl_wallet_mdoc::utils::issuer_auth::IssuerRegistration;
+
     let mut settings =
         Settings::new_custom("ws_integration_test.toml", "ws_integration_test").expect("default settings");
 
@@ -231,8 +252,11 @@ fn test_settings_no_issuer_registration() {
     assert_matches!(error, CertificateVerificationError::IncompleteCertificateType(key) if key == "com.example.no_registration");
 }
 
+#[cfg(feature = "issuance")]
 #[test]
 fn test_settings_wrong_issuer_ca() {
+    use nl_wallet_mdoc::utils::issuer_auth::IssuerRegistration;
+
     let mut settings =
         Settings::new_custom("ws_integration_test.toml", "ws_integration_test").expect("default settings");
 
