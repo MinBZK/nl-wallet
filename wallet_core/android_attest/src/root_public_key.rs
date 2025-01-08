7523dd85390d379700ae499bdfda85091593580a
Ruud Klaver <ruud@pantsforcats.nl>
2025-01-08 18:54:32 +0100

    Implement comparison of Android root public key
    Compare the `RootPublicKey` type to `PublicKey` from the `x509_parser`
    crate, using `PartialEq` implementations.

--- wallet_core/android_attest/src/root_public_key.rs
+++ wallet_core/android_attest/src/root_public_key.rs
@@ -2,8 +2,11 @@ use std::sync::LazyLock;
 
 use const_decoder::Pem;
 use p256::ecdsa::VerifyingKey;
+use rsa::traits::PublicKeyParts;
+use rsa::BigUint;
 use rsa::RsaPublicKey;
 use spki::DecodePublicKey;
+use x509_parser::public_key::PublicKey;
 
 const GOOGLE_ROOT_PUBKEY_DER: [u8; 550] =
     Pem::decode(include_bytes!("../assets/google_hardware_attestation_root_pubkey.pem"));
@@ -23,7 +26,7 @@ pub static EMULATOR_PUBKEYS: LazyLock<Vec<RootPublicKey>> = LazyLock::new(|| {
     ]
 });
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum RootPublicKey {
     Rsa(RsaPublicKey),
     Ecdsa(VerifyingKey),
@@ -36,5 +39,81 @@ impl RootPublicKey {
 
     pub fn ecdsa_from_der(der: impl AsRef<[u8]>) -> Result<Self, spki::Error> {
         VerifyingKey::from_public_key_der(der.as_ref()).map(Self::Ecdsa)
+    }
+}
+
+impl<'a> PartialEq<PublicKey<'a>> for RootPublicKey {
+    fn eq(&self, other: &PublicKey<'a>) -> bool {
+        match (self, other) {
+            (Self::Rsa(public_key), PublicKey::RSA(other_key)) => {
+                let modulus = BigUint::from_bytes_be(other_key.modulus);
+                let exponent = BigUint::from_bytes_be(other_key.exponent);
+
+                *public_key.n() == modulus && *public_key.e() == exponent
+            }
+            (Self::Ecdsa(verifying_key), PublicKey::EC(other_key)) => {
+                verifying_key.to_encoded_point(false).as_bytes() == other_key.data()
+            }
+            _ => false,
+        }
+    }
+}
+
+impl PartialEq<RootPublicKey> for PublicKey<'_> {
+    fn eq(&self, other: &RootPublicKey) -> bool {
+        other.eq(self)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use p256::ecdsa::SigningKey;
+    use rsa::RsaPrivateKey;
+    use rsa::RsaPublicKey;
+    use spki::EncodePublicKey;
+    use x509_parser::prelude::FromDer;
+    use x509_parser::x509::SubjectPublicKeyInfo;
+
+    use super::RootPublicKey;
+
+    #[test]
+    fn test_root_public_key_rsa_partial_eq() {
+        let mut thread_rng = rand::thread_rng();
+        let private_key = RsaPrivateKey::new(&mut thread_rng, 512).unwrap();
+        let public_key = RootPublicKey::Rsa(RsaPublicKey::from(&private_key));
+
+        let other_private_key = RsaPrivateKey::new(&mut thread_rng, 512).unwrap();
+        let other_public_key = RootPublicKey::Rsa(RsaPublicKey::from(&other_private_key));
+
+        let public_key_der = RsaPublicKey::from(&private_key).to_public_key_der().unwrap();
+        let (_, x509_public_key_info) = SubjectPublicKeyInfo::from_der(public_key_der.as_bytes()).unwrap();
+        let x509_public_key = x509_public_key_info.parsed().unwrap();
+
+        assert_eq!(public_key, x509_public_key);
+        assert_eq!(x509_public_key, public_key);
+
+        assert_ne!(other_public_key, x509_public_key);
+        assert_ne!(x509_public_key, other_public_key);
+    }
+
+    #[test]
+    fn test_root_public_key_ecdsa_partial_eq() {
+        let mut thread_rng = rand::thread_rng();
+
+        let signing_key = SigningKey::random(&mut thread_rng);
+        let verifying_key = RootPublicKey::Ecdsa(*signing_key.verifying_key());
+
+        let other_signing_key = SigningKey::random(&mut thread_rng);
+        let other_verifying_key = RootPublicKey::Ecdsa(*other_signing_key.verifying_key());
+
+        let public_key_der = signing_key.verifying_key().to_public_key_der().unwrap();
+        let (_, x509_public_key_info) = SubjectPublicKeyInfo::from_der(public_key_der.as_bytes()).unwrap();
+        let x509_public_key = x509_public_key_info.parsed().unwrap();
+
+        assert_eq!(verifying_key, x509_public_key);
+        assert_eq!(x509_public_key, verifying_key);
+
+        assert_ne!(other_verifying_key, x509_public_key);
+        assert_ne!(x509_public_key, other_verifying_key);
     }
 }
