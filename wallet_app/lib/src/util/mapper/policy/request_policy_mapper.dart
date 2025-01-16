395ed037db15a633180f7a781b16e05ff55a5f98
Jippe Holwerda <mail@jippeholwerda.nl>
2025-01-16 16:07:15 +0100

    Use let operator instead of null check

--- wallet_app/lib/src/util/mapper/policy/request_policy_mapper.dart
+++ wallet_app/lib/src/util/mapper/policy/request_policy_mapper.dart
@@ -1,16 +1,15 @@
 import 'package:wallet_core/core.dart';
 
-import '../../../domain/model/policy/policy.dart';
-import '../mapper.dart';
+import '/src/domain/model/policy/policy.dart';
+import '/src/util/extension/object_extension.dart';
+import '/src/util/mapper/mapper.dart';
 
 class RequestPolicyMapper extends Mapper<RequestPolicy, Policy> {
   RequestPolicyMapper();
 
   @override
   Policy map(RequestPolicy input) => Policy(
-        storageDuration: input.dataStorageDurationInMinutes == null
-            ? null
-            : Duration(minutes: input.dataStorageDurationInMinutes!.toInt()),
+        storageDuration: input.dataStorageDurationInMinutes?.let((it) => Duration(minutes: it.toInt())),
         dataIsShared: input.dataSharedWithThirdParties,
         deletionCanBeRequested: input.dataDeletionPossible,
         privacyPolicyUrl: input.policyUrl,
