d5486f9131210021a606d31f3dc654c855b55249
John Berendsen <john.berendsen@ictu.nl>
2025-01-29 16:59:41 +0000

    fix personalize E2E tests
    * undo local apk path change
    
    * fix personalize tests

--- uiautomation/src/test/kotlin/screen/personalize/PersonalizePidPreviewScreen.kt
+++ uiautomation/src/test/kotlin/screen/personalize/PersonalizePidPreviewScreen.kt
@@ -7,9 +7,9 @@ class PersonalizePidPreviewScreen : MobileActions() {
     private val screen = find.byValueKey("personalizePidPreviewPage")
 
     private val birthText = find.byText("24 maart 2000")
-    private val streetNameText = find.byText("Groenewoudsedijk")
-    private val postcodeText = find.byText("3528BG")
-    private val houseNumberText = find.byText("51")
+    private val streetNameText = find.byText("Van Wijngaerdenstraat")
+    private val postcodeText = find.byText("2596TW")
+    private val houseNumberText = find.byText("1")
 
     private val acceptButton = find.byValueKey("acceptButton")
     private val rejectButton = find.byValueKey("rejectButton")
@@ -19,12 +19,18 @@ class PersonalizePidPreviewScreen : MobileActions() {
     fun humanReadablePidDataVisible() =
         isElementVisible(birthText) && isElementVisible(streetNameText) && isElementVisible(postcodeText) && isElementVisible(houseNumberText)
 
-    fun confirmButtonsVisible() = isElementVisible(acceptButton) && isElementVisible(rejectButton)
+    fun confirmButtonsVisible(): Boolean {
+        scrollToEnd(ScrollableType.CustomScrollView)
+        return isElementVisible(acceptButton) && isElementVisible(rejectButton)
+    }
 
     fun clickAcceptButton() {
         scrollToEnd(ScrollableType.CustomScrollView)
         clickElement(acceptButton)
     }
 
-    fun clickRejectButton() = clickElement(rejectButton)
+    fun clickRejectButton() {
+        scrollToEnd(ScrollableType.CustomScrollView)
+        clickElement(rejectButton)
+    }
 }
