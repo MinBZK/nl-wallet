943817f4c936664f44ca505bbd52a9ddaa07c8ef
Jippe Holwerda <mail@jippeholwerda.nl>
2025-02-07 12:55:02 +0100

    Add a rationale for the flutter_api/models module

--- wallet_core/flutter_api/src/models/mod.rs
+++ wallet_core/flutter_api/src/models/mod.rs
@@ -1,4 +1,14 @@
-//! module doc
+//! This module contains models that represent data coming from `wallet-core` that should be represented in Flutter.
+//!
+//! Flutter-rust-bridge (frb) offers functionality to scan external crates and generate bridging code for it. However,
+//! there are quite some limitions to this approach:
+//! - unit structs cannot be parsed;
+//! - frb needs to parse and generate code for every type that is used in Flutter. Examples of external types that cause
+//!   problems are IndexMap and Uri;
+//!
+//! This leaves us to either use a subset of Rust for the models that need to be translated or create separate mapping
+//! models. The former option places severe limitions on how Rust can be used in `wallet-core`. The latter option is
+//! chosen here, even though it causes a lot duplication.
 
 pub mod attestation;
 pub mod config;
