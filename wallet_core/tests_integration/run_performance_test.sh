d7b7c1a0d4cc7958f075ebab873b4b63711fd8a4
Jacob Middag <jacob.middag@ictu.nl>
2025-02-13 15:39:26 +0000

    Fix performance test
    * Kill all jobs on interrupt
    
    * Allow to set tracing logging with RUST_LOG
    
    * Clean up tempdir on end (.into_path gives up on deletion)
    
    * Reduce concurrency on performance test and remove double wait

--- wallet_core/tests_integration/run_performance_test.sh
+++ wallet_core/tests_integration/run_performance_test.sh
@@ -2,31 +2,40 @@
 
 set -e
 
-SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPTS_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 NUM="${1:-1}"
 
 export CONFIG_ENV=${CONFIG_ENV:dev}
-cargo build --manifest-path "${SCRIPTS_DIR}/Cargo.toml" --release --bin performance_test --features performance_test,allow_insecure_url
+
+BINARY=${SCRIPTS_DIR}"/../target/release/performance_test"
+cargo build --manifest-path "${SCRIPTS_DIR}/Cargo.toml" \
+    --release --bin performance_test \
+    --features performance_test,allow_insecure_url
+
+START_DATE=$(date -u +%s)
 
 pids=()
+stop() {
+    kill ${pids[@]}
+}
+trap stop INT
+
 for ((i=1; i <= NUM; i++)); do
-  ("${SCRIPTS_DIR}"/../target/release/performance_test 2>&1) & pids+=($!)
+  (RUST_LOG=warn "$BINARY" 2>&1) & pids+=($!)
 done
 
-# wait and collect return codes
-rets=()
-for pid in ${pids[*]}; do
-    wait "$pid"
-    rets+=($?)
-done
-echo "Return codes: ${rets[*]}"
-
-error=false
-for pid in ${pids[*]}; do
+FAILED=0
+for pid in ${pids[@]}; do
     if ! wait "$pid"; then
-        error=true
+        FAILED=$((FAILED + 1))
     fi
 done
 
-if $error; then exit 1; fi
-exit 0
+END_DATE=$(date -u +%s)
+
+echo "Load: $NUM"
+echo "Duration: $(( END_DATE - START_DATE )) seconds"
+if [[ $FAILED -gt 0 ]]; then
+    echo "$FAILED out of $NUM failed"
+    exit 1
+fi
