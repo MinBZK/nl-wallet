863d472527f762912a0c94708d438b35f2d126c9
John Berendsen <john.berendsen@ictu.nl>
2025-02-14 07:19:44 +0000

    PVW-3622: fallback page tests
    * applied review comments and suggestion
    
    * update ci
    
    * ci updates
    
    * fallback page browsertest

---
+++ browsertests/packages/fallback-pages/pages/fallbackPage.js
@@ -0,0 +1,28 @@
+class FallbackPage {
+  constructor(page) {
+    this.page = page
+
+    this.deeplink = "#deeplink"
+    this.pageTitle = "h1"
+    this.storeBanners = ".store-banners"
+    this.helpLink = "footer .button-link"
+  }
+
+  async getPageTitle() {
+    return this.page.textContent(this.pageTitle)
+  }
+
+  async getDeeplink() {
+    return this.page.locator(this.deeplink)
+  }
+
+  async getStoreBanners() {
+    return this.page.locator(this.storeBanners)
+  }
+
+  async getHelplink() {
+    return this.page.locator(this.storeBanners)
+  }
+}
+
+export default { FallbackPage: FallbackPage }
