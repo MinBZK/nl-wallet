MinBZK/nl-wallet

commit feec202c723abb92f7e15fdf4c62033bcdef0c06

Drop openid dependency in favor of our own code
* Add missing space for consistency in openid4vc/Cargo.toml

* Fix OIDC discovery test

* Fix outdated rustdocs of OidcClient

* Add rustdoc to DigiD/rdo-max integration test on how to run it

* Fix some outdated comments in wallet issuance unit tests

* Fix order of openid4vc use statements in wallet

* Add happy flow test in OIDC client

* Fix alphabetical order of dev dependencies of openid4vc

* Improve url_with_query_pairs() readability

* Rename BsnMap to UserInfo

* Rename AuthBearerErrorType to AuthBearerErrorCode

* Reverse order of JWE algorithm error check

* Rename an OIDC unit test

* Remove leftover dbg!() in DigiD integration test

* Fix alphabetical order of imports in openid4vc/Cargo.toml

* Simplify OIDC client API

* Drop client_id setting from WS DigiD configuration

* Further simplify OIDC code in Wallet Server

* Drop openid dependency from wallet_server

* Improve some OIDC error handling

* Remove an unnecessary #[allow(dead_code)] from OIDC

* Simplify OIDC client errors

* Drop matches_received_redirect_uri from OIDC trait

* Rename new OIDC subcrate of openid4vc

* Rename various structs and functions of OIDC client

* Drop DigidSession trait, instead directly use OIDC client in wallet

* Add and reorganize OIDC tests

* Drop openid dependency from wallet

* Rename OIDC error type

* Make OpenID Connect client check redirect URI

* Split OpenID Connect functionality into several modules

* Make pkce, state, nonce fields of OpenID client not Optional

* Initial work on OIDC client

wallet_core/flutter_api/src/errors.rs
@@ -3,9 +3,12 @@ use std::{error::Error, fmt::Display};
 use anyhow::Chain;
 use serde::Serialize;
 
-use wallet::errors::{
-    openid, reqwest, AccountProviderError, DigidError, DisclosureError, HistoryError, InstructionError,
-    PidIssuanceError, UriIdentificationError, WalletInitError, WalletRegistrationError, WalletUnlockError,
+use wallet::{
+    errors::{
+        reqwest, AccountProviderError, DisclosureError, HistoryError, InstructionError, PidIssuanceError,
+        UriIdentificationError, WalletInitError, WalletRegistrationError, WalletUnlockError,
+    },
+    OidcError,
 };
 
 /// A type encapsulating data about a Flutter error that
@@ -125,15 +128,6 @@ impl FlutterApiErrorFields for PidIssuanceError {
         // within the error tree, just look for it with some help
         // from the `anyhow::Chain` iterator.
         for source in Chain::new(self) {
-            // Unfortunately `openid::error::Error` is a special case, because one of its
-            // variants holds a `reqwest::Error` with the `transparent` error attribute.
-            // This means that the `.source()` method will be forwarded directly to the contained
-            // error and the reqwest error itself will be skipped in the source chain!
-            // For this reason we need to extract it manually.
-            if let Some(openid::Error::Http(_)) = source.downcast_ref::<openid::Error>() {
-                return FlutterApiErrorType::Networking;
-            }
-
             if source.is::<reqwest::Error>() {
                 return FlutterApiErrorType::Networking;
             }
@@ -143,23 +137,20 @@ impl FlutterApiErrorFields for PidIssuanceError {
             PidIssuanceError::NotRegistered | PidIssuanceError::Locked | PidIssuanceError::SessionState => {
                 FlutterApiErrorType::WalletState
             }
-            PidIssuanceError::DigidSessionFinish(DigidError::RedirectUriError {
-                error: _,
-                error_description: _,
-            }) => FlutterApiErrorType::RedirectUri,
+            PidIssuanceError::DigidSessionFinish(OidcError::RedirectUriError(_)) => FlutterApiErrorType::RedirectUri,
+
             _ => FlutterApiErrorType::Generic,
         }
     }
 
     fn data(&self) -> Option<serde_json::Value> {
         match self {
-            Self::DigidSessionFinish(DigidError::RedirectUriError {
-                error,
-                error_description: _,
-            }) => [("redirect_error", error.clone())]
-                .into_iter()
-                .collect::<serde_json::Value>()
-                .into(),
+            Self::DigidSessionFinish(OidcError::RedirectUriError(err)) => {
+                [("redirect_error", format!("{:?}", &err.error))]
+                    .into_iter()
+                    .collect::<serde_json::Value>()
+                    .into()
+            }
             _ => None,
         }
     }

wallet_core/wallet/src/errors.rs
@@ -1,8 +1,4 @@
 // Expose some downstream errors that may be useful.
-pub mod openid {
-    pub use openid::error::Error;
-}
-
 pub mod reqwest {
     pub use reqwest::Error;
 }
@@ -10,7 +6,6 @@ pub mod reqwest {
 pub use crate::{
     account_provider::{AccountProviderError, AccountProviderResponseError},
     config::{ConfigurationError, FileStorageError},
-    digid::{DigidError, OpenIdError},
     document::{AttributeValueType, DocumentMdocError},
     instruction::{InstructionError, RemoteEcdsaKeyError},
     pin::{key::PinKeyError, validation::PinValidationError},

wallet_core/Cargo.toml
@@ -48,6 +48,7 @@ axum-extra = "0.8.0"
 axum-server = "0.5.1"
 backtrace = "0.3.67"
 base64 = "0.21.0"
+biscuit = "0.7.0"
 chrono = { version = "0.4.26", default-features = false }
 ciborium = "0.2.1"
 clap = "4.4.7"
@@ -79,7 +80,7 @@ mockall = "0.11.4"
 never = "0.1.0"
 nutype = "0.4.0"
 once_cell = "1.17.1"
-openid = { version = "0.12.0", default-features = false }
+openid = { version = "0.12.0", default-features = false } # TODO remove when pid_issuer is removed
 openssl = "0.10.6"
 oslog = { version = "0.2.0", default-features = false }
 p256 = { version = "0.13.2", default-features = false }

wallet_core/openid4vc/Cargo.toml
@@ -17,11 +17,13 @@ mock = ["dep:mockall"]
 
 [dependencies]
 base64.workspace = true
+biscuit.workspace = true
 chrono = { workspace = true, features = ["std", "clock"] }
 derive_more.workspace = true
 futures.workspace = true
 indexmap.workspace = true
 itertools.workspace = true
+josekit.workspace = true
 jsonwebtoken.workspace = true
 mime.workspace = true
 mockall = { workspace = true, optional = true }
@@ -40,8 +42,10 @@ wallet_common.path = "../wallet_common"
 nl_wallet_mdoc.path = "../mdoc"
 
 [dev-dependencies]
+assert_matches.workspace = true
 ciborium.workspace = true
-tokio = { workspace = true, features = ["macros"] }
-rstest.workspace = true
-nl_wallet_mdoc = { path = "../mdoc", features = ["mock", "software_key_factory", "generate"]}
 mockall.workspace = true
+nl_wallet_mdoc = { path = "../mdoc", features = ["mock", "software_key_factory", "generate"] }
+rstest.workspace = true
+tokio = { workspace = true, features = ["macros"] }
+wiremock.workspace = true

wallet_core/openid4vc/src/authorization.rs
@@ -1,11 +1,13 @@
+use indexmap::IndexSet;
 use serde::{Deserialize, Serialize};
-use serde_with::skip_serializing_none;
+use serde_with::{formats::SpaceSeparator, serde_as, skip_serializing_none, StringWithSeparator};
 use url::Url;
 
 /// https://openid.github.io/OpenID4VCI/openid-4-verifiable-credential-issuance-wg-draft.html#name-authorization-request
 /// and https://www.rfc-editor.org/rfc/rfc6749.html#section-4.1.1.
 /// When sent using [PAR (Pushed Authorization Requests)](https://datatracker.ietf.org/doc/html/rfc9126),
 /// it is usually sent URL-encoded in the request body to POST /op/par.
+#[serde_as]
 #[skip_serializing_none]
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct AuthorizationRequest {
@@ -13,7 +15,7 @@ pub struct AuthorizationRequest {
     pub client_id: String,
     pub redirect_uri: Option<Url>,
     pub state: Option<String>,
-    pub authorization_details: Vec<AuthorizationDetails>,
+    pub authorization_details: Option<Vec<AuthorizationDetails>>,
 
     /// https://datatracker.ietf.org/doc/html/rfc9126. MUST NOT be sent in a PAR.
     /// This is a `String` and not a `Url`, because despite its name it need not be an actual URL;
@@ -22,6 +24,11 @@ pub struct AuthorizationRequest {
 
     #[serde(flatten)]
     pub code_challenge: PkceCodeChallenge,
+
+    #[serde_as(as = "Option<StringWithSeparator::<SpaceSeparator, String>>")]
+    pub scope: Option<IndexSet<String>>,
+
+    pub nonce: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]

wallet_core/openid4vc/src/lib.rs
@@ -16,6 +16,8 @@ pub mod issuer;
 pub mod errors;
 pub use errors::*;
 
+pub mod oidc;
+
 #[cfg(feature = "mock")]
 pub mod mock;
 

wallet_core/openid4vc/src/oidc/config.rs
@@ -1,0 +1,224 @@
+//! OpenID discovery, loosely based on https://crates.io/crates/openid.
+
+use biscuit::{jwk::JWKSet, Empty};
+use indexmap::IndexSet;
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
+use url::Url;
+
+use super::OidcError;
+
+/// OpenID metadata as defind by https://openid.net/specs/openid-connect-discovery-1_0.html,
+/// to be published at `.well-known/openid-configuration`.
+#[skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Config {
+    pub issuer: Url,
+    pub authorization_endpoint: Url,
+    pub token_endpoint: Url,
+    #[serde(default)]
+    pub userinfo_endpoint: Option<Url>,
+    pub jwks_uri: Url,
+    #[serde(default)]
+    pub registration_endpoint: Option<Url>,
+    #[serde(default)]
+    pub scopes_supported: Option<IndexSet<String>>,
+    // There are only three valid response types, plus combinations of them, and none
+    // If we want to make these user friendly we want a struct to represent all 7 types
+    pub response_types_supported: IndexSet<String>,
+    // There are only two possible values here, query and fragment. Default is both.
+    #[serde(default)]
+    pub response_modes_supported: Option<IndexSet<String>>,
+    // Must support at least authorization_code and implicit.
+    #[serde(default)]
+    pub grant_types_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub acr_values_supported: Option<IndexSet<String>>,
+    // pairwise and public are valid by spec, but servers can add more
+    #[serde(default = "IndexSet::new")]
+    pub subject_types_supported: IndexSet<String>,
+    // Must include at least RS256, none is only allowed with response types without id tokens
+    #[serde(default = "IndexSet::new")]
+    pub id_token_signing_alg_values_supported: IndexSet<String>,
+    #[serde(default)]
+    pub id_token_encryption_alg_values_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub id_token_encryption_enc_values_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub userinfo_signing_alg_values_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub userinfo_encryption_alg_values_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub userinfo_encryption_enc_values_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub request_object_signing_alg_values_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub request_object_encryption_alg_values_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub request_object_encryption_enc_values_supported: Option<IndexSet<String>>,
+    // Spec options are client_secret_post, client_secret_basic, client_secret_jwt, private_key_jwt
+    // If omitted, client_secret_basic is used
+    #[serde(default)]
+    pub token_endpoint_auth_methods_supported: Option<IndexSet<String>>,
+    // Only wanted with jwt auth methods, should have RS256, none not allowed
+    #[serde(default)]
+    pub token_endpoint_auth_signing_alg_values_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub display_values_supported: Option<IndexSet<String>>,
+    // Valid options are normal, aggregated, and distributed. If omitted, only use normal
+    #[serde(default)]
+    pub claim_types_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub claims_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub service_documentation: Option<Url>,
+    #[serde(default)]
+    pub claims_locales_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub ui_locales_supported: Option<IndexSet<String>>,
+    #[serde(default)]
+    pub claims_parameter_supported: bool,
+    #[serde(default)]
+    pub request_parameter_supported: bool,
+    #[serde(default = "bool_value::<true>")]
+    pub request_uri_parameter_supported: bool,
+    #[serde(default)]
+    pub require_request_uri_registration: bool,
+
+    #[serde(default)]
+    pub op_policy_uri: Option<Url>,
+    #[serde(default)]
+    pub op_tos_uri: Option<Url>,
+    // This is a NONSTANDARD extension Google uses that is a part of the Oauth discovery draft
+    #[serde(default)]
+    pub code_challenge_methods_supported: Option<IndexSet<String>>,
+}
+
+impl Config {
+    pub(crate) async fn discover(client: &reqwest::Client, mut issuer: Url) -> Result<Self, OidcError> {
+        issuer
+            .path_segments_mut()
+            .map_err(|_| OidcError::CannotBeABase)?
+            .extend(&[".well-known", "openid-configuration"]);
+
+        let resp = client.get(issuer).send().await?.error_for_status()?;
+        resp.json().await.map_err(OidcError::from)
+    }
+
+    /// Get the JWK set from the given Url. Errors are either a reqwest error or an Insecure error if
+    /// the url isn't https.
+    pub(crate) async fn jwks(&self, client: &reqwest::Client) -> Result<JWKSet<Empty>, OidcError> {
+        let resp = client.get(self.jwks_uri.as_ref()).send().await?.error_for_status()?;
+        resp.json().await.map_err(OidcError::from)
+    }
+
+    /// Construct a new `Config` based on the OP's URL and some standardized or reasonable defaults.
+    #[cfg(test)] // Currently only used in tests
+    pub(crate) fn new(issuer: Url) -> Self {
+        Self {
+            issuer: issuer.clone(),
+            authorization_endpoint: issuer.join("/authorize").unwrap(),
+            token_endpoint: issuer.join("/token").unwrap(),
+            userinfo_endpoint: Some(issuer.join("/userinfo").unwrap()),
+            jwks_uri: issuer.join("/jwks.json").unwrap(),
+            registration_endpoint: None,
+            scopes_supported: Some(IndexSet::from_iter(["openid".to_string()])),
+            response_types_supported: IndexSet::from_iter(
+                ["code", "code id_token", "id_token", "id_token token"].map(str::to_string),
+            ),
+            response_modes_supported: None,
+            grant_types_supported: None,
+            acr_values_supported: None,
+            subject_types_supported: IndexSet::new(),
+            id_token_signing_alg_values_supported: IndexSet::from_iter(["RS256".to_string()]),
+            id_token_encryption_alg_values_supported: None,
+            id_token_encryption_enc_values_supported: None,
+            userinfo_signing_alg_values_supported: None,
+            userinfo_encryption_alg_values_supported: None,
+            userinfo_encryption_enc_values_supported: None,
+            request_object_signing_alg_values_supported: None,
+            request_object_encryption_alg_values_supported: None,
+            request_object_encryption_enc_values_supported: None,
+            token_endpoint_auth_methods_supported: None,
+            token_endpoint_auth_signing_alg_values_supported: None,
+            display_values_supported: None,
+            claim_types_supported: None,
+            claims_supported: None,
+            service_documentation: None,
+            claims_locales_supported: None,
+            ui_locales_supported: None,
+            claims_parameter_supported: false,
+            request_parameter_supported: false,
+            request_uri_parameter_supported: false,
+            require_request_uri_registration: false,
+            op_policy_uri: None,
+            op_tos_uri: None,
+            code_challenge_methods_supported: None,
+        }
+    }
+}
+
+const fn bool_value<const B: bool>() -> bool {
+    B
+}
+
+#[cfg(test)]
+pub mod tests {
+    use serde_json::json;
+    use url::Url;
+    use wiremock::{
+        matchers::{method, path},
+        Mock, MockServer, ResponseTemplate,
+    };
+
+    use super::Config;
+
+    pub async fn start_discovery_server() -> (MockServer, Url) {
+        let server = MockServer::start().await;
+        let server_url = Url::parse(&server.uri()).unwrap();
+
+        // Mock OpenID configuration endpoint
+        Mock::given(method("GET"))
+            .and(path("/.well-known/openid-configuration"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(json!({
+                "issuer": server_url,
+                "authorization_endpoint": server_url.join("/oauth2/authorize").unwrap(),
+                "token_endpoint": server_url.join("/oauth2/token").unwrap(),
+                "jwks_uri": server_url.join("/.well-known/jwks.json").unwrap(),
+                "response_types_supported": ["code", "id_token", "token id_token"],
+                "scopes_supported": ["openid"],
+            })))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        // Mock JWKS endpoint
+        Mock::given(method("GET"))
+            .and(path("/.well-known/jwks.json"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(json!({
+                "keys": []
+            })))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        (server, server_url)
+    }
+
+    #[tokio::test]
+    async fn test_discovery() {
+        let (_server, server_url) = start_discovery_server().await;
+        let http_client = reqwest::Client::new();
+
+        let discovered = Config::discover(&http_client, server_url.clone()).await.unwrap();
+
+        assert_eq!(&discovered.issuer, &server_url);
+        assert_eq!(
+            &discovered.authorization_endpoint,
+            &server_url.join("/oauth2/authorize").unwrap()
+        );
+
+        let jwks = discovered.jwks(&http_client).await.unwrap();
+        assert!(jwks.keys.is_empty());
+    }
+}

commit 6374cd5712d30a63ab0f8d88362ac20637a453fd

Pass original AuthorizationErrorCode to Flutter
This fixes a bug that was probably introduced in feec202, where the
error code returned by the DigiD connector was passed to Flutter in
PascalCase instead of snake_case.

wallet_core/flutter_api/src/errors.rs
@@ -223,7 +223,9 @@ impl FlutterApiErrorFields for PidIssuanceError {
     fn data(&self) -> serde_json::Value {
         match self {
             Self::DigidSessionFinish(DigidSessionError::Oidc(OidcError::RedirectUriError(err))) => {
-                [("redirect_error", format!("{:?}", &err.error))]
+                let auth_error_code = serde_json::to_value(err.error)
+                    .expect("a value of type AuthorizationErrorCode should always serialize");
+                [("redirect_error", auth_error_code)]
                     .into_iter()
                     .collect::<serde_json::Value>()
             }