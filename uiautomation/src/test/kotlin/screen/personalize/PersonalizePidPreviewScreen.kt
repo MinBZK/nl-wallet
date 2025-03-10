1a220c3c8267203ac810213e740d8074a8f21c5d
Daan <2034049+daandebie@users.noreply.github.com>
2025-03-10 15:20:43 +0000

    PVW-4091: Update DigiD flow (UI improvement)
    * Fix golden
    
    * Update goldens
    
    * Implement MR feedback
    
    * Update PID preview values
    
    * Update PID preview values
    
    * Add missing `setUp()` step
    
    * Fix text locator
    
    * Fix renamed method
    
    * Update e2e tests
    
    * Update goldens due to texts changes
    
    * Update personalize goldens
    
    * Update personalize intro & digid error screens
    
    * Remove no digid screen & tests
    
    * Remove digid help screen & tests
    
    * Sync Lokalise texts

--- uiautomation/src/test/kotlin/screen/personalize/PersonalizePidPreviewScreen.kt
+++ uiautomation/src/test/kotlin/screen/personalize/PersonalizePidPreviewScreen.kt
@@ -6,10 +6,10 @@ class PersonalizePidPreviewScreen : MobileActions() {
 
     private val screen = find.byValueKey("personalizePidPreviewPage")
 
-    private val birthText = find.byText("24 maart 2000")
-    private val streetNameText = find.byText("Van Wijngaerdenstraat")
-    private val postcodeText = find.byText("2596TW")
-    private val houseNumberText = find.byText("1")
+    private val birthText = find.byText("2000-03-24")
+    private val streetNameText = find.byText("Groenewoudsedijk")
+    private val postcodeText = find.byText("3528BG")
+    private val houseNumberText = find.byText("51")
 
     private val acceptButton = find.byValueKey("acceptButton")
     private val rejectButton = find.byValueKey("rejectButton")
@@ -17,7 +17,9 @@ class PersonalizePidPreviewScreen : MobileActions() {
     fun visible() = isElementVisible(screen)
 
     fun humanReadablePidDataVisible() =
-        isElementVisible(birthText) && isElementVisible(streetNameText) && isElementVisible(postcodeText) && isElementVisible(houseNumberText)
+        isElementVisible(birthText) && isElementVisible(streetNameText) && isElementVisible(postcodeText) && isElementVisible(
+            houseNumberText
+        )
 
     fun confirmButtonsVisible(): Boolean {
         scrollToEnd(ScrollableType.CustomScrollView)
