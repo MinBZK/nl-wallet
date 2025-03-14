217f26ab250dd63896c6f4a678af34a1bf3005ee
peterpaul <pp.kleinhaneveld@gmail.com>
2025-03-14 11:23:46 +0100

    Add todo

--- wallet_core/jwt/src/lib.rs
+++ wallet_core/jwt/src/lib.rs
@@ -4,5 +4,6 @@ pub mod jwt;
 
 pub use jwt::*;
 
+// TODO: remove this nl-wallet specific constant: PVW-4192
 /// Used as the `iss` field in various JWTs, identifying this wallet solution as the issuer of the JWT.
 pub const NL_WALLET_CLIENT_ID: &str = "https://wallet.edi.rijksoverheid.nl";
