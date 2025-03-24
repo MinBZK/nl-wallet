ddebf17eaf2d6deb53436c6d4bc2772e8b6b807c
Sietse Ringers <mail@sietseringers.net>
2025-03-24 11:35:33 +0100

    Remove useless default settings from pid_issuer

--- wallet_core/wallet_server/pid_issuer/src/settings.rs
+++ wallet_core/wallet_server/pid_issuer/src/settings.rs
@@ -61,9 +61,7 @@ impl ServerSettings for PidIssuerSettings {
                 default_store_timeouts.failed_deletion.as_secs() / 60,
             )?
             .set_default("wallet_client_ids", vec![jwt::NL_WALLET_CLIENT_ID.to_string()])?
-            .set_default("brp_server", "http://localhost:3007/")?
-            .set_default("valid_days", 365)?
-            .set_default("copy_count", 4)?;
+            .set_default("brp_server", "http://localhost:3007/")?;
 
         // Look for a config file that is in the same directory as Cargo.toml if run through cargo,
         // otherwise look in the current working directory.
