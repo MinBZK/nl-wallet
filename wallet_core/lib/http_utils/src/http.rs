89145621dc7b22036cd25d77d99fc496b04f74ea
peterpaul <pp.kleinhaneveld@gmail.com>
2025-04-04 13:48:51 +0200

    Add client feature to http_utils

--- wallet_core/lib/http_utils/src/http.rs
+++ wallet_core/lib/http_utils/src/http.rs
@@ -1,28 +1,38 @@
-use std::hash::DefaultHasher;
-use std::hash::Hash;
-use std::hash::Hasher;
-#[cfg(feature = "axum")]
-use std::io;
-use std::path::Path;
+use cfg_if::cfg_if;
 
-#[cfg(feature = "axum")]
-use axum_server::tls_rustls::RustlsConfig;
-use derive_more::Debug;
-use http::Method;
-use reqwest::Client;
 use serde::Deserialize;
-use serde::Serialize;
 use serde_with::base64::Base64;
 use serde_with::serde_as;
 
-use crate::reqwest::tls_pinned_client_builder;
-use crate::reqwest::ClientBuilder;
-use crate::reqwest::JsonClientBuilder;
-use crate::reqwest::JsonReqwestBuilder;
-use crate::reqwest::RequestBuilder;
-use crate::reqwest::ReqwestBuilder;
-use crate::reqwest::ReqwestTrustAnchor;
-use crate::urls::BaseUrl;
+cfg_if! {
+    if #[cfg(feature = "axum")] {
+        use std::io;
+
+        use axum_server::tls_rustls::RustlsConfig;
+    }
+}
+
+cfg_if! {
+    if #[cfg(feature = "client")] {
+        use std::hash::DefaultHasher;
+        use std::hash::Hash;
+        use std::hash::Hasher;
+        use std::path::Path;
+
+        use http::Method;
+        use reqwest::Client;
+        use serde::Serialize;
+
+        use crate::reqwest::tls_pinned_client_builder;
+        use crate::reqwest::ClientBuilder;
+        use crate::reqwest::JsonClientBuilder;
+        use crate::reqwest::JsonReqwestBuilder;
+        use crate::reqwest::RequestBuilder;
+        use crate::reqwest::ReqwestBuilder;
+        use crate::reqwest::ReqwestTrustAnchor;
+        use crate::urls::BaseUrl;
+    }
+}
 
 #[serde_as]
 #[derive(Clone, Deserialize)]
@@ -36,8 +46,9 @@ pub struct TlsServerConfig {
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct TlsPinningConfigHash(u64);
 
+#[cfg(feature = "client")]
 #[serde_as]
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Hash)]
+#[derive(derive_more::Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Hash)]
 pub struct TlsPinningConfig {
     pub base_url: BaseUrl,
     #[debug(skip)]
@@ -45,39 +56,14 @@ pub struct TlsPinningConfig {
     pub trust_anchors: Vec<ReqwestTrustAnchor>,
 }
 
+#[cfg(feature = "client")]
 impl TlsPinningConfig {
     pub fn to_hash(&self) -> TlsPinningConfigHash {
         let mut hasher = DefaultHasher::new();
         self.hash(&mut hasher);
         TlsPinningConfigHash(hasher.finish())
     }
-}
 
-impl ClientBuilder for TlsPinningConfig {
-    fn builder(&self) -> reqwest::ClientBuilder {
-        tls_pinned_client_builder(self.certificates())
-    }
-}
-
-impl JsonClientBuilder for TlsPinningConfig {}
-
-impl RequestBuilder for TlsPinningConfig {
-    fn request(&self, method: Method, path: impl AsRef<Path>) -> (reqwest::Client, reqwest::RequestBuilder) {
-        let client = self.client();
-        let request = self.request_with_client(&client, method, &path);
-        (client, request)
-    }
-
-    fn request_with_client(&self, client: &Client, method: Method, path: impl AsRef<Path>) -> reqwest::RequestBuilder {
-        client.request(method, self.base_url.join(&path.as_ref().to_string_lossy()))
-    }
-}
-
-impl ReqwestBuilder for TlsPinningConfig {}
-
-impl JsonReqwestBuilder for TlsPinningConfig {}
-
-impl TlsPinningConfig {
     fn client(&self) -> reqwest::Client {
         self.builder()
             .build()
@@ -92,6 +78,36 @@ impl TlsPinningConfig {
     }
 }
 
+
+#[cfg(feature = "client")]
+impl ClientBuilder for TlsPinningConfig {
+    fn builder(&self) -> reqwest::ClientBuilder {
+        tls_pinned_client_builder(self.certificates())
+    }
+}
+
+#[cfg(feature = "client")]
+impl JsonClientBuilder for TlsPinningConfig {}
+
+#[cfg(feature = "client")]
+impl RequestBuilder for TlsPinningConfig {
+    fn request(&self, method: Method, path: impl AsRef<Path>) -> (reqwest::Client, reqwest::RequestBuilder) {
+        let client = self.client();
+        let request = self.request_with_client(&client, method, &path);
+        (client, request)
+    }
+
+    fn request_with_client(&self, client: &Client, method: Method, path: impl AsRef<Path>) -> reqwest::RequestBuilder {
+        client.request(method, self.base_url.join(&path.as_ref().to_string_lossy()))
+    }
+}
+
+#[cfg(feature = "client")]
+impl ReqwestBuilder for TlsPinningConfig {}
+
+#[cfg(feature = "client")]
+impl JsonReqwestBuilder for TlsPinningConfig {}
+
 #[cfg(feature = "axum")]
 impl TlsServerConfig {
     pub async fn to_rustls_config(&self) -> Result<RustlsConfig, io::Error> {
@@ -99,7 +115,7 @@ impl TlsServerConfig {
     }
 }
 
-#[cfg(feature = "insecure_http_client")]
+#[cfg(all(feature = "client", feature = "insecure_http_client"))]
 pub mod test {
     use std::path::Path;
 
--- wallet_core/lib/http_utils/src/lib.rs
+++ wallet_core/lib/http_utils/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod http;
 pub mod http_error;
+#[cfg(feature = "client")]
 pub mod reqwest;
 pub mod urls;
