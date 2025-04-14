87289f0e5c81f66ecafbac9b8f3aefbc911f0793
Jippe Holwerda <mail@jippeholwerda.nl>
2025-04-14 20:40:22 +0200

    Move metadata belonging to SD-JWT from the spec to separate spec example folder

--- wallet_core/lib/sd_jwt_vc_metadata/src/examples.rs
+++ wallet_core/lib/sd_jwt_vc_metadata/src/examples.rs
@@ -2,7 +2,7 @@ pub const EXAMPLE_METADATA_BYTES: &[u8] = include_bytes!("../examples/example-me
 pub const EXAMPLE_EXTENSION_METADATA_BYTES: &[u8] = include_bytes!("../examples/example-v2-metadata.json");
 pub const PID_METADATA_BYTES: &[u8] = include_bytes!("../examples/pid-metadata.json");
 pub const ADDRESS_METADATA_BYTES: &[u8] = include_bytes!("../examples/address-metadata.json");
-pub const SD_JWT_VC_SPEC_METADATA_BYTES: &[u8] = include_bytes!("../examples/sd_jwt_vc_spec_metadata.json");
+pub const SD_JWT_VC_SPEC_METADATA_BYTES: &[u8] = include_bytes!("../examples/spec/sd_jwt_vc_spec_metadata.json");
 pub const DIPLOMA_METADATA_BYTES: &[u8] = include_bytes!("../examples/diploma-metadata.json");
 pub const CREDENTIAL_PAYLOAD_SD_JWT_SPEC_METADATA_BYTES: &[u8] =
     include_bytes!("../examples/credential_payload_sd_jwt_metadata.json");
