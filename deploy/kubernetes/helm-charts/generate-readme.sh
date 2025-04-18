c69d01e52370d087a25e6a9d5b596da9fb7cb2b4
Jacob Middag <jacob.middag@ictu.nl>
2025-04-18 11:46:02 +0200

    Add script to generate README.md for helm-charts

---
+++ deploy/kubernetes/helm-charts/generate-readme.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BASE_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd -P)"
+
+if command -v readme-generator > /dev/null; then
+    GENERATOR='readme-generator'
+elif command -v npxsd > /dev/null; then
+    GENERATOR='npx @bitnami/readme-generator-for-helm'
+else
+    >&2 echo 'ERROR: Cannot find readme-generator or npx'
+    exit 1
+fi
+
+for values_file in $(find "$BASE_DIR" -type f -name 'values.yaml'); do
+    $GENERATOR -r "$(dirname "$values_file")/README.md" -v "$values_file"
+done
