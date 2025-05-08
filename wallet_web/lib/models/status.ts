6884552fc3137c9267cc78df20418561df4c53c2
Arjen <edi@minbzk.nl>
2025-05-08 13:10:47 +0000

    Add tracking PVW for TODO

--- wallet_web/lib/models/status.ts
+++ wallet_web/lib/models/status.ts
@@ -1,5 +1,5 @@
 export type StatusResponse =
-  | { status: "CREATED"; ul: URL } // TODO apparently the UL parameter is optional in the Rust code
+  | { status: "CREATED"; ul: URL } // TODO apparently the UL parameter is optional in the Rust code (PVW-4383)
   | { status: "WAITING_FOR_RESPONSE" }
   | { status: "DONE" }
   | { status: "FAILED" }
