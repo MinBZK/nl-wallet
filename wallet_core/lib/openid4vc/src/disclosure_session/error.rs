0b387d8c9b879f36f84aa48067af81f98e065240
Ruud Klaver <ruud@pantsforcats.nl>
2025-06-11 12:21:48 +0200

    Small DisclosureSession error submodule code fixes
    This commit only moves code around without making changes.

--- wallet_core/lib/openid4vc/src/disclosure_session/error.rs
+++ wallet_core/lib/openid4vc/src/disclosure_session/error.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use derive_more::Constructor;
 
 use attestation_data::auth::reader_auth::ValidationError;
@@ -16,6 +18,7 @@ use super::VpMessageClientError;
 pub enum VpSessionError {
     #[error("{0}")]
     Client(#[from] VpClientError),
+
     #[error("{0}")]
     Verifier(#[from] VpVerifierError),
 }
@@ -58,25 +61,34 @@ impl From<CertificateError> for VpSessionError {
 pub enum VpClientError {
     #[error("error sending OpenID4VP message: {0}")]
     Request(#[source] VpMessageClientError),
+
     #[error("error creating mdoc device response: {0}")]
     DeviceResponse(#[source] mdoc::Error),
+
     #[error("error matching requested attributes against mdocs: {0}")]
     MatchRequestedAttributes(#[source] mdoc::Error),
+
     #[error("multiple candidates for disclosure is unsupported, found for doc types: {}", .0.join(", "))]
-    #[category(pd)] // we don't want to leak information about what's in the wallet
+    // we don't want to leak information about what's in the wallet
+    #[category(pd)]
     MultipleCandidates(Vec<String>),
+
     #[error("error encrypting Authorization Response: {0}")]
     #[category(unexpected)]
     AuthResponseEncryption(#[source] AuthResponseError),
+
     #[error("error deserializing request_uri object: {0}")]
-    #[category(pd)] // we cannot be sure that the URL is not included in the error.
+    // we cannot be sure that the URL is not included in the error.
+    #[category(pd)]
     RequestUri(#[source] serde_urlencoded::de::Error),
+
     #[error("mismatch between session type and disclosure URI source: {0} not allowed from {1}")]
     #[category(critical)]
     DisclosureUriSourceMismatch(SessionType, DisclosureUriSource),
+
     #[error("error constructing PoA: {0}")]
     #[category(pd)]
-    Poa(#[source] Box<dyn std::error::Error + Send + Sync + 'static>),
+    Poa(#[source] Box<dyn Error + Send + Sync + 'static>),
 }
 
 #[derive(Debug, thiserror::Error, ErrorCategory)]
@@ -84,35 +96,43 @@ pub enum VpClientError {
 pub enum VpVerifierError {
     #[error("error verifying Authorization Request: {0}")]
     AuthRequestValidation(#[source] AuthRequestValidationError),
+
     #[error("incorrect client_id: expected {expected}, found {found}")]
     #[category(critical)]
     IncorrectClientId { expected: String, found: String },
+
     #[error("no reader registration in RP certificate")]
     #[category(critical)]
     MissingReaderRegistration,
+
     #[error("missing session_type query parameter in request URI")]
     #[category(critical)]
     MissingSessionType,
+
     #[error("malformed session_type query parameter in request URI: {0}")]
-    #[category(pd)] // we cannot be sure that the URL is not included in the error
+    // we cannot be sure that the URL is not included in the error
+    #[category(pd)]
     MalformedSessionType(#[source] serde_urlencoded::de::Error),
+
     #[error("error sending OpenID4VP message: {0}")]
     Request(#[source] VpMessageClientError),
+
     #[error("error validating requested attributes: {0}")]
     RequestedAttributesValidation(#[source] ValidationError),
+
     #[error("error parsing RP certificate: {0}")]
     RpCertificate(#[source] CertificateError),
 }
 
-#[derive(thiserror::Error, Debug, Constructor)]
+#[derive(Debug, Constructor, thiserror::Error)]
 #[error("could not perform actual disclosure, attributes were shared: {data_shared}, error: {error}")]
-pub struct DisclosureError<E: std::error::Error> {
+pub struct DisclosureError<E: Error> {
     pub data_shared: bool,
     #[source]
     pub error: E,
 }
 
-impl<E: std::error::Error> DisclosureError<E> {
+impl<E: Error> DisclosureError<E> {
     pub fn before_sharing(error: E) -> Self {
         Self {
             data_shared: false,
@@ -130,10 +150,11 @@ impl<E: std::error::Error> DisclosureError<E> {
 
 impl From<VpMessageClientError> for DisclosureError<VpClientError> {
     fn from(source: VpMessageClientError) -> Self {
-        let data_shared = match source {
-            VpMessageClientError::Http(ref reqwest_error) => !reqwest_error.is_connect(),
+        let data_shared = match &source {
+            VpMessageClientError::Http(reqwest_error) => !reqwest_error.is_connect(),
             _ => true,
         };
+
         Self::new(data_shared, VpClientError::Request(source))
     }
 }
