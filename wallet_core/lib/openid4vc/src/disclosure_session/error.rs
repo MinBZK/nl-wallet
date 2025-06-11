7ff2d59917c3fc54769f50a87a00a01cfbf2bef5
Ruud Klaver <ruud@pantsforcats.nl>
2025-06-11 11:57:47 +0200

    Move DisclosureSession error types to submodule
    This commit only moves code around without making changes.

---
+++ wallet_core/lib/openid4vc/src/disclosure_session/error.rs
@@ -0,0 +1,139 @@
+use derive_more::Constructor;
+
+use attestation_data::auth::reader_auth::ValidationError;
+use crypto::x509::CertificateError;
+use error_category::ErrorCategory;
+
+use crate::openid4vp::AuthRequestValidationError;
+use crate::openid4vp::AuthResponseError;
+use crate::verifier::SessionType;
+
+use super::DisclosureUriSource;
+use super::VpMessageClientError;
+
+#[derive(Debug, thiserror::Error, ErrorCategory)]
+#[category(defer)]
+pub enum VpSessionError {
+    #[error("{0}")]
+    Client(#[from] VpClientError),
+    #[error("{0}")]
+    Verifier(#[from] VpVerifierError),
+}
+
+impl From<VpMessageClientError> for VpSessionError {
+    fn from(source: VpMessageClientError) -> Self {
+        match &source {
+            VpMessageClientError::Json(_) => VpSessionError::Verifier(VpVerifierError::Request(source)),
+            _ => VpSessionError::Client(VpClientError::Request(source)),
+        }
+    }
+}
+
+impl From<AuthResponseError> for VpSessionError {
+    fn from(source: AuthResponseError) -> Self {
+        VpSessionError::Client(VpClientError::AuthResponseEncryption(source))
+    }
+}
+
+impl From<AuthRequestValidationError> for VpSessionError {
+    fn from(source: AuthRequestValidationError) -> Self {
+        VpSessionError::Verifier(VpVerifierError::AuthRequestValidation(source))
+    }
+}
+
+impl From<ValidationError> for VpSessionError {
+    fn from(source: ValidationError) -> Self {
+        VpSessionError::Verifier(VpVerifierError::RequestedAttributesValidation(source))
+    }
+}
+
+impl From<CertificateError> for VpSessionError {
+    fn from(source: CertificateError) -> Self {
+        VpSessionError::Verifier(VpVerifierError::RpCertificate(source))
+    }
+}
+
+#[derive(Debug, thiserror::Error, ErrorCategory)]
+#[category(defer)]
+pub enum VpClientError {
+    #[error("error sending OpenID4VP message: {0}")]
+    Request(#[source] VpMessageClientError),
+    #[error("error creating mdoc device response: {0}")]
+    DeviceResponse(#[source] mdoc::Error),
+    #[error("error matching requested attributes against mdocs: {0}")]
+    MatchRequestedAttributes(#[source] mdoc::Error),
+    #[error("multiple candidates for disclosure is unsupported, found for doc types: {}", .0.join(", "))]
+    #[category(pd)] // we don't want to leak information about what's in the wallet
+    MultipleCandidates(Vec<String>),
+    #[error("error encrypting Authorization Response: {0}")]
+    #[category(unexpected)]
+    AuthResponseEncryption(#[source] AuthResponseError),
+    #[error("error deserializing request_uri object: {0}")]
+    #[category(pd)] // we cannot be sure that the URL is not included in the error.
+    RequestUri(#[source] serde_urlencoded::de::Error),
+    #[error("mismatch between session type and disclosure URI source: {0} not allowed from {1}")]
+    #[category(critical)]
+    DisclosureUriSourceMismatch(SessionType, DisclosureUriSource),
+    #[error("error constructing PoA: {0}")]
+    #[category(pd)]
+    Poa(#[source] Box<dyn std::error::Error + Send + Sync + 'static>),
+}
+
+#[derive(Debug, thiserror::Error, ErrorCategory)]
+#[category(defer)]
+pub enum VpVerifierError {
+    #[error("error verifying Authorization Request: {0}")]
+    AuthRequestValidation(#[source] AuthRequestValidationError),
+    #[error("incorrect client_id: expected {expected}, found {found}")]
+    #[category(critical)]
+    IncorrectClientId { expected: String, found: String },
+    #[error("no reader registration in RP certificate")]
+    #[category(critical)]
+    MissingReaderRegistration,
+    #[error("missing session_type query parameter in request URI")]
+    #[category(critical)]
+    MissingSessionType,
+    #[error("malformed session_type query parameter in request URI: {0}")]
+    #[category(pd)] // we cannot be sure that the URL is not included in the error
+    MalformedSessionType(#[source] serde_urlencoded::de::Error),
+    #[error("error sending OpenID4VP message: {0}")]
+    Request(#[source] VpMessageClientError),
+    #[error("error validating requested attributes: {0}")]
+    RequestedAttributesValidation(#[source] ValidationError),
+    #[error("error parsing RP certificate: {0}")]
+    RpCertificate(#[source] CertificateError),
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("could not perform actual disclosure, attributes were shared: {data_shared}, error: {error}")]
+pub struct DisclosureError<E: std::error::Error> {
+    pub data_shared: bool,
+    #[source]
+    pub error: E,
+}
+
+impl<E: std::error::Error> DisclosureError<E> {
+    pub fn before_sharing(error: E) -> Self {
+        Self {
+            data_shared: false,
+            error,
+        }
+    }
+
+    pub fn after_sharing(error: E) -> Self {
+        Self {
+            data_shared: true,
+            error,
+        }
+    }
+}
+
+impl From<VpMessageClientError> for DisclosureError<VpClientError> {
+    fn from(source: VpMessageClientError) -> Self {
+        let data_shared = match source {
+            VpMessageClientError::Http(ref reqwest_error) => !reqwest_error.is_connect(),
+            _ => true,
+        };
+        Self::new(data_shared, VpClientError::Request(source))
+    }
+}
--- wallet_core/lib/openid4vc/src/disclosure_session/mod.rs
+++ wallet_core/lib/openid4vc/src/disclosure_session/mod.rs
@@ -1,6 +1,5 @@
 use std::hash::Hash;
 
-use derive_more::Constructor;
 use futures::TryFutureExt;
 use itertools::Itertools;
 use rustls_pki_types::TrustAnchor;
@@ -8,15 +7,12 @@ use tracing::info;
 use tracing::warn;
 
 use attestation_data::auth::reader_auth::ReaderRegistration;
-use attestation_data::auth::reader_auth::ValidationError;
 use attestation_data::identifiers::AttributeIdentifier;
 use attestation_data::x509::CertificateType;
 use crypto::factory::KeyFactory;
 use crypto::keys::CredentialEcdsaKey;
 use crypto::utils::random_string;
 use crypto::x509::BorrowingCertificate;
-use crypto::x509::CertificateError;
-use error_category::ErrorCategory;
 use http_utils::urls::BaseUrl;
 use mdoc::disclosure::DeviceResponse;
 use mdoc::engagement::SessionTranscript;
@@ -30,8 +26,6 @@ use utils::vec_at_least::VecAtLeastTwoUnique;
 use crate::errors::AuthorizationErrorCode;
 use crate::errors::ErrorResponse;
 use crate::errors::VpAuthorizationErrorCode;
-use crate::openid4vp::AuthRequestValidationError;
-use crate::openid4vp::AuthResponseError;
 use crate::openid4vp::IsoVpAuthorizationRequest;
 use crate::openid4vp::RequestUriMethod;
 use crate::openid4vp::VpAuthorizationRequest;
@@ -45,125 +39,13 @@ pub use self::client::VpMessageClient;
 pub use self::client::VpMessageClientError;
 pub use self::client::VpMessageClientErrorType;
 pub use self::client::APPLICATION_OAUTH_AUTHZ_REQ_JWT;
+pub use self::error::DisclosureError;
+pub use self::error::VpClientError;
+pub use self::error::VpSessionError;
+pub use self::error::VpVerifierError;
 
 mod client;
-
-#[derive(Debug, thiserror::Error, ErrorCategory)]
-#[category(defer)]
-pub enum VpSessionError {
-    #[error("{0}")]
-    Client(#[from] VpClientError),
-    #[error("{0}")]
-    Verifier(#[from] VpVerifierError),
-}
-
-impl From<VpMessageClientError> for VpSessionError {
-    fn from(source: VpMessageClientError) -> Self {
-        match &source {
-            VpMessageClientError::Json(_) => VpSessionError::Verifier(VpVerifierError::Request(source)),
-            _ => VpSessionError::Client(VpClientError::Request(source)),
-        }
-    }
-}
-
-impl From<AuthResponseError> for VpSessionError {
-    fn from(source: AuthResponseError) -> Self {
-        VpSessionError::Client(VpClientError::AuthResponseEncryption(source))
-    }
-}
-
-impl From<AuthRequestValidationError> for VpSessionError {
-    fn from(source: AuthRequestValidationError) -> Self {
-        VpSessionError::Verifier(VpVerifierError::AuthRequestValidation(source))
-    }
-}
-
-impl From<ValidationError> for VpSessionError {
-    fn from(source: ValidationError) -> Self {
-        VpSessionError::Verifier(VpVerifierError::RequestedAttributesValidation(source))
-    }
-}
-
-impl From<CertificateError> for VpSessionError {
-    fn from(source: CertificateError) -> Self {
-        VpSessionError::Verifier(VpVerifierError::RpCertificate(source))
-    }
-}
-
-#[derive(Debug, thiserror::Error, ErrorCategory)]
-#[category(defer)]
-pub enum VpClientError {
-    #[error("error sending OpenID4VP message: {0}")]
-    Request(#[source] VpMessageClientError),
-    #[error("error creating mdoc device response: {0}")]
-    DeviceResponse(#[source] mdoc::Error),
-    #[error("error matching requested attributes against mdocs: {0}")]
-    MatchRequestedAttributes(#[source] mdoc::Error),
-    #[error("multiple candidates for disclosure is unsupported, found for doc types: {}", .0.join(", "))]
-    #[category(pd)] // we don't want to leak information about what's in the wallet
-    MultipleCandidates(Vec<String>),
-    #[error("error encrypting Authorization Response: {0}")]
-    #[category(unexpected)]
-    AuthResponseEncryption(#[source] AuthResponseError),
-    #[error("error deserializing request_uri object: {0}")]
-    #[category(pd)] // we cannot be sure that the URL is not included in the error.
-    RequestUri(#[source] serde_urlencoded::de::Error),
-    #[error("mismatch between session type and disclosure URI source: {0} not allowed from {1}")]
-    #[category(critical)]
-    DisclosureUriSourceMismatch(SessionType, DisclosureUriSource),
-    #[error("error constructing PoA: {0}")]
-    #[category(pd)]
-    Poa(#[source] Box<dyn std::error::Error + Send + Sync + 'static>),
-}
-
-#[derive(Debug, thiserror::Error, ErrorCategory)]
-#[category(defer)]
-pub enum VpVerifierError {
-    #[error("error verifying Authorization Request: {0}")]
-    AuthRequestValidation(#[source] AuthRequestValidationError),
-    #[error("incorrect client_id: expected {expected}, found {found}")]
-    #[category(critical)]
-    IncorrectClientId { expected: String, found: String },
-    #[error("no reader registration in RP certificate")]
-    #[category(critical)]
-    MissingReaderRegistration,
-    #[error("missing session_type query parameter in request URI")]
-    #[category(critical)]
-    MissingSessionType,
-    #[error("malformed session_type query parameter in request URI: {0}")]
-    #[category(pd)] // we cannot be sure that the URL is not included in the error
-    MalformedSessionType(#[source] serde_urlencoded::de::Error),
-    #[error("error sending OpenID4VP message: {0}")]
-    Request(#[source] VpMessageClientError),
-    #[error("error validating requested attributes: {0}")]
-    RequestedAttributesValidation(#[source] ValidationError),
-    #[error("error parsing RP certificate: {0}")]
-    RpCertificate(#[source] CertificateError),
-}
-
-#[derive(thiserror::Error, Debug, Constructor)]
-#[error("could not perform actual disclosure, attributes were shared: {data_shared}, error: {error}")]
-pub struct DisclosureError<E: std::error::Error> {
-    pub data_shared: bool,
-    #[source]
-    pub error: E,
-}
-
-impl<E: std::error::Error> DisclosureError<E> {
-    pub fn before_sharing(error: E) -> Self {
-        Self {
-            data_shared: false,
-            error,
-        }
-    }
-
-    pub fn after_sharing(error: E) -> Self {
-        Self {
-            data_shared: true,
-            error,
-        }
-    }
-}
+mod error;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, strum::Display)]
 #[strum(serialize_all = "snake_case")] // Symmetrical to `SessionType`.
@@ -548,16 +430,6 @@ where
     }
 }
 
-impl From<VpMessageClientError> for DisclosureError<VpClientError> {
-    fn from(source: VpMessageClientError) -> Self {
-        let data_shared = match source {
-            VpMessageClientError::Http(ref reqwest_error) => !reqwest_error.is_connect(),
-            _ => true,
-        };
-        Self::new(data_shared, VpClientError::Request(source))
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use std::convert::identity;
@@ -640,14 +512,14 @@ mod tests {
     use super::client::mock::MockVerifierVpMessageClient;
     use super::client::mock::WalletMessage;
     use super::client::VpMessageClient;
+    use super::client::VpMessageClientError;
+    use super::error::DisclosureError;
+    use super::error::VpClientError;
     use super::CommonDisclosureData;
-    use super::DisclosureError;
     use super::DisclosureMissingAttributes;
     use super::DisclosureProposal;
     use super::DisclosureSession;
     use super::DisclosureUriSource;
-    use super::VpClientError;
-    use super::VpMessageClientError;
 
     // Constants for testing.
     const VERIFIER_URL: &str = "http://example.com/disclosure";
