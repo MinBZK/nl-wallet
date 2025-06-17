8bd9a85f4fdeb460f45299148f766195cd9ffbed
peterpaul <pp.kleinhaneveld@gmail.com>
2025-06-17 14:49:15 +0200

    Remove unused DeviceRequest::verify method

--- wallet_core/lib/mdoc/src/holder/disclosure/request.rs
+++ wallet_core/lib/mdoc/src/holder/disclosure/request.rs
@@ -2,8 +2,6 @@ use chrono::DateTime;
 use chrono::Utc;
 use rustls_pki_types::TrustAnchor;
 
-use attestation_data::auth::reader_auth::ReaderRegistration;
-use attestation_data::x509::CertificateType;
 use crypto::x509::BorrowingCertificate;
 use crypto::x509::CertificateUsage;
 use utils::generator::Generator;
@@ -13,7 +11,6 @@ use crate::device_retrieval::DocRequest;
 use crate::device_retrieval::ReaderAuthenticationKeyed;
 use crate::engagement::SessionTranscript;
 use crate::errors::Result;
-use crate::holder::HolderError;
 use crate::utils::cose::ClonePayload;
 use crate::utils::serialization;
 use crate::utils::serialization::CborSeq;
@@ -21,63 +18,6 @@ use crate::utils::serialization::TaggedBytes;
 use crate::ItemsRequest;
 
 impl DeviceRequest {
-    /// Verify reader authentication, if present.
-    /// Note that since each DocRequest carries its own reader authentication, the spec allows the
-    /// the DocRequests to be signed by distinct readers. TODO maybe support this (PVW-2368).
-    /// For now, this function requires either none of the DocRequests to be signed, or all of them
-    /// by the same reader.
-    pub fn verify(
-        &self,
-        session_transcript: &SessionTranscript,
-        time: &impl Generator<DateTime<Utc>>,
-        trust_anchors: &[TrustAnchor],
-    ) -> Result<Option<(BorrowingCertificate, ReaderRegistration)>> {
-        // If there are no doc requests or none of them have reader authentication, return `None`.
-        if self.doc_requests.iter().all(|d| d.reader_auth.is_none()) {
-            return Ok(None);
-        }
-
-        // Otherwise, all of the doc requests need reader authentication.
-        if self.doc_requests.iter().any(|d| d.reader_auth.is_none()) {
-            return Err(HolderError::ReaderAuthMissing.into());
-        }
-
-        // Verify all `DocRequest` entries and make sure the resulting certificates are all exactly equal.
-        let certificate = self
-            .doc_requests
-            .iter()
-            .try_fold(None, {
-                |result_cert, doc_request| -> Result<_> {
-                    // This `.unwrap()` is safe, because `.verify()` will only return `None`
-                    // if `reader_auth` is absent, the presence of which we checked above.
-                    let doc_request_cert = doc_request.verify(session_transcript, time, trust_anchors)?.unwrap();
-
-                    // If there is a certificate from a previous iteration, compare our certificate to that.
-                    if let Some(result_cert) = result_cert {
-                        if doc_request_cert != result_cert {
-                            return Err(HolderError::ReaderAuthsInconsistent.into());
-                        }
-                    }
-
-                    Ok(doc_request_cert.into())
-                }
-            })?
-            .unwrap(); // This `.unwrap()` is safe for the same reason stated above.
-
-        // Extract `ReaderRegistration` from the one certificate.
-        let reader_registration = match CertificateType::from_certificate(&certificate).map_err(HolderError::from)? {
-            CertificateType::ReaderAuth(Some(reader_registration)) => *reader_registration,
-            _ => return Err(HolderError::NoReaderRegistration(Box::new(certificate)).into()),
-        };
-
-        // Verify that the requested attributes are included in the reader authentication.
-        reader_registration
-            .verify_requested_attributes(&self.items_requests())
-            .map_err(HolderError::from)?;
-
-        Ok((certificate, reader_registration).into())
-    }
-
     pub fn items_requests(&self) -> impl Iterator<Item = &ItemsRequest> + Clone {
         self.doc_requests.iter().map(|doc_request| &doc_request.items_request.0)
     }
@@ -114,6 +54,7 @@ impl DocRequest {
 mod tests {
     use assert_matches::assert_matches;
 
+    use attestation_data::auth::reader_auth::ReaderRegistration;
     use attestation_data::x509::generate::mock::generate_reader_mock;
     use crypto::server_keys::generate::Ca;
     use crypto::server_keys::KeyPair;
@@ -153,62 +94,6 @@ mod tests {
             reader_auth,
         }
     }
-
-    #[tokio::test]
-    async fn test_device_request_verify() {
-        // Create two certificates and private keys.
-        let ca = Ca::generate_reader_mock_ca().unwrap();
-        let reader_registration = ReaderRegistration::new_mock();
-        let private_key1 = generate_reader_mock(&ca, reader_registration.clone().into()).unwrap();
-        let private_key2 = generate_reader_mock(&ca, reader_registration.clone().into()).unwrap();
-
-        let session_transcript = SessionTranscript::new_mock();
-
-        // Create an empty `ItemsRequest` and generate `DeviceRequest` with two `DocRequest`s
-        // from it, each signed with the same certificate.
-        let items_request = ItemsRequest::new_example_empty();
-
-        let device_request = DeviceRequest::from_doc_requests(vec![
-            create_doc_request(items_request.clone(), &session_transcript, &private_key1).await,
-            create_doc_request(items_request.clone(), &session_transcript, &private_key1).await,
-        ]);
-
-        // Verifying this `DeviceRequest` should succeed and return the `ReaderRegistration`.
-        let trust_anchors = &[ca.to_trust_anchor()];
-
-        let verified_reader_registration = device_request
-            .verify(&session_transcript, &TimeGenerator, trust_anchors)
-            .expect("Could not verify DeviceRequest");
-
-        assert_eq!(
-            verified_reader_registration,
-            Some((private_key1.certificate().clone(), reader_registration))
-        );
-
-        // Verifying a `DeviceRequest` that has no reader auth at all should succeed and return `None`.
-        let device_request = DeviceRequest::from_items_requests(vec![items_request.clone(), items_request.clone()]);
-
-        let no_reader_registration = device_request
-            .verify(&session_transcript, &TimeGenerator, trust_anchors)
-            .expect("Could not verify DeviceRequest");
-
-        assert!(no_reader_registration.is_none());
-
-        // Generate `DeviceRequest` with two `DocRequest`s, each signed
-        // with a different key and including a different certificate.
-        let device_request = DeviceRequest::from_doc_requests(vec![
-            create_doc_request(items_request.clone(), &session_transcript, &private_key1).await,
-            create_doc_request(items_request, &session_transcript, &private_key2).await,
-        ]);
-
-        // Verifying this `DeviceRequest` should result in a `HolderError::ReaderAuthsInconsistent` error.
-        let error = device_request
-            .verify(&session_transcript, &TimeGenerator, trust_anchors)
-            .expect_err("Verifying DeviceRequest should have resulted in an error");
-
-        assert_matches!(error, Error::Holder(HolderError::ReaderAuthsInconsistent));
-    }
-
     #[tokio::test]
     async fn test_doc_request_verify() {
         // Create a CA, certificate and private key and trust anchors.
