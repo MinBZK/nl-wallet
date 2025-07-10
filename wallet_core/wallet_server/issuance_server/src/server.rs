80e1d70d1bce08e5b9189dca24aa05cd744ea547
peterpaul <pp.kleinhaneveld@gmail.com>
2025-07-10 10:13:34 +0200

    Remove ItemsRequests from issuance server configuration

--- wallet_core/wallet_server/issuance_server/src/server.rs
+++ wallet_core/wallet_server/issuance_server/src/server.rs
@@ -76,7 +76,7 @@ where
             WalletInitiatedUseCase::try_new(
                 s.key_pair.parse(hsm.clone()).await?,
                 SessionTypeReturnUrl::Both,
-                s.to_disclose.into(),
+                s.to_disclose.try_into()?,
                 format!("{OPENID4VCI_CREDENTIAL_OFFER_URL_SCHEME}://").parse().unwrap(),
             )?,
         ))
