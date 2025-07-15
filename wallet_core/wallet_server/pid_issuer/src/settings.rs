6a68262e46b42b0f9a71408a14cc7e4da9c1505b
Sietse Ringers <mail@sietseringers.net>
2025-07-15 15:11:05 +0200

    Remove config parameters from PID issuer which are actually constant

--- wallet_core/wallet_server/pid_issuer/src/settings.rs
+++ wallet_core/wallet_server/pid_issuer/src/settings.rs
@@ -17,7 +17,6 @@ use server_utils::settings::ServerSettings;
 use server_utils::settings::Settings;
 use server_utils::settings::NL_WALLET_CLIENT_ID;
 use utils::path::prefix_local_path;
-use utils::vec_at_least::VecNonEmpty;
 
 #[serde_as]
 #[derive(Clone, Deserialize)]
@@ -38,10 +37,6 @@ pub struct PidIssuerSettings {
 pub struct RecoveryCode {
     #[serde(flatten)]
     pub hmac_secret: SecretKey,
-
-    pub attestation_types: VecNonEmpty<String>,
-    pub recovery_code_claim_paths: VecNonEmpty<String>,
-    pub bsn_claim_paths: VecNonEmpty<String>,
 }
 
 #[derive(Clone, Deserialize)]
