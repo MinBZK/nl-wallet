66cd6dc815d31ac85809869e854e74d20a0c61e5
Daan <2034049+daandebie@users.noreply.github.com>
2025-07-15 12:55:02 +0200

    PVW-4407: Remove warning on attribute page
    * Remove 'dataPrivacyBanner' from e2e tests
    
    * Fix MR feedback
    
    * Fix code-gen
    
    * Update goldens & add attributes divider
    
    * Add subtitle to card data CTA button
    
    * Remove `DataPrivacyBanner` widget
    
    * Remove data privacy warning from design system
    
    * Remove data privacy warning from page

--- uiautomation/src/test/kotlin/screen/card/CardDataScreen.kt
+++ uiautomation/src/test/kotlin/screen/card/CardDataScreen.kt
@@ -5,15 +5,12 @@ import util.MobileActions
 class CardDataScreen : MobileActions() {
 
     private val screen = find.byValueKey("cardDataScreen")
-    private val dataPrivacyBanner = find.byValueKey("dataPrivacyBanner")
     private val dataIncorrectButton = find.byText(l10n.getString("cardDataScreenIncorrectCta"))
     private val bottomBackButton = find.byText(l10n.getString("generalBottomBackCta"))
 
     private val scrollableType = ScrollableType.CustomScrollView
 
     fun visible() = isElementVisible(screen)
-
-    fun dataPrivacyBannerVisible() = isElementVisible(dataPrivacyBanner)
 
     fun dataAttributeVisible(attribute: String): Boolean {
         return isElementVisible(find.byText(attribute))
