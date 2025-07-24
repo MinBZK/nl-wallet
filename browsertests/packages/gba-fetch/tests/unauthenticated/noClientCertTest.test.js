d759efc810f344925d348ff2e4f8d751480ad2e0
johnberendsen <john.berendsen@ictu.nl>
2025-07-24 14:33:07 +0200

    remove tests and jobs

--- browsertests/packages/gba-fetch/tests/unauthenticated/noClientCertTest.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "@playwright/test"
-import { test } from "../common.js"
-
-test.describe("GBA Fetch when no client certificate is provided", () => {
-  test("The webpage can only be accessed via VPN and using an approved user certificate.", async ({ gbaFetchPage }) => {
-    expect(await gbaFetchPage.getPageTitle()).toBe("400 Bad Request")
-  })
-})
