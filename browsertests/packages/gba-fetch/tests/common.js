2deb6ff4f142d91a0b290ef87abecdb3d082dc52
johnberendsen <john.berendsen@ictu.nl>
2025-07-24 14:33:07 +0200

    remove tests and jobs

--- browsertests/packages/gba-fetch/tests/common.js
+++
@@ -1,9 +0,0 @@
-import { test as base } from "@playwright/test"
-import { GbaFetchPage } from "../pages/gbaFetchPage.js"
-
-export const test = base.extend({
-  gbaFetchPage: async ({ page, baseURL }, use) => {
-    await page.goto(baseURL)
-    await use(new GbaFetchPage(page))
-  },
-})
