894f0c9394841325139ad2c556755c150130ac90
John Berendsen <john.berendsen@ictu.nl>
2025-08-04 06:33:51 +0000

    PVW-4464: Add e2e test for card renewal
    * applied review comment
    
    * applied review comments
    
    * set correct test annotation
    
    * add e2e test for card renewal

--- uiautomation/src/test/kotlin/feature/personalize/DisclosureBasedIssuanceTests.kt
+++ uiautomation/src/test/kotlin/feature/personalize/DisclosureBasedIssuanceTests.kt
@@ -98,7 +98,7 @@ class DisclosureBasedIssuanceTests : TestBase() {
         setUp(testInfo)
         MenuNavigator().toScreen(MenuNavigatorScreen.Menu)
         MenuScreen().clickBrowserTestButton()
-        indexWebPage.clickInsurAnceButton()
+        indexWebPage.clickInsuranceButton()
         val platform = indexWebPage.platformName()
         issuerWebPage.openSameDeviceWalletFlow(platform)
         issuerWebPage.switchToAppContext()
--- uiautomation/src/test/kotlin/navigator/CardNavigator.kt
+++ uiautomation/src/test/kotlin/navigator/CardNavigator.kt
@@ -7,12 +7,12 @@ import screen.dashboard.DashboardScreen
 
 class CardNavigator {
 
-    fun toScreen(screen: CardNavigatorScreen) {
+    fun toScreen(screen: CardNavigatorScreen, vct: String = "urn:eudi:pid:nl:1") {
         // Navigate through onboarding flow
         OnboardingNavigator().toScreen(OnboardingNavigatorScreen.Dashboard)
 
         // Navigate card flow
-        if (screen > CardNavigatorScreen.Dashboard) DashboardScreen().clickPidCard()
+        if (screen > CardNavigatorScreen.Dashboard) DashboardScreen().clickCard(vct)
         if (screen > CardNavigatorScreen.CardDetail) CardDetailScreen().clickCardDataButton()
     }
 }
