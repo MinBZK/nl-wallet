413244a01991946164ae395b29c12a1295f983c9
Jacob Middag <jacob.middag@ictu.nl>
2025-08-11 15:12:37 +0200

    Update Debian bookworm to trixie

--- deploy/docker-images/ci/apt.sh
+++ deploy/docker-images/ci/apt.sh
@@ -10,7 +10,7 @@ c2a9a16fde95e037bafd0fa6b7e31f41b4ff1e85851de5558f19a2a2f0e955e2  /etc/apt/trust
 0b7dc94b880f0b63e2093394b113cafd870badb86e020a35614f49b9d83beb1e  /etc/apt/trusted.gpg.d/debian-archive-bullseye-automatic.asc
 716e79393c724d14ecba8be46e99ecbe1b689f67ceff3cb3cab28f6e69e8b8b8  /etc/apt/trusted.gpg.d/debian-archive-bullseye-security-automatic.asc
 fb260ce8521a2faa4937d98a29a5347807e10614b97d510fbabe5480c803bda9  /etc/apt/trusted.gpg.d/debian-archive-bullseye-stable.asc
-9c854992fc6c423efe8622c3c326a66e73268995ecbe8f685129063206a18043  /etc/apt/trusted.gpg.d/debian-archive-buster-automatic.asc
-4cf886d6df0fc1c185ce9fb085d1cd8d678bc460e6267d80a833d7ea507a0fbd  /etc/apt/trusted.gpg.d/debian-archive-buster-security-automatic.asc
-ca9bd1a0b3743495ae45693c6d4e54abadcffb242d72df15eda5b28e4ff385fa  /etc/apt/trusted.gpg.d/debian-archive-buster-stable.asc
+6f1d277429dd7ffedcc6f8688a7ad9a458859b1139ffa026d1eeaadcbffb0da7  /etc/apt/trusted.gpg.d/debian-archive-trixie-automatic.asc
+844c07d242db37f283afab9d5531270a0550841e90f9f1a9c3bd599722b808b7  /etc/apt/trusted.gpg.d/debian-archive-trixie-security-automatic.asc
+4d097bb93f83d731f475c5b92a0c2fcf108cfce1d4932792fca72d00b48d198b  /etc/apt/trusted.gpg.d/debian-archive-trixie-stable.asc
 EOD
