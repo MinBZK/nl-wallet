85a85fb1d3ea1c6d073628756f3e3c974059d34c
Jacob Middag <jacob.middag@ictu.nl>
2025-08-11 15:12:37 +0200

    Update Debian bookworm to trixie

--- deploy/docker-images/ci/python.sh
+++ deploy/docker-images/ci/python.sh
@@ -2,9 +2,4 @@
 set -euxo pipefail
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-  python3 python3-pip pipx
-
-# Download from GitHub as Debian version is too old
-wget --no-hsts -q -O /usr/local/bin/git-filter-repo https://github.com/newren/git-filter-repo/raw/refs/tags/v2.47.0/git-filter-repo
-echo "67447413e273fc76809289111748870b6f6072f08b17efe94863a92d810b7d94  /usr/local/bin/git-filter-repo" | sha256sum -c
-chmod +x /usr/local/bin/git-filter-repo
+  python3 python3-pip pipx git-filter-repo
