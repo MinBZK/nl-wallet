6cd29cbd7126d19cc0695cd2278801bb32617581
Jacob Middag <jacob.middag@ictu.nl>
2025-08-11 15:12:57 +0200

    Update quality tooling

--- deploy/docker-images/ci/dependency-check.sh
+++ deploy/docker-images/ci/dependency-check.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-VERSION=12.1.0
+VERSION=12.1.3
 
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 259A55407DD6C00299E6607EFFDE55BE73A2D1ED
-wget --no-hsts -q https://github.com/jeremylong/DependencyCheck/releases/download/v${VERSION}/dependency-check-${VERSION}-release.zip
-wget --no-hsts -q https://github.com/jeremylong/DependencyCheck/releases/download/v${VERSION}/dependency-check-${VERSION}-release.zip.asc
+wget --no-hsts -q https://github.com/dependency-check/DependencyCheck/releases/download/v${VERSION}/dependency-check-${VERSION}-release.zip
+wget --no-hsts -q https://github.com/dependency-check/DependencyCheck/releases/download/v${VERSION}/dependency-check-${VERSION}-release.zip.asc
 gpg --verify dependency-check-${VERSION}-release.zip.asc
 
 unzip dependency-check-${VERSION}-release.zip -d $(dirname $DEPENDENCY_CHECK_HOME)
