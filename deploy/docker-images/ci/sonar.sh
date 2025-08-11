68309dea4482d3a9b2d12ecb1fa5633211b76f73
Jacob Middag <jacob.middag@ictu.nl>
2025-08-11 15:12:57 +0200

    Update quality tooling

--- deploy/docker-images/ci/sonar.sh
+++ deploy/docker-images/ci/sonar.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-wget --no-hsts -q -O sonar-scanner-cli.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-6.2.1.4610-linux-x64.zip
-echo "0b8a3049f0bd5de7abc1582c78c233960d3d4ed7cc983a1d1635e8552f8bb439  sonar-scanner-cli.zip" | sha256sum -c
+wget --no-hsts -q -O sonar-scanner-cli.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-7.2.0.5079-linux-x64.zip
+echo "da9f4e64a3d555f08ce38b5469ebd91fe2b311af473f7001a5ee5c1fd58b004b  sonar-scanner-cli.zip" | sha256sum -c
 
 unzip sonar-scanner-cli.zip -d $(dirname $SONAR_HOME)
 mv -T $(dirname $SONAR_HOME)/sonar-scanner-* $SONAR_HOME
