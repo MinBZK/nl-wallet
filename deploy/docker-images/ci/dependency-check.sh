ee3d654b030916a12f7ca00ca8612bd25d55e655
Jacob Middag <jacob.middag@ictu.nl>
2025-08-12 13:42:28 +0200

    Use port 80 for Ubuntu keyserver
    The default hkp port is blocked by some corporate firewalls

--- deploy/docker-images/ci/dependency-check.sh
+++ deploy/docker-images/ci/dependency-check.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 VERSION=12.1.3
 
-gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 259A55407DD6C00299E6607EFFDE55BE73A2D1ED
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 259A55407DD6C00299E6607EFFDE55BE73A2D1ED
 wget --no-hsts -q https://github.com/dependency-check/DependencyCheck/releases/download/v${VERSION}/dependency-check-${VERSION}-release.zip
 wget --no-hsts -q https://github.com/dependency-check/DependencyCheck/releases/download/v${VERSION}/dependency-check-${VERSION}-release.zip.asc
 gpg --verify dependency-check-${VERSION}-release.zip.asc
