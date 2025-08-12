ebbafabda88260686b785db19ca83766e29f6e3e
John Berendsen <john.berendsen@ictu.nl>
2025-08-12 10:33:13 +0000

    pvw-4777 set jobnr asd browserstack buildnr
    * set jobnr asd browserstack buildnr

--- uiautomation/src/main/kotlin/helper/BrowserStackHelper.kt
+++ uiautomation/src/main/kotlin/helper/BrowserStackHelper.kt
@@ -7,12 +7,8 @@ import io.restassured.RestAssured
 import io.restassured.http.ContentType
 import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.remote.RemoteWebDriver
-import util.EnvironmentUtil
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 object BrowserStackHelper {
-    val buildName = generateBuildName()
 
     fun getAppUrl(endpoint: String, userName: String, accessKey: String, customId: String): String {
         return RestAssured
@@ -32,15 +28,6 @@ object BrowserStackHelper {
         val jse: JavascriptExecutor = getWebDriver() as RemoteWebDriver
         jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"$status\"}}")
         Selenide.closeWebDriver()
-    }
-
-    private fun generateBuildName(): String {
-        val currentDateTime = LocalDateTime.now()
-        val formatter = EnvironmentUtil.getVar("BUILD_NAME_DATE_FORMAT_OVERRIDE").takeIf { it.isNotEmpty() }
-            ?.let { DateTimeFormatter.ofPattern(it) }
-            ?: DateTimeFormatter.ofPattern("dd/MM-HH:mm:ss")
-        val formattedDateTime = currentDateTime.format(formatter)
-        return "build-$formattedDateTime"
     }
 
     fun setNetwork(endpoint: String, userName: String, accessKey: String, sessionId: String, networkProfile: String) {
