78746be13d99600780fa4e0c06da652c13f949ef
Jacob Middag <jacob.middag@ictu.nl>
2025-08-22 10:40:06 +0200

    Update Android tooling

--- deploy/docker-images/ci/android.sh
+++ deploy/docker-images/ci/android.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-wget --no-hsts -q -O sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
-echo "2d2d50857e4eb553af5a6dc3ad507a17adf43d115264b1afc116f95c92e5e258 sdk.zip" | sha256sum -c
+wget --no-hsts -q -O sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-13114758_latest.zip
+echo "7ec965280a073311c339e571cd5de778b9975026cfcbe79f2b1cdcb1e15317ee sdk.zip" | sha256sum -c
 
 unzip -q sdk.zip
 rm sdk.zip
@@ -17,13 +17,15 @@ set -o pipefail
 
 # "ndk should be installed separately due to size.
 sdkmanager --install \
-  "build-tools;34.0.0" \
+  "build-tools;35.0.0" \
+  "build-tools;36.0.0" \
   "cmake;3.22.1" \
-  "cmdline-tools;17.0" \
+  "cmdline-tools;19.0" \
   "platforms;android-31" \
   "platforms;android-33" \
   "platforms;android-34" \
   "platforms;android-35" \
+  "platforms;android-36" \
   "platform-tools"
 
 rm -rf ~/.android
