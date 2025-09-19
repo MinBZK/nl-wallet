9a9b20ce633a4d9359b4909461597c3d297452d8
Ruud Klaver <ruud@pantsforcats.nl>
2025-09-19 15:17:03 +0200

    Remove IssuerSigned::dangerous_public_key() method

--- wallet_core/lib/mdoc/src/verifier.rs
+++ wallet_core/lib/mdoc/src/verifier.rs
@@ -155,17 +155,6 @@ impl ValidityInfo {
 }
 
 impl IssuerSigned {
-    pub fn dangerous_public_key(&self) -> Result<VerifyingKey> {
-        let public_key = self
-            .issuer_auth
-            .dangerous_parse_unverified()?
-            .0
-            .device_key_info
-            .try_into()?;
-
-        Ok(public_key)
-    }
-
     pub fn verify(
         &self,
         validity: ValidityRequirement,
@@ -326,11 +315,8 @@ mod tests {
 
     use chrono::Duration;
     use chrono::Utc;
-    use p256::ecdsa::SigningKey;
-    use rand_core::OsRng;
 
     use crypto::examples::Examples;
-    use crypto::mock_remote::MockRemoteEcdsaKey;
     use crypto::server_keys::generate::Ca;
 
     use crate::examples::EXAMPLE_ATTR_NAME;
@@ -339,7 +325,6 @@ mod tests {
     use crate::examples::EXAMPLE_NAMESPACE;
     use crate::examples::Example;
     use crate::examples::IsoCertTimeGenerator;
-    use crate::holder::Mdoc;
     use crate::iso::disclosure::DeviceResponse;
     use crate::iso::engagement::DeviceAuthenticationBytes;
     use crate::iso::mdocs::ValidityInfo;
@@ -386,21 +371,6 @@ mod tests {
         validity
             .verify_is_valid_at(now, ValidityRequirement::AllowNotYetValid)
             .unwrap();
-    }
-
-    #[tokio::test]
-    async fn test_issuer_signed_public_key() {
-        let key = SigningKey::random(&mut OsRng);
-        let key = MockRemoteEcdsaKey::new("identifier".to_string(), key);
-        let mdoc = Mdoc::new_mock_with_key(&key).await;
-
-        let public_key = mdoc
-            .issuer_signed
-            .dangerous_public_key()
-            .expect("Could not get public key from from IssuerSigned");
-
-        // The example mdoc should contain the generated key.
-        assert_eq!(public_key, *key.verifying_key());
     }
 
     /// Verify the example disclosure from the standard.
