15a76f3b93c56ee8a29db628965e97f1ecd8515b
Rob <edi@minbzk.nl>
2025-09-23 10:15:35 +0200

    Refactor NavigationRequest and update tests
    This commit refactors the `NavigationRequest` factory constructors and updates the corresponding tests in `CoreUriRepositoryTest`.

--- wallet_app/test/src/data/repository/uri/core/core_uri_repository_test.dart
+++ wallet_app/test/src/data/repository/uri/core/core_uri_repository_test.dart
@@ -2,8 +2,6 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:wallet/src/data/repository/uri/core/core_uri_repository.dart';
 import 'package:wallet/src/domain/model/navigation/navigation_request.dart';
-import 'package:wallet/src/feature/disclosure/argument/disclosure_screen_argument.dart';
-import 'package:wallet/src/feature/issuance/argument/issuance_screen_argument.dart';
 import 'package:wallet_core/core.dart';
 
 import '../../../../mocks/wallet_mocks.mocks.dart';
@@ -22,24 +20,14 @@ void main() {
       const testUri = 'https://disclosure.org';
       when(mockWalletCore.identifyUri(testUri)).thenAnswer((realInvocation) async => IdentifyUriResult.Disclosure);
       final result = await uriRepository.processUri(Uri.parse(testUri));
-      expect(result, isA<DisclosureNavigationRequest>());
-      expect(
-        result.argument,
-        const DisclosureScreenArgument(uri: testUri, isQrCode: false),
-        reason: 'The original uri should be passed to the correct screen as an argument',
-      );
+      expect(result, NavigationRequest.disclosure(testUri));
     });
 
     test('Pid Issuance uri should result in a PidIssuanceNavigationRequest', () async {
       const testUri = 'https://pid_issuance.org';
       when(mockWalletCore.identifyUri(testUri)).thenAnswer((realInvocation) async => IdentifyUriResult.PidIssuance);
       final result = await uriRepository.processUri(Uri.parse(testUri));
-      expect(result, isA<PidIssuanceNavigationRequest>());
-      expect(
-        result.argument,
-        testUri,
-        reason: 'The original uri should be passed to the correct screen as an argument',
-      );
+      expect(result, NavigationRequest.pidIssuance(testUri));
     });
 
     test('Disclosure based issuance uri should result in a IssuanceNavigationRequest', () async {
@@ -48,12 +36,28 @@ void main() {
         mockWalletCore.identifyUri(testUri),
       ).thenAnswer((realInvocation) async => IdentifyUriResult.DisclosureBasedIssuance);
       final result = await uriRepository.processUri(Uri.parse(testUri));
-      expect(result, isA<IssuanceNavigationRequest>());
-      expect(
-        result.argument,
-        const IssuanceScreenArgument(uri: testUri, isRefreshFlow: false, isQrCode: false),
-        reason: 'The original uri should be passed to the correct screen as an argument',
-      );
+      expect(result, NavigationRequest.issuance(testUri));
+    });
+
+    test('Pid Renewal uri should result in a PidRenewalNavigationRequest', () async {
+      const testUri = 'https://pid_renewal.org';
+      when(mockWalletCore.identifyUri(testUri)).thenAnswer((realInvocation) async => IdentifyUriResult.PidRenewal);
+      final result = await uriRepository.processUri(Uri.parse(testUri));
+      expect(result, NavigationRequest.pidRenewal(testUri));
+    });
+
+    test('Pin Recovery uri should result in a PinRecoveryNavigationRequest', () async {
+      const testUri = 'https://pin_recovery.org';
+      when(mockWalletCore.identifyUri(testUri)).thenAnswer((realInvocation) async => IdentifyUriResult.PinRecovery);
+      final result = await uriRepository.processUri(Uri.parse(testUri));
+      expect(result, NavigationRequest.pinRecovery(testUri));
+    });
+
+    test('Transfer uri should result in a WalletTransferSourceNavigationRequest', () async {
+      const testUri = 'https://transfer.org';
+      when(mockWalletCore.identifyUri(testUri)).thenAnswer((realInvocation) async => IdentifyUriResult.Transfer);
+      final result = await uriRepository.processUri(Uri.parse(testUri));
+      expect(result, NavigationRequest.walletTransfer(testUri));
     });
   });
 }
