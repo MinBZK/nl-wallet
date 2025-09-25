a861df4020a27296a184dd99a2a6934168ba8edd
Jacob Middag <jacob.middag@ictu.nl>
2025-09-25 16:58:22 +0200

    Prefer importing async_trait

--- wallet_core/wallet_server/pid_issuer/migrations/src/main.rs
+++ wallet_core/wallet_server/pid_issuer/migrations/src/main.rs
@@ -1,10 +1,11 @@
+use async_trait::async_trait;
 use sea_orm_migration::prelude::*;
 
 mod m20241023_095134_create_wua_table;
 
 pub struct Migrator;
 
-#[async_trait::async_trait]
+#[async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         let mut migrations = server_utils_migrations::Migrator::migrations();
