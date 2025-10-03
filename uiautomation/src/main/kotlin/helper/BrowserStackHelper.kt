2addec6dabea660904230444e490d48448be19cb
John Berendsen <john.berendsen@ictu.nl>
2025-10-03 05:50:05 +0000

    Pvw 4960 implement browserstack sdk
    * set rule back
    
    * try to run on base image
    
    * Apply 2 suggestion(s) to 2 file(s)
    
    Co-authored-by: Jacob Middag <jacob.middag@ictu.nl>
    
    * add newlines add aend of files
    
    * fix running of tests in English/Dutch
    
    * setup browserstack sdk

--- uiautomation/src/main/kotlin/helper/BrowserStackHelper.kt
+++ uiautomation/src/main/kotlin/helper/BrowserStackHelper.kt
@@ -1,34 +1,9 @@
 package helper
 
-import com.codeborne.selenide.Selenide
-import com.codeborne.selenide.WebDriverRunner.getWebDriver
-import data.TestConfigRepository.Companion.testConfig
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
-import org.openqa.selenium.JavascriptExecutor
-import org.openqa.selenium.remote.RemoteWebDriver
 
 object BrowserStackHelper {
-
-    fun getAppUrl(endpoint: String, userName: String, accessKey: String, customId: String): String {
-        return RestAssured
-            .given()
-            .auth().basic(userName, accessKey)
-            .`when`()
-            .get(endpoint + customId)
-            .then()
-            .statusCode(200)
-            .extract()
-            .path("[0].app_url")
-    }
-
-    fun markTest(status: String) {
-        if (!testConfig.remote) return
-
-        val jse: JavascriptExecutor = getWebDriver() as RemoteWebDriver
-        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"$status\"}}")
-        Selenide.closeWebDriver()
-    }
 
     fun setNetwork(endpoint: String, userName: String, accessKey: String, sessionId: String, networkProfile: String) {
         RestAssured
