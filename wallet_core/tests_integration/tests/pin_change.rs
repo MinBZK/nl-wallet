853cc5e803857772467f24765d5d68becfb07512
Sietse Ringers <mail@sietseringers.net>
2025-10-15 07:31:33 +0200

    Move PIN recovery integration test to new module

--- wallet_core/tests_integration/tests/pin_change.rs
+++ wallet_core/tests_integration/tests/pin_change.rs
@@ -20,20 +20,3 @@ async fn test_pin_change() {
     let result = wallet.continue_change_pin(new_pin_str).await;
     assert!(result.is_ok(), "continue_change_pin failed: {:?}", result.err());
 }
-
-#[tokio::test]
-#[serial(hsm)]
-async fn test_pin_recovery() {
-    let pin = "112233".to_string();
-    let (mut wallet, _, _) = setup_wallet_and_default_env(WalletDeviceVendor::Apple).await;
-    wallet = do_wallet_registration(wallet, &pin).await;
-    wallet = do_pid_issuance(wallet, pin).await;
-
-    let new_pin = "314159".to_string();
-    let uri = wallet.create_pin_recovery_redirect_uri().await.unwrap();
-    wallet.continue_pin_recovery(uri).await.unwrap();
-    wallet.complete_pin_recovery(new_pin.clone()).await.unwrap();
-
-    // The wallet can now use the new PIN.
-    wallet.check_pin(new_pin).await.unwrap();
-}
