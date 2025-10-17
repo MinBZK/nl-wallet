f8a8c7909998e30da96643fdd11d2a7d4dc89583
Rob <edi@minbzk.nl>
2025-10-17 13:32:16 +0000

    PVW-5076: Upgrade Flutter to 3.35.6
    * Update Flutter .fvmrc and ci image versions
    
    * Update flutter shasum
    
    * Update Node.js setup script checksum
    
    * Upgrade Flutter to 3.35.6

--- deploy/docker-images/ci/flutter.sh
+++ deploy/docker-images/ci/flutter.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-wget --no-hsts -q -O flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.35.1-stable.tar.xz
-echo "58efd9d1e570a1bf976e218cfbbcca3f23b21b873d765a74e045d5b9022ab515  flutter.tar.xz" | sha256sum -c
+wget --no-hsts -q -O flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.35.6-stable.tar.xz
+echo "87493b72916f12054176c2a8bbf9547fe63cb5754bdddfe300219d9b57e626af  flutter.tar.xz" | sha256sum -c
 
 tar -xf flutter.tar.xz
 rm flutter.tar.xz
