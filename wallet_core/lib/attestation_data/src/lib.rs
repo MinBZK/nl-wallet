369b1ca9d4aa53574bff51ea6d2a7a4afab5e5ce
Ruud Klaver <ruud@pantsforcats.nl>
2025-10-30 14:23:40 +0100

    Fix attestation_data tests compilation

--- wallet_core/lib/attestation_data/src/lib.rs
+++ wallet_core/lib/attestation_data/src/lib.rs
@@ -6,7 +6,7 @@ pub mod disclosure_type;
 pub mod issuable_document;
 pub mod x509;
 
-#[cfg(feature = "pid_constants")]
+#[cfg(any(test, feature = "pid_constants"))]
 pub mod pid_constants;
 
 #[cfg(feature = "test_credential")]
