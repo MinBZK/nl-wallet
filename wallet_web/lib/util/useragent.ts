34d22428fa7744295ccd4b84d2c3d59c8733c963
Daan <2034049+daandebie@users.noreply.github.com>
2025-11-20 11:28:16 +0100

    PVW-4013: Support same device flow on tablet
    * Fix lint warning
    
    * test tablet as mobile device
    
    * Detect 'iPad' as non desktop to support cross device flow

--- wallet_web/lib/util/useragent.ts
+++ wallet_web/lib/util/useragent.ts
@@ -5,8 +5,10 @@ export const isMobileKey = Symbol() as InjectionKey<boolean>
 /// Decide whether a desktop is used. The implementation is loosely based on
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#mobile_tablet_or_desktop
 export const isDesktop = (userAgent: string): boolean => {
+  const mobile = /Mobi/.test(userAgent)
   const android = /Android/i.test(userAgent)
   const iPhone = /iPhone/.test(userAgent)
-  const mobile = /Mobi/.test(userAgent)
-  return !(mobile && (android || iPhone))
+  const iPad = /iPad/.test(userAgent) || (userAgent.includes("Macintosh") && navigator.maxTouchPoints > 1)
+
+  return !(mobile || android || iPhone || iPad)
 }
