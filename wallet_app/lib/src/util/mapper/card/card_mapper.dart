298a6941713e8fe2b4a81aca144fea75cd10660d
Daan <2034049+daandebie@users.noreply.github.com>
2025-12-05 10:47:54 +0100

    Map `RevocationStatus` to `CardStatus` in UI
    * Handle `RevocationStatus` rename
    
    * Update tests after refactor
    
    * Implement card status mapping + tests
    
    * Add `CardStatusMapper` class

--- wallet_app/lib/src/util/mapper/card/card_mapper.dart
+++ wallet_app/lib/src/util/mapper/card/card_mapper.dart
@@ -14,11 +14,13 @@ class CardMapper extends Mapper<core.AttestationPresentation, WalletCard> {
   final Mapper<CardAttributeWithCardId, DataAttribute> _attributeMapper;
   final Mapper<core.Organization, Organization> _organizationMapper;
   final Mapper<core.DisplayMetadata, CardDisplayMetadata> _displayMetadataMapper;
+  final Mapper<core.RevocationStatus?, CardStatus> _cardStatusMapper;
 
   CardMapper(
     this._attributeMapper,
     this._organizationMapper,
     this._displayMetadataMapper,
+    this._cardStatusMapper,
   );
 
   @override
@@ -32,8 +34,8 @@ class CardMapper extends Mapper<core.AttestationPresentation, WalletCard> {
       attestationId: cardId,
       attestationType: input.attestationType,
       issuer: _organizationMapper.map(input.issuer),
-      // TODO(Daan): Implement status, validFrom & validUntil mapping once Core logic is implemented in [PVW-4566];
-      status: CardStatus.valid,
+      status: _cardStatusMapper.map(input.revocationStatus),
+      // TODO(Daan): Implement validFrom & validUntil mapping once Core logic is implemented in [PVW-5161];
       validFrom: clock.now().subtract(const Duration(days: 1)),
       validUntil: clock.now().add(const Duration(days: 365)),
       metadata: _displayMetadataMapper.mapList(input.displayMetadata),
