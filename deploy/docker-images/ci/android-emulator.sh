0ce2ea9106690faa719514dd933540264bab5710
Jacob Middag <jacob.middag@ictu.nl>
2025-12-09 09:39:36 +0100

    Add libx11-xcb1 library

--- deploy/docker-images/ci/android-emulator.sh
+++ deploy/docker-images/ci/android-emulator.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-    libx11-6
+    libx11-6 libx11-xcb1
 
 sdkmanager --install "system-images;android-34;google_apis_playstore;x86_64"
 avdmanager -s create avd --name phone --package "system-images;android-34;google_apis_playstore;x86_64"
