dc17bbb18ca0d432da883655699d95e56a750151
Jacob Middag <jacob.middag@ictu.nl>
2025-12-09 15:04:24 +0100

    Use f64 for ratio

--- wallet_core/lib/utils/src/num.rs
+++ wallet_core/lib/utils/src/num.rs
@@ -18,12 +18,11 @@ impl NonZeroU31 {
     derive(Debug, Clone, Copy, PartialEq, Eq, TryFrom, Into, Deserialize),
     validate(finite, greater = 0, less_or_equal = 1)
 )]
-pub struct Ratio(f32);
+pub struct Ratio(f64);
 
 impl Ratio {
     pub fn of_nonzero_u31(self, size: NonZeroU31) -> NonZeroU31 {
-        // Calculating in f64 because of the limited precision of f32 compared to i32
-        let mut result = ((i32::from(size) as f64) * (f32::from(self) as f64)).round() as u32;
+        let mut result = ((i32::from(size) as f64) * self.into_inner()).round() as u32;
         if result == 0 {
             result = 1
         }
@@ -32,8 +31,7 @@ impl Ratio {
     }
 
     pub fn of_duration(self, duration: Duration) -> Duration {
-        // Calculating in f64 because of the limited precision of f32 compared to u64
-        duration.mul_f64(f32::from(self) as f64)
+        duration.mul_f64(self.into_inner())
     }
 }
 
@@ -49,7 +47,7 @@ mod tests {
     #[case(0.1, 101, 10)]
     #[case(0.0001, 1, 1)]
     #[case(1.0, i32::MAX, i32::MAX)]
-    fn test_ratio_of_nonzero_u31(#[case] ratio: f32, #[case] size: i32, #[case] expected_result: i32) {
+    fn test_ratio_of_nonzero_u31(#[case] ratio: f64, #[case] size: i32, #[case] expected_result: i32) {
         let ratio = Ratio::try_new(ratio).unwrap();
         assert_eq!(
             ratio.of_nonzero_u31(size.try_into().unwrap()),
