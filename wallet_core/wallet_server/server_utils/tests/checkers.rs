3a2977c509a0940c0289e5cc4cd311196ee1772f
Jacob Middag <jacob.middag@ictu.nl>
2025-12-15 13:59:29 +0100

    Add comments on the Proxy struct and methods

--- wallet_core/wallet_server/server_utils/tests/checkers.rs
+++ wallet_core/wallet_server/server_utils/tests/checkers.rs
@@ -36,7 +36,9 @@ impl ProxyHandle {
     }
 }
 
+/// Simple test TCP proxy that can be aborted
 impl Proxy {
+    /// Create a server that connects to `server_addr` and binds to localhost on a random port.
     pub async fn new(server_addr: impl ToSocketAddrs) -> Self {
         let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
         let server_addr = lookup_host(server_addr).await.unwrap().next().unwrap();
@@ -48,12 +50,17 @@ impl Proxy {
         }
     }
 
+    /// Spawn the Proxy server in a separate task and return a handle to stop the task.
+    ///
+    /// Starts a simple loop that puts every new connection in a separate task
+    /// that handles the proxying.  When a signal is sent via the ProxyHandle,
+    /// it aborts all started tasks.
     pub fn spawn(self) -> ProxyHandle {
-        let (tx, mut rx) = unbounded_channel::<()>();
+        let (abort_tx, mut abort_rx) = unbounded_channel::<()>();
         let handle = tokio::spawn(async move {
             let mut abort_handles = Vec::new();
             loop {
-                let abort = rx.recv();
+                let abort = abort_rx.recv();
                 let accept = self.listener.accept();
                 select! {
                     _ = abort => break,
@@ -80,9 +87,14 @@ impl Proxy {
                 handle.abort();
             }
         });
-        ProxyHandle(tx, handle)
+        ProxyHandle(abort_tx, handle)
     }
 
+    /// Handles a single client connected to the proxy.
+    ///
+    /// It connects to the configured server and simply passes what is received
+    /// from one end to the other end via a small buffer. If one connections
+    /// closes, it closes the other connection as well.
     async fn handle(server_addr: SocketAddr, mut client_stream: TcpStream) -> anyhow::Result<()> {
         let mut server_stream = TcpStream::connect(server_addr).await?;
         let mut server_buf = [0; 1024];
@@ -120,7 +132,7 @@ async fn test_db_check_up() {
 async fn test_db_check_down(#[values(false, true)] test_before_acquire: bool) {
     let url = test_settings().storage_url;
 
-    // Create proxy
+    // Create proxy, this is needed because the pool tests the connection on startup
     let addr = format!("{}:{}", url.host().unwrap(), url.port().unwrap());
     let proxy = Proxy::new(addr).await;
     let port = proxy.port;
