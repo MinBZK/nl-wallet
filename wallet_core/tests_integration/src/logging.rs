bd1e030bac7be9996aed4c76dfbfb80b8688affe
Ruud Klaver <ruud@pantsforcats.nl>
2025-12-16 18:06:01 +0100

    Catch log messages in tests_integration
    Include any logging performed using the `log` crate during integration
    tests, such as those from `reqwest`, by using
    `SubscriberBuilder::init()`.

--- wallet_core/tests_integration/src/logging.rs
+++ wallet_core/tests_integration/src/logging.rs
@@ -1,14 +1,13 @@
 use tracing::level_filters::LevelFilter;
+use tracing_subscriber::EnvFilter;
 
 pub fn init_logging() {
-    let _ = tracing::subscriber::set_global_default(
-        tracing_subscriber::fmt()
-            .with_env_filter(
-                tracing_subscriber::EnvFilter::builder()
-                    .with_default_directive(LevelFilter::DEBUG.into())
-                    .from_env_lossy(),
-            )
-            .with_test_writer()
-            .finish(),
-    );
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::DEBUG.into())
+                .from_env_lossy(),
+        )
+        .with_test_writer()
+        .init();
 }
