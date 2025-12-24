b3fa8a26f71f93b71b86ed74bc321eef9df928de
John Berendsen <john.berendsen@ictu.nl>
2025-12-24 13:55:03 +0000

    pvw-5404 readme for wallet_docs
    * exclude readme from build and checks
    
    * readme for wallet_docs

--- wallet_docs/conf.py
+++ wallet_docs/conf.py
@@ -27,7 +27,7 @@ source_suffix = {
 }
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv', '_venv', 'venv']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv', '_venv', 'venv', 'README.md']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
