b071888e4c3c994924839851c4a23d29d54c25e7
Jacob Middag <jacob.middag@ictu.nl>
2026-01-02 17:04:50 +0100

    Prefetch previous layer from docker daemon

--- deploy/docker-images/build-ci.sh
+++ deploy/docker-images/build-ci.sh
@@ -7,21 +7,31 @@ CONTEXT_BASE="$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd -P )"
 IMAGE_NAME="$1"
 DOCKERFILE="${CONTEXT_BASE}/${IMAGE_NAME}.Dockerfile"
 CONTEXT="${CONTEXT_BASE}/${IMAGE_NAME%%-*}"
-FULL_IMAGE_PREFIX="${HARBOR_REGISTRY}/${HARBOR_PROJECT}/${IMAGE_PREFIX}"
+FROM_IMAGE_PREFIX="${HARBOR_REGISTRY}/${HARBOR_PROJECT}/${IMAGE_PREFIX}-"
 
 # Tell us what image name we're using:
 echo "Image to build is: \"$IMAGE_NAME\" with tag \"${IMAGE_TAG}\""
 
 if [[ -e $DOCKERFILE ]]; then
+    # Prefetch previous layers from docker daemon of host to speed up
+    for id in $(awk '/^FROM \$\{FROM_IMAGE_PREFIX\}/ {
+        gsub(/\$\{FROM_IMAGE_PREFIX}/, "'"${FROM_IMAGE_PREFIX}"'", $2);
+        gsub(/\$\{TAG}/, "'"${IMAGE_TAG}"'", $2);
+        print $2;
+    }' "${DOCKERFILE}"); do
+        buildah pull "docker-daemon:${id}"
+    done
+
     buildah build \
         --build-arg "DOCKER_HUB_PROXY=${HARBOR_REGISTRY}/docker-hub-proxy/" \
-        --build-arg "FROM_IMAGE_PREFIX=${FULL_IMAGE_PREFIX}-" \
+        --build-arg "FROM_IMAGE_PREFIX=${FROM_IMAGE_PREFIX}" \
         --build-arg "TAG=${IMAGE_TAG}" \
         --file "${DOCKERFILE}" \
         --tag "${IMAGE_NAME}:${IMAGE_TAG}" \
         "${CONTEXT}"
     for HARBOR_REGISTRY in $HARBOR_REGISTRIES; do
         FULL_IMAGE="${HARBOR_REGISTRY}/${HARBOR_PROJECT}/${IMAGE_PREFIX}-${IMAGE_NAME}:${IMAGE_TAG}"
+        # Push to docker daemon of host to speed up next use
         buildah push ${IMAGE_NAME}:${IMAGE_TAG} "docker-daemon:${FULL_IMAGE}"
         buildah push ${IMAGE_NAME}:${IMAGE_TAG} "docker://${FULL_IMAGE}"
     done
