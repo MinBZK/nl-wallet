d36fd23d144712282a61eef3d5bb0e8f9c29edd5
Jacob Middag <jacob.middag@ictu.nl>
2026-01-02 16:04:52 +0100

    Push CI images to both Harbor registries

--- deploy/docker-images/build-ci.sh
+++ deploy/docker-images/build-ci.sh
@@ -12,16 +12,19 @@ FULL_IMAGE_PREFIX="${HARBOR_REGISTRY}/${HARBOR_PROJECT}/${IMAGE_PREFIX}"
 # Tell us what image name we're using:
 echo "Image to build is: \"$IMAGE_NAME\" with tag \"${IMAGE_TAG}\""
 
-if [[ -e "$DOCKERFILE" ]]; then
+if [[ -e $DOCKERFILE ]]; then
     buildah build \
-        --build-arg "HARBOR_REGISTRY=${HARBOR_REGISTRY}" \
         --build-arg "DOCKER_HUB_PROXY=${HARBOR_REGISTRY}/docker-hub-proxy/" \
         --build-arg "FROM_IMAGE_PREFIX=${FULL_IMAGE_PREFIX}-" \
         --build-arg "TAG=${IMAGE_TAG}" \
         --file "${DOCKERFILE}" \
-        --tag "${FULL_IMAGE_PREFIX}-${IMAGE_NAME}:${IMAGE_TAG}" \
+        --tag "${IMAGE_NAME}:${IMAGE_TAG}" \
         "${CONTEXT}"
-    buildah push "${FULL_IMAGE_PREFIX}-${IMAGE_NAME}:${IMAGE_TAG}"
+    for HARBOR_REGISTRY in $HARBOR_REGISTRIES; do
+        FULL_IMAGE="${HARBOR_REGISTRY}/${HARBOR_PROJECT}/${IMAGE_PREFIX}-${IMAGE_NAME}:${IMAGE_TAG}"
+        buildah push ${IMAGE_NAME}:${IMAGE_TAG} "docker-daemon:${FULL_IMAGE}"
+        buildah push ${IMAGE_NAME}:${IMAGE_TAG} "docker://${FULL_IMAGE}"
+    done
 else
     echo "ERROR: ${IMAGE_NAME}.Dockerfile does not exist!"
     exit 1
