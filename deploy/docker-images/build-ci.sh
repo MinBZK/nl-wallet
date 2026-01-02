dc6c3847916a4ddd7c8fd934afc0134e11ceda9e
Jacob Middag <jacob.middag@ictu.nl>
2026-01-02 18:49:59 +0100

    Echo steps on ci-build

--- deploy/docker-images/build-ci.sh
+++ deploy/docker-images/build-ci.sh
@@ -14,14 +14,16 @@ echo "Image to build is: \"$IMAGE_NAME\" with tag \"${IMAGE_TAG}\""
 
 if [[ -e $DOCKERFILE ]]; then
     # Prefetch previous layers from docker daemon of host to speed up
-    for id in $(awk '/^FROM \$\{FROM_IMAGE_PREFIX\}/ {
+    for ID in $(awk '/^FROM \$\{FROM_IMAGE_PREFIX\}/ {
         gsub(/\$\{FROM_IMAGE_PREFIX}/, "'"${FROM_IMAGE_PREFIX}"'", $2);
         gsub(/\$\{TAG}/, "'"${IMAGE_TAG}"'", $2);
         print $2;
     }' "${DOCKERFILE}"); do
-        buildah pull "docker-daemon:${id}" || true
+        echo "Prefetching $ID"
+        buildah pull "docker-daemon:${ID}" || true
     done
 
+    echo "Building"
     buildah build \
         --build-arg "DOCKER_HUB_PROXY=${HARBOR_REGISTRY}/docker-hub-proxy/" \
         --build-arg "FROM_IMAGE_PREFIX=${FROM_IMAGE_PREFIX}" \
@@ -31,8 +33,12 @@ if [[ -e $DOCKERFILE ]]; then
         "${CONTEXT}"
     for HARBOR_REGISTRY in $HARBOR_REGISTRIES; do
         FULL_IMAGE="${HARBOR_REGISTRY}/${HARBOR_PROJECT}/${IMAGE_PREFIX}-${IMAGE_NAME}:${IMAGE_TAG}"
+
         # Push to docker daemon of host to speed up next use
+        echo "Pushing for $HARBOR_REGISTRY to docker-daemon"
         buildah push ${IMAGE_NAME}:${IMAGE_TAG} "docker-daemon:${FULL_IMAGE}"
+
+        echo "Pushing for $HARBOR_REGISTRY to registry"
         buildah push ${IMAGE_NAME}:${IMAGE_TAG} "docker://${FULL_IMAGE}"
     done
 else
