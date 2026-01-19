f9624e9c6b0e1822e27b7a0443965115ff803053
Ruud Klaver <ruud@pantsforcats.nl>
2026-01-19 10:02:34 +0100

    Simplify Hsm type derived traits and hide user PIN

--- wallet_core/lib/hsm/src/settings.rs
+++ wallet_core/lib/hsm/src/settings.rs
@@ -1,15 +1,16 @@
 use std::path::PathBuf;
 use std::time::Duration;
 
+use derive_more::Debug;
 use serde::Deserialize;
 use serde_with::DurationSeconds;
 use serde_with::serde_as;
 
 #[serde_as]
-#[derive(Debug, Clone, Deserialize)]
-#[cfg_attr(feature = "test", derive(PartialEq, Eq))]
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
 pub struct Hsm {
     pub library_path: PathBuf,
+    #[debug(skip)]
     pub user_pin: String,
     pub max_sessions: u8,
 
