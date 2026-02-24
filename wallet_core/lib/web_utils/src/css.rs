0d800bf3f930e7de5668690f2c17d57000ec57ea
Jippe Holwerda <mail@jippeholwerda.nl>
2026-02-24 13:23:04 +0100

    Use etag crate

--- wallet_core/lib/web_utils/src/css.rs
+++ wallet_core/lib/web_utils/src/css.rs
@@ -1,30 +1,42 @@
+use std::sync::OnceLock;
+
 use axum::http::HeaderMap;
+use axum::http::HeaderValue;
 use axum::http::StatusCode;
 use axum::http::header;
 use axum::response::IntoResponse;
 use axum::response::Response;
-use base64::Engine;
-use base64::prelude::BASE64_STANDARD;
-
-use crypto::utils::sha256;
+use etag::EntityTag;
 
 /// Serves bundled CSS content with ETag-based caching.
 ///
 /// Returns 304 Not Modified if the client's `If-None-Match` header matches the content hash.
 pub fn serve_bundled_css(headers: &HeaderMap, css: &'static str) -> Response {
-    let etag = format!("\"{}\"", BASE64_STANDARD.encode(sha256(css.as_bytes())));
+    static BUNDLED_ETAG: OnceLock<EntityTag> = OnceLock::new();
+    let bundled_etag = BUNDLED_ETAG.get_or_init(|| EntityTag::from_data(css.as_bytes()));
 
-    if let Some(if_none_match) = headers.get(header::IF_NONE_MATCH)
-        && if_none_match.as_bytes() == etag.as_bytes()
-    {
-        return (StatusCode::NOT_MODIFIED, [(header::ETAG, etag)]).into_response();
+    if let Some(if_none_match) = headers.get(header::IF_NONE_MATCH) {
+        let Some(entity_tag) = if_none_match.to_str().ok().and_then(|etag| etag.parse().ok()) else {
+            return (StatusCode::BAD_REQUEST).into_response();
+        };
+
+        if bundled_etag.weak_eq(&entity_tag) {
+            return (StatusCode::NOT_MODIFIED, [(header::ETAG, bundled_etag.to_string())]).into_response();
+        }
     }
 
     (
+        // We can safely unwrap all header values below because we know there are no non-ascii characters used.
         [
-            (header::CONTENT_TYPE, "text/css; charset=utf-8".to_string()),
-            (header::ETAG, etag),
-            (header::CACHE_CONTROL, "public, max-age=604800".to_string()),
+            (
+                header::CONTENT_TYPE,
+                HeaderValue::from_str("text/css; charset=utf-8").unwrap(),
+            ),
+            (header::ETAG, HeaderValue::from_str(&bundled_etag.to_string()).unwrap()),
+            (
+                header::CACHE_CONTROL,
+                HeaderValue::from_str("public, max-age=604800").unwrap(),
+            ),
         ],
         css,
     )
