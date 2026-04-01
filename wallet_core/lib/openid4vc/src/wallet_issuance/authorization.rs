881e7de39d6e78a49fa470855445aa32548cb961
Jippe Holwerda <mail@jippeholwerda.nl>
2026-04-01 20:17:41 +0200

    Re-add unit tests for HttpAuthorizationSession

--- wallet_core/lib/openid4vc/src/wallet_issuance/authorization.rs
+++ wallet_core/lib/openid4vc/src/wallet_issuance/authorization.rs
@@ -183,3 +183,225 @@ impl AuthorizationSession for HttpAuthorizationSession {
         .await
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use assert_matches::assert_matches;
+    use rstest::rstest;
+    use serial_test::serial;
+    use url::Url;
+
+    use http_utils::reqwest::HttpJsonClient;
+    use http_utils::reqwest::default_reqwest_client_builder;
+
+    use crate::AuthorizationErrorCode;
+    use crate::metadata::issuer_metadata::IssuerMetadata;
+    use crate::metadata::oauth_metadata::AuthorizationServerMetadata;
+    use crate::pkce::MockPkcePair;
+
+    use super::HttpAuthorizationSession;
+    use super::OAuthError;
+
+    const ISSUER_URL: &str = "https://example.com";
+    const CLIENT_ID: &str = "client-1";
+    const REDIRECT_URI: &str = "redirect://here";
+    const CSRF_TOKEN: &str = "csrf_token";
+    const CODE: &str = "code";
+
+    pub fn url_with_query_pairs(mut url: Url, query_pairs: &[(&str, &str)]) -> Url {
+        if !query_pairs.is_empty() {
+            let mut query = url.query_pairs_mut();
+            query_pairs.iter().for_each(|(name, value)| {
+                query.append_pair(name, value);
+            });
+        }
+        url
+    }
+
+    fn try_new_mock_session() -> HttpAuthorizationSession<MockPkcePair> {
+        let pkce_context = MockPkcePair::generate_context();
+        pkce_context.expect().return_once(|| {
+            let mut pkce_pair = MockPkcePair::new();
+            pkce_pair.expect_code_challenge().return_const("challenge".to_string());
+            pkce_pair
+        });
+        HttpAuthorizationSession::<MockPkcePair>::try_new(
+            HttpJsonClient::try_new(default_reqwest_client_builder()).unwrap(),
+            IssuerMetadata::new_mock(ISSUER_URL.parse().unwrap(), "test"),
+            AuthorizationServerMetadata::new_mock(ISSUER_URL.parse().unwrap()),
+            CLIENT_ID.to_string(),
+            REDIRECT_URI.parse().unwrap(),
+        )
+        .unwrap()
+    }
+
+    fn create_session() -> HttpAuthorizationSession<MockPkcePair> {
+        let mut pkce_pair = MockPkcePair::new();
+        pkce_pair.expect_code_challenge().return_const("challenge".to_string());
+        HttpAuthorizationSession {
+            issuer_metadata: IssuerMetadata::new_mock(ISSUER_URL.parse().unwrap(), "test"),
+            oauth_metadata: AuthorizationServerMetadata::new_mock(ISSUER_URL.parse().unwrap()),
+            http_client: HttpJsonClient::try_new(default_reqwest_client_builder()).unwrap(),
+            auth_url: ISSUER_URL.parse().unwrap(),
+            client_id: CLIENT_ID.to_string(),
+            redirect_uri: REDIRECT_URI.parse().unwrap(),
+            pkce_pair,
+            state: CSRF_TOKEN.to_string(),
+        }
+    }
+
+    #[tokio::test]
+    #[serial(MockPkcePair)]
+    async fn test_start_and_into_token_request() {
+        let session = try_new_mock_session();
+
+        let state = session.state.clone();
+        let redirect_uri: Url = REDIRECT_URI.parse().unwrap();
+        let received = url_with_query_pairs(redirect_uri, &[("code", CODE), ("state", &state)]);
+
+        let code = session.authorization_code(&received).unwrap();
+
+        assert_eq!(code.as_ref(), CODE);
+    }
+
+    #[tokio::test]
+    #[serial(MockPkcePair)]
+    async fn test_user_cancels() {
+        let session = try_new_mock_session();
+
+        let state = session.state.clone();
+        let redirect_uri: Url = REDIRECT_URI.parse().unwrap();
+        let received = url_with_query_pairs(redirect_uri, &[("error", "access_denied"), ("state", &state)]);
+
+        let error = session.authorization_code(&received).unwrap_err();
+
+        assert_matches!(error, OAuthError::Denied);
+    }
+
+    #[tokio::test]
+    #[serial(MockPkcePair)]
+    async fn test_auth_url() {
+        let session = try_new_mock_session();
+
+        let params: std::collections::HashMap<_, _> = session.auth_url.query_pairs().collect();
+
+        assert_eq!(params.get("response_type").map(|v| v.as_ref()), Some("code"));
+        assert_eq!(params.get("client_id").map(|v| v.as_ref()), Some(CLIENT_ID));
+        assert_eq!(params.get("redirect_uri").map(|v| v.as_ref()), Some(REDIRECT_URI));
+        assert_eq!(params.get("code_challenge_method").map(|v| v.as_ref()), Some("S256"));
+        assert_eq!(params.get("code_challenge").map(|v| v.as_ref()), Some("challenge"));
+        assert_eq!(params.get("scope").map(|v| v.as_ref()), Some("openid"));
+        assert!(params.contains_key("state"));
+        assert!(params.contains_key("nonce"));
+    }
+
+    #[test]
+    fn test_matches_received_redirect_uri() {
+        let session = create_session();
+
+        assert!(session.matches_received_redirect_uri(&Url::parse(REDIRECT_URI).unwrap()));
+        assert!(session.matches_received_redirect_uri(&url_with_query_pairs(
+            Url::parse(REDIRECT_URI).unwrap(),
+            &[("foo", "bar"), ("bleh", "blah")]
+        )));
+
+        assert!(!session.matches_received_redirect_uri(&Url::parse("https://example.com").unwrap()));
+        assert!(!session.matches_received_redirect_uri(&Url::parse("scheme://host/path").unwrap()));
+    }
+
+    #[test]
+    fn test_redirect_uri_mismatch() {
+        let session = create_session();
+        let uri = Url::parse("http://not-the-redirect-uri.com").unwrap();
+
+        let error = session.authorization_code(&uri).unwrap_err();
+
+        assert_matches!(error, OAuthError::RedirectUriMismatch);
+    }
+
+    #[test]
+    fn test_error() {
+        let session = create_session();
+        let uri = url_with_query_pairs(
+            Url::parse(REDIRECT_URI).unwrap(),
+            &[
+                ("error", "invalid_request"),
+                ("error_description", "this is the error description"),
+            ],
+        );
+
+        let error = session.authorization_code(&uri).unwrap_err();
+
+        assert_matches!(
+            error,
+            OAuthError::RedirectUriError(response)
+                if matches!(response.error, AuthorizationErrorCode::InvalidRequest)
+                && response.error_description == Some("this is the error description".to_string())
+        );
+    }
+
+    #[test]
+    fn test_state_mismatch() {
+        let session = create_session();
+        let uri = url_with_query_pairs(
+            Url::parse(REDIRECT_URI).unwrap(),
+            &[("code", CODE), ("state", "foobar")],
+        );
+
+        let error = session.authorization_code(&uri).unwrap_err();
+
+        assert_matches!(error, OAuthError::StateTokenMismatch);
+    }
+
+    #[test]
+    fn test_missing_code() {
+        let session = create_session();
+        let uri = url_with_query_pairs(Url::parse(REDIRECT_URI).unwrap(), &[("state", CSRF_TOKEN)]);
+
+        let error = session.authorization_code(&uri).unwrap_err();
+
+        assert_matches!(error, OAuthError::UrlDecoding(err) if err.to_string() == "missing field `code`");
+    }
+
+    #[test]
+    fn test_get_authorization_url() {
+        let session = create_session();
+        let uri = url_with_query_pairs(
+            Url::parse(REDIRECT_URI).unwrap(),
+            &[("state", CSRF_TOKEN), ("code", "123")],
+        );
+
+        assert_eq!(session.authorization_code(&uri).unwrap().as_ref(), "123");
+    }
+
+    #[test]
+    fn test_into_token_request() {
+        let mut session = create_session();
+        session
+            .pkce_pair
+            .expect_into_code_verifier()
+            .return_const("code_verifier".to_string());
+
+        let uri = url_with_query_pairs(
+            Url::parse(REDIRECT_URI).unwrap(),
+            &[("state", CSRF_TOKEN), ("code", "123")],
+        );
+
+        let code = session.authorization_code(&uri).unwrap();
+
+        assert_eq!(code.as_ref(), "123");
+    }
+
+    #[rstest]
+    #[case("http://example.com", [], "http://example.com")]
+    #[case("http://example.com", [("foo", "bar"), ("bleh", "blah")], "http://example.com?foo=bar&bleh=blah")]
+    #[case("http://example.com", [("foo", ""), ("foo", "more_foo")], "http://example.com?foo=&foo=more_foo")]
+    fn test_url_with_query_pairs<const N: usize>(
+        #[case] url: Url,
+        #[case] query_pairs: [(&str, &str); N],
+        #[case] expected: Url,
+    ) {
+        let url = url_with_query_pairs(url, &query_pairs);
+        assert_eq!(url, expected);
+    }
+}
