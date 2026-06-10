f8b1c903e0e82ecaf953a59e44f6b019c2d1092e
Jacob Middag <jacob.middag@ictu.nl>
2026-06-10 14:04:37 +0200

    Update playwright to 1.60.0

--- deploy/docker-images/ci/playwright.sh
+++ deploy/docker-images/ci/playwright.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-DEBIAN_FRONTEND=noninteractive npx @playwright/test@1.56.1 install-deps
+DEBIAN_FRONTEND=noninteractive npx @playwright/test@1.60.0 install-deps
+
 rm -rf ~/.npm
