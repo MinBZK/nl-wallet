96f41e3d3231a9d97374d17d031bd58f9631b1b3
Jacob Middag <jacob.middag@ictu.nl>
2026-06-10 14:10:31 +0200

    Preinstall playwright browsers

--- deploy/docker-images/ci/playwright.sh
+++ deploy/docker-images/ci/playwright.sh
@@ -3,4 +3,11 @@ set -euxo pipefail
 
 DEBIAN_FRONTEND=noninteractive npx @playwright/test@1.60.0 install-deps
 
+# Change ownership to wallet
+mkdir -p .cache # rosetta already creates cache directory
+chown -R wallet:wallet .cache .npm
+
+# Preinstall browsers
+sudo -E -H -u wallet -- sh -c 'cd $HOME && npx @playwright/test@1.60.0 install chromium webkit'
+
 rm -rf ~/.npm
