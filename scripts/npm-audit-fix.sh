e0cb5bc8f4cc9d6388c29c486ed9e7cb5440b9ec
Arjen <edi@minbzk.nl>
2026-07-30 11:17:49 +0200

    Also do pnpm audit --fix

--- scripts/npm-audit-fix.sh
+++ scripts/npm-audit-fix.sh
@@ -14,6 +14,16 @@ done < <(find "$BASE_DIR" -name package-lock.json -not -path '*/node_modules/*' 
 
 cd "$BASE_DIR"
 
+while IFS= read -r -d '' lockfile; do
+    echo "Fixing $lockfile..."
+    cd "$(dirname "$lockfile")"
+    # First try to update. If it can't be fixed, override
+    pnpm audit --fix update || true
+    pnpm audit --fix override && pnpm install || true
+done < <(find "$BASE_DIR" -name pnpm-lock.yaml -not -path '*/node_modules/*' -print0)
+
+cd "$BASE_DIR"
+
 # Create MR if on CI and main
 if [[ -n $CI && -n $(git status --porcelain) && $CI_COMMIT_BRANCH == "$CI_DEFAULT_BRANCH" ]]; then
     exec "$BASE_DIR/deploy/bin/audit-fix-pr.sh" npm-audit-fix "Run npm audit fix"
