36a68c92b627bf8ec257db2f73d6a22cf9386dac
Ruud Klaver <ruud@pantsforcats.nl>
2026-08-10 11:10:31 +0200

    Add types for OpenID4VCI 1.0 Credential Request

--- wallet_core/lib/openid4vc/src/credential.rs
+++ wallet_core/lib/openid4vc/src/credential.rs
@@ -2,6 +2,10 @@ use std::time::Duration;
 
 use attestation_types::credential_format::Format;
 use derive_more::Constructor;
+use jwk_simple::Key;
+use jwt::UnverifiedJwt;
+use jwt::headers::HeaderWithJwk;
+use jwt::pop::JwtPopClaims;
 use mdoc::IssuerSigned;
 use mdoc::utils::serialization::CborBase64;
 use sd_jwt::sd_jwt::UnverifiedSdJwt;
@@ -17,6 +21,10 @@ use utils::vec_at_least::IntoNonEmptyIterator;
 use utils::vec_at_least::NonEmptyIterator;
 use utils::vec_at_least::VecNonEmpty;
 use utils::vec_nonempty;
+
+use crate::jwe::JweCompressionAlgorithm;
+use crate::jwe::JweEncryptionAlgorithm;
+use crate::metadata::issuer_metadata::CredentialConfigurationId;
 
 pub mod draft {
     use std::fmt;
@@ -108,6 +116,87 @@ pub mod draft {
     pub struct CredentialResponses {
         pub credential_responses: Vec<CredentialResponse>,
     }
+}
+
+/// A request sent to the issuer's Credential Endpoint.
+///
+/// See: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.2>
+#[skip_serializing_none]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CredentialRequest {
+    /// Either a Credential or Credential Configuration Identifier, depending on whether the Token Response contained
+    /// the `authorization_details` field.
+    #[serde(flatten)]
+    pub identifier: CredentialRequestIdentifier,
+
+    /// Object providing one or more proof of possessions of the cryptographic key material to which the issued
+    /// Credential instances will be bound to. The `proofs` parameter contains exactly one parameter named as the proof
+    /// type, the value set for this parameter is a non-empty array containing parameters as defined by the
+    /// corresponding proof type.
+    pub proofs: Option<CredentialRequestProofs>,
+
+    /// Object containing information for encrypting the Credential Response. If this request element is not present,
+    /// the corresponding credential response returned is not encrypted.
+    ///
+    /// TODO (PVW-5538): Implement credential request and response encryption.
+    pub credential_response_encryption: Option<CredentialResponseEncryption>,
+}
+
+impl CredentialRequest {
+    pub fn new_credential_id(credential_id: String, proofs: VecNonEmpty<UnverifiedJwtProof>) -> Self {
+        Self {
+            identifier: CredentialRequestIdentifier::CredentialIdentifier(credential_id),
+            proofs: Some(CredentialRequestProofs::Jwt(proofs)),
+            credential_response_encryption: None,
+        }
+    }
+
+    pub fn new_config_id(config_id: CredentialConfigurationId, proofs: VecNonEmpty<UnverifiedJwtProof>) -> Self {
+        Self {
+            identifier: CredentialRequestIdentifier::CredentialConfigurationId(config_id),
+            proofs: Some(CredentialRequestProofs::Jwt(proofs)),
+            credential_response_encryption: None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum CredentialRequestIdentifier {
+    /// REQUIRED when an Authorization Details of type `openid_credential` was returned from the Token Response. It MUST
+    /// NOT be used otherwise. A string that identifies a Credential Dataset that is requested for issuance.
+    CredentialIdentifier(String),
+
+    /// REQUIRED if a credential_identifiers parameter was not returned from the Token Response as part of the
+    /// `authorization_details` parameter. It MUST NOT be used otherwise. String that uniquely identifies one of the
+    /// keys in the name/value pairs stored in the `credential_configurations_supported` Credential Issuer metadata. The
+    /// corresponding object in the `credential_configurations_supported` map MUST contain one of the value(s) used in
+    /// the scope parameter in the Authorization Request.
+    CredentialConfigurationId(CredentialConfigurationId),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum CredentialRequestProofs {
+    // TODO (PVW-5548): Implement `attestation` proof type and update `jwt` proof type to OpenID4VCI 1.0.
+    Jwt(VecNonEmpty<UnverifiedJwt<JwtPopClaims, HeaderWithJwk>>),
+}
+
+/// Object containing information for encrypting the Credential Response.
+///
+/// See: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.2-2.4.1>
+#[skip_serializing_none]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CredentialResponseEncryption {
+    /// Object containing a single public key as a JWK used for encrypting the Credential Response.
+    pub jwk: Key,
+
+    /// JWE (RFC7516) enc algorithm (RFC7518) for encrypting Credential Responses.
+    pub enc: JweEncryptionAlgorithm,
+
+    /// JWE (RFC7516) zip algorithm (RFC7518) for compressing Credential Responses prior to encryption. If absent then
+    /// compression MUST not be used.
+    pub zip: Option<JweCompressionAlgorithm>,
 }
 
 /// A Credential Response, see: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.3>.
@@ -242,12 +331,103 @@ mod tests {
     use sd_jwt::examples::SD_JWT_VC;
     use serde_json::json;
 
+    use super::CredentialRequest;
+    use super::CredentialRequestIdentifier;
+    use super::CredentialRequestProofs;
     use super::CredentialResponse;
     use super::Credentials;
 
     #[test]
+    fn test_credential_request_serialization_config_id_example() {
+        // Source: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.2-12>,
+        // modified using: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#appendix-F.1-6>
+        let example_json = json!({
+            "credential_configuration_id": "org.iso.18013.5.1.mDL",
+            "proofs": {
+                "jwt": [
+                        "eyJ0eXAiOiJvcGVuaWQ0dmNpLXByb29mK2p3dCIsImFsZyI6IkVTMjU2Iiwiand
+                         rIjp7Imt0eSI6IkVDIiwiY3J2IjoiUC0yNTYiLCJ4IjoiblVXQW9BdjNYWml0aDh
+                         FN2kxOU9kYXhPTFlGT3dNLVoyRXVNMDJUaXJUNCIsInkiOiJIc2tIVThCalVpMVU
+                         5WHFpN1N3bWo4Z3dBS18weGtjRGpFV183MVNvc0VZIn19.eyJhdWQiOiJodHRwcz
+                         ovL2NyZWRlbnRpYWwtaXNzdWVyLmV4YW1wbGUuY29tIiwiaWF0IjoxNzAxOTYwND
+                         Q0LCJub25jZSI6IkxhclJHU2JtVVBZdFJZTzZCUTR5bjgifQ.-a3EDsxClUB4O3L
+                         eDD5DVGEnNMT01FCQW4P6-2-BNBqc_Zxf0Qw4CWayLEpqkAomlkLb9zioZoipdP-
+                         jvh1WlA"
+                    ]
+                }
+        });
+
+        let credential_request = serde_json::from_value::<CredentialRequest>(example_json.clone())
+            .expect("deserializing CredentialRequest should succeed");
+
+        let CredentialRequestIdentifier::CredentialConfigurationId(config_id) = &credential_request.identifier else {
+            panic!("identifier in CredentialRequest should be Credential Configuration ID");
+        };
+        assert_eq!(config_id.as_ref(), "org.iso.18013.5.1.mDL");
+
+        let proof_count = credential_request
+            .proofs
+            .as_ref()
+            .map(|CredentialRequestProofs::Jwt(jwts)| jwts.len().get())
+            .unwrap_or_default();
+        assert_eq!(proof_count, 1);
+
+        let json = serde_json::to_value(credential_request).expect("serializing CredentialRequest should succeed");
+
+        assert_eq!(example_json, json);
+    }
+
+    #[test]
+    fn test_credential_request_serialization_credential_id_example() {
+        // Source: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.2-14>,
+        // modified using: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#appendix-F.1-6>
+        let example_json = json!({
+            "credential_identifier": "CivilEngineeringDegree-2023",
+            "proofs": {
+            "jwt": [
+                        "eyJ0eXAiOiJvcGVuaWQ0dmNpLXByb29mK2p3dCIsImFsZyI6IkVTMjU2Iiwiand
+                         rIjp7Imt0eSI6IkVDIiwiY3J2IjoiUC0yNTYiLCJ4IjoiblVXQW9BdjNYWml0aDh
+                         FN2kxOU9kYXhPTFlGT3dNLVoyRXVNMDJUaXJUNCIsInkiOiJIc2tIVThCalVpMVU
+                         5WHFpN1N3bWo4Z3dBS18weGtjRGpFV183MVNvc0VZIn19.eyJhdWQiOiJodHRwcz
+                         ovL2NyZWRlbnRpYWwtaXNzdWVyLmV4YW1wbGUuY29tIiwiaWF0IjoxNzAxOTYwND
+                         Q0LCJub25jZSI6IkxhclJHU2JtVVBZdFJZTzZCUTR5bjgifQ.-a3EDsxClUB4O3L
+                         eDD5DVGEnNMT01FCQW4P6-2-BNBqc_Zxf0Qw4CWayLEpqkAomlkLb9zioZoipdP-
+                         jvh1WlA",
+                        "eyJ0eXAiOiJvcGVuaWQ0dmNpLXByb29mK2p3dCIsImFsZyI6IkVTMjU2Iiwiand
+                         rIjp7Imt0eSI6IkVDIiwiY3J2IjoiUC0yNTYiLCJ4IjoiblVXQW9BdjNYWml0aDh
+                         FN2kxOU9kYXhPTFlGT3dNLVoyRXVNMDJUaXJUNCIsInkiOiJIc2tIVThCalVpMVU
+                         5WHFpN1N3bWo4Z3dBS18weGtjRGpFV183MVNvc0VZIn19.eyJhdWQiOiJodHRwcz
+                         ovL2NyZWRlbnRpYWwtaXNzdWVyLmV4YW1wbGUuY29tIiwiaWF0IjoxNzAxOTYwND
+                         Q0LCJub25jZSI6IkxhclJHU2JtVVBZdFJZTzZCUTR5bjgifQ.-a3EDsxClUB4O3L
+                         eDD5DVGEnNMT01FCQW4P6-2-BNBqc_Zxf0Qw4CWayLEpqkAomlkLb9zioZoipdP-
+                         jvh1WlA"
+                ]
+            }
+        });
+
+        let credential_request = serde_json::from_value::<CredentialRequest>(example_json.clone())
+            .expect("deserializing CredentialRequest should succeed");
+
+        let CredentialRequestIdentifier::CredentialIdentifier(credential_id) = &credential_request.identifier else {
+            panic!("identifier in CredentialRequest should be Credential ID");
+        };
+        assert_eq!(credential_id, "CivilEngineeringDegree-2023");
+
+        let proof_count = credential_request
+            .proofs
+            .as_ref()
+            .map(|CredentialRequestProofs::Jwt(jwts)| jwts.len().get())
+            .unwrap_or_default();
+        assert_eq!(proof_count, 2);
+
+        let json = serde_json::to_value(credential_request).expect("serializing CredentialRequest should succeed");
+
+        assert_eq!(example_json, json);
+    }
+
+    #[test]
     fn test_deferred_credential_response_serialization() {
-        // Source: https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.3-12
+        // Source: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.3-12>
         let json = json!({
             "transaction_id": "8xLOxBtZp8",
             "interval" : 3600
@@ -272,7 +452,7 @@ mod tests {
 
     #[test]
     fn test_sd_jwt_credential_response_serialization() {
-        // Source: https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.3-8
+        // Source: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.3-8>
         let json = json!({
             "credentials": [
                 {
@@ -307,7 +487,7 @@ mod tests {
         let issuer_signed = device_response.documents.unwrap().into_first().issuer_signed;
         let credential = BASE64_URL_SAFE_NO_PAD.encode(cbor_serialize(&issuer_signed).unwrap());
 
-        // Source: https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.3-8
+        // Source: <https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.3-8>
         let json = json!({
             "credentials": [
                 {
